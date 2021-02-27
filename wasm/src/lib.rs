048ab2aaf4e831d479b1430065dad109c09231c5
howardwu <howardwu@berkeley.edu>
2021-02-27 12:31:50 -0800

    Adds initial wasm module

---
+++ wasm/src/lib.rs
@@ -0,0 +1,30 @@
+// Copyright (C) 2019-2021 Aleo Systems Inc.
+// This file is part of the snarkVM library.
+
+// The snarkVM library is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+
+// The snarkVM library is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU General Public License for more details.
+
+// You should have received a copy of the GNU General Public License
+// along with the snarkVM library. If not, see <https://www.gnu.org/licenses/>.
+
+// pub use snarkvm_algorithms::{
+//     commitment::*,
+//     commitment_tree::*,
+//     crh::*,
+//     encoding::*,
+//     encryption::*,
+//     fft::*,
+//     merkle_tree::*,
+//     msm::*,
+//     prf::*,
+//     signature::*,
+//     snark::*,
+// };
+pub use snarkvm_curves::{bls12_377::*, bw6_761::*, edwards_bls12::*, edwards_sw6::*};
