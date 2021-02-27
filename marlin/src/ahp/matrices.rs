91dd06632f12960fe5b18f4f017127e2ae0e8f66
howardwu <howardwu@berkeley.edu>
2021-02-26 19:56:49 -0800

    Add matrix operations

--- marlin/src/ahp/matrices.rs
+++ marlin/src/ahp/matrices.rs
@@ -53,9 +53,69 @@ pub(crate) fn to_matrix_helper<F: Field>(matrix: &[Vec<(F, VarIndex)>], num_inpu
     new_matrix
 }
 
+pub(crate) fn balance_matrices<F: Field>(a_matrix: &mut Matrix<F>, b_matrix: &mut Matrix<F>) {
+    let mut a_density: usize = a_matrix.iter().map(|row| row.len()).sum();
+    let mut b_density: usize = b_matrix.iter().map(|row| row.len()).sum();
+    let mut max_density = core::cmp::max(a_density, b_density);
+    let mut a_is_denser = a_density == max_density;
+    for (a_row, b_row) in a_matrix.iter_mut().zip(b_matrix) {
+        if a_is_denser {
+            let a_row_size = a_row.len();
+            let b_row_size = b_row.len();
+            core::mem::swap(a_row, b_row);
+            a_density = a_density - a_row_size + b_row_size;
+            b_density = b_density - b_row_size + a_row_size;
+            max_density = core::cmp::max(a_density, b_density);
+            a_is_denser = a_density == max_density;
+        }
+    }
+}
+
+// pub(crate) fn num_non_zero<F: PrimeField>(matrices: &ConstraintMatrices<F>) -> usize {
+//     *[
+//         matrices.a_num_non_zero,
+//         matrices.b_num_non_zero,
+//         matrices.c_num_non_zero,
+//     ]
+//     .iter()
+//     .max()
+//     .unwrap()
+// }
+
+pub(crate) fn make_matrices_square_for_indexer<F: PrimeField, CS: ConstraintSystem<F>>(cs: &mut CS) {
+    let num_variables = cs.num_public_variables() + cs.num_private_variables();
+    let matrix_dim = padded_matrix_dim(num_variables, cs.num_constraints());
+    make_matrices_square(cs, num_variables);
+    assert_eq!(
+        cs.num_public_variables() + cs.num_private_variables(),
+        cs.num_constraints(),
+        "padding failed!"
+    );
+    assert_eq!(
+        cs.num_public_variables() + cs.num_private_variables(),
+        matrix_dim,
+        "padding does not result in expected matrix size!"
+    );
+}
+
 /// This must *always* be in sync with `make_matrices_square`.
 pub(crate) fn padded_matrix_dim(num_formatted_variables: usize, num_constraints: usize) -> usize {
     core::cmp::max(num_formatted_variables, num_constraints)
+}
+
+pub(crate) fn pad_input_for_indexer_and_prover<F: PrimeField, CS: ConstraintSystem<F>>(cs: &mut CS) {
+    let formatted_input_size = cs.num_public_variables();
+
+    let domain_x = EvaluationDomain::<F>::new(formatted_input_size);
+    assert!(domain_x.is_some());
+
+    let padded_size = domain_x.unwrap().size();
+
+    if padded_size > formatted_input_size {
+        for i in 0..(padded_size - formatted_input_size) {
+            cs.alloc_input(|| format!("pad_input_{}", i), || Ok(F::zero())).unwrap();
+        }
+    }
 }
 
 pub(crate) fn make_matrices_square<F: Field, CS: ConstraintSystem<F>>(cs: &mut CS, num_formatted_variables: usize) {
@@ -66,13 +126,13 @@ pub(crate) fn make_matrices_square<F: Field, CS: ConstraintSystem<F>>(cs: &mut C
         use core::convert::identity as iden;
         // Add dummy constraints of the form 0 * 0 == 0
         for i in 0..matrix_padding {
-            cs.enforce(|| format!("pad constraint {}", i), iden, iden, iden);
+            cs.enforce(|| format!("pad_constraint_{}", i), iden, iden, iden);
         }
     } else {
         // Add dummy unconstrained variables
         for i in 0..matrix_padding {
             let _ = cs
-                .alloc(|| format!("pad var {}", i), || Ok(F::one()))
+                .alloc(|| format!("pad_variable_{}", i), || Ok(F::one()))
                 .expect("alloc failed");
         }
     }
@@ -233,4 +293,32 @@ fn is_in_ascending_order<T: Ord>(x_s: &[T], is_less_than: impl Fn(&T, &T) -> boo
         }
         is_sorted
     }
+}
+
+/* ************************************************************************* */
+/* ************************************************************************* */
+/* ************************************************************************* */
+
+/// Formats the public input according to the requirements of the constraint
+/// system
+pub(crate) fn format_public_input<F: PrimeField>(public_input: &[F]) -> Vec<F> {
+    let mut input = vec![F::one()];
+    input.extend_from_slice(public_input);
+    input
+}
+
+/// Takes in a previously formatted public input and removes the formatting
+/// imposed by the constraint system.
+pub(crate) fn unformat_public_input<F: PrimeField>(input: &[F]) -> Vec<F> {
+    input[1..].to_vec()
+}
+
+pub(crate) fn make_matrices_square_for_prover<F: PrimeField, CS: ConstraintSystem<F>>(cs: &mut CS) {
+    let num_variables = cs.num_public_variables() + cs.num_private_variables();
+    make_matrices_square(cs, num_variables);
+    assert_eq!(
+        cs.num_public_variables() + cs.num_private_variables(),
+        cs.num_constraints(),
+        "padding failed!"
+    );
 }
