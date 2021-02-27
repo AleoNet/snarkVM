de70ff3cfb7671c3aa0ce4ccf88cbe6b6c580059
howardwu <howardwu@berkeley.edu>
2021-02-27 13:49:08 -0800

    Update parameter error usage

--- models/src/parameters/parameters.rs
+++ models/src/parameters/parameters.rs
@@ -14,7 +14,7 @@
 // You should have received a copy of the GNU General Public License
 // along with the snarkVM library. If not, see <https://www.gnu.org/licenses/>.
 
-use snarkvm_errors::parameters::ParameterError;
+use snarkvm_parameters::errors::ParameterError;
 
 pub trait Parameter {
     const CHECKSUM: &'static str;
--- parameters/src/params.rs
+++ parameters/src/params.rs
@@ -14,8 +14,8 @@
 // You should have received a copy of the GNU General Public License
 // along with the snarkVM library. If not, see <https://www.gnu.org/licenses/>.
 
+use crate::errors::ParameterError;
 use snarkvm_algorithms::crh::sha256::sha256;
-use snarkvm_errors::parameters::ParameterError;
 use snarkvm_models::parameters::Parameter;
 
 use std::{
