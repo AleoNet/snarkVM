2a14b77d006129267cb22f4d70d7701a8455e80d
collin <collin.chin@berkeley.edu>
2021-03-18 13:02:02 -0700

    fix dependencies

--- gadgets/src/traits/utilities/uint/unsigned_integer.rs
+++ gadgets/src/traits/utilities/uint/unsigned_integer.rs
@@ -14,13 +14,6 @@
 // You should have received a copy of the GNU General Public License
 // along with the snarkVM library. If not, see <https://www.gnu.org/licenses/>.
 
-use core::borrow::Borrow;
-use std::{cmp::Ordering, fmt::Debug};
-
-use snarkvm_fields::{Field, FieldParameters, PrimeField, ToConstraintField};
-use snarkvm_r1cs::{errors::SynthesisError, Assignment, ConstraintSystem, LinearCombination};
-use snarkvm_utilities::bytes::ToBytes;
-
 use crate::{
     fields::FpGadget,
     utilities::{
@@ -33,11 +26,12 @@ use crate::{
         ToBytesGadget,
     },
 };
-use snarkvm_fields::{fp_parameters::FpParameters, traits::to_field_vec::ToConstraintField, Field, PrimeField};
+use snarkvm_fields::{Field, FieldParameters, PrimeField, ToConstraintField};
 use snarkvm_r1cs::{errors::SynthesisError, Assignment, ConstraintSystem, LinearCombination};
 use snarkvm_utilities::bytes::ToBytes;
 
-use std::fmt::Debug;
+use core::borrow::Borrow;
+use std::{cmp::Ordering, fmt::Debug};
 
 uint_impl!(UInt8, u8, 8);
 uint_impl!(UInt16, u16, 16);
