84d50d333a8971de3b47f3b98a96ebf47530d84b
raychu86 <raychu86@gmail.com>
2021-03-31 18:06:27 -0700

    Create dedicated proof gadget submodule

--- polycommit/src/marlin_pc/gadgets/proof/mod.rs
+++ polycommit/src/marlin_pc/gadgets/proof/mod.rs
@@ -14,16 +14,8 @@
 // You should have received a copy of the GNU General Public License
 // along with the snarkVM library. If not, see <https://www.gnu.org/licenses/>.
 
-/// Gadgets for Marlin-KZG10 commitments.
-pub mod commitment;
-pub(crate) use commitment::*;
+/// Gadget for a `BatchLCProof`.
+pub mod batch_lc_proof;
 
-/// Prepared verifier key gadget for the Marlin-KZG10 polynomial commitment scheme.
-pub mod prepared_verifier_key;
-
-/// Gadgets for Marlin-KZG10 proofs.
+/// Gadget for a Marlin-KZG10 proof.
 pub mod proof;
-
-/// Verifier key gadge for the Marlin-KZG10 polynomial commitment scheme.
-mod verifier_key;
-pub(crate) use verifier_key::*;
