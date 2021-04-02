2e9ccb913f9e09503513bf0bdffe99c3ec5ac934
Howard Wu <howard@aleo.org>
2021-04-02 14:19:20 -0700

    tarpaulin docker

--- .resources/kcov.sh
+++ .resources/kcov.sh
@@ -1,10 +1,12 @@
 cd /home/circleci/project/ &&
-echo "---START_LIST---"
-ls target/debug/deps
-echo "---END_LIST---"
-for file in target/debug/deps/snarkvm*-*[^\.d];
-  do
-    mkdir -p "target/cov/$(basename $file)";
-    echo "Processing target/cov/$(basename $file)"
-    /usr/local/bin/kcov --exclude-pattern=/.cargo,/usr/lib --verify "target/cov/$(basename $file)" "$file";
-  done
+#echo "---START_LIST---"
+#ls target/debug/deps
+#echo "---END_LIST---"
+#for file in target/debug/deps/snarkvm*-*[^\.d];
+#  do
+#    mkdir -p "target/cov/$(basename $file)";
+#    echo "Processing target/cov/$(basename $file)"
+#    /usr/local/bin/kcov --exclude-pattern=/.cargo,/usr/lib --verify "target/cov/$(basename $file)" "$file";
+#  done
+cargo install cargo-tarpaulin &&
+cargo tarpaulin -v --workspace --manifest-path ./Cargo.toml --ciserver circle-ci --out Xml
