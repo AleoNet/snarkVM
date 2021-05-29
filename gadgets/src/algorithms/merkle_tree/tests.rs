f6a130507ad02152919886ec3aa87e3e8d64cddf
Protryon <max.bruce12@gmail.com>
2021-05-28 21:35:14 -0700

    fix test

--- gadgets/src/algorithms/merkle_tree/tests.rs
+++ gadgets/src/algorithms/merkle_tree/tests.rs
@@ -62,7 +62,7 @@ fn generate_merkle_tree<P: MerkleParameters, F: PrimeField, HG: CRHGadget<P::H, 
     use_bad_root: bool,
 ) {
     let parameters = P::default();
-    let tree = MerkleTree::<P>::new(Arc::new(parameters.clone()), leaves.iter()).unwrap();
+    let tree = MerkleTree::<P>::new(Arc::new(parameters.clone()), &leaves[..]).unwrap();
     let root = tree.root();
     let mut satisfied = true;
     for (i, leaf) in leaves.iter().enumerate() {
