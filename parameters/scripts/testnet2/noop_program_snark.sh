fad0784c4ddf483bf1e0e3bf1f5dc5e42577b1fe
raychu86 <14917648+raychu86@users.noreply.github.com>
2021-06-14 17:48:26 -0700

    Implement parameter generation scripts for testnet 2

---
+++ parameters/scripts/testnet2/noop_program_snark.sh
@@ -0,0 +1,11 @@
+# This script will run the Noop program SNARK setup and move the resulting `.params`
+# and `.checksum` files to `params` folder under the `src` directory.
+# If the parameter size has changed, you will need to manually update these in each corresponding struct.
+
+cargo run --release --example testnet2_noop_program_snark
+
+mv noop_program_snark_pk.params ../../src/testnet2
+mv noop_program_snark_pk.checksum ../../src/testnet2
+
+mv noop_program_snark_vk.params ../../src/testnet2
+mv noop_program_snark_vk.checksum ../../src/testnet2
---
+++ parameters/scripts/testnet2/posw_snark.sh
@@ -0,0 +1,11 @@
+# This script will run the PoSW SNARK setup and move the resulting `.params`
+# and `.checksum` files to `params` folder under the `src` directory.
+# If the parameter size has changed, you will need to manually update these in each corresponding struct.
+
+cargo run --release --example testnet2_posw_snark
+
+mv posw_snark_pk*.params ../../src/testnet2
+mv posw_snark_pk.checksum ../../src/testnet2
+
+mv posw_snark_vk.params ../../src/testnet2
+mv posw_snark_vk.checksum ../../src/testnet2
