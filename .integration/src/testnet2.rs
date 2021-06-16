5a876e1cf4719a8bf25675282df638570e9f47a6
raychu86 <14917648+raychu86@users.noreply.github.com>
2021-06-15 21:10:44 -0700

    Implement testnet2 integration tests

--- .integration/src/testnet2.rs
+++ .integration/src/testnet2.rs
@@ -45,14 +45,17 @@ pub fn setup_or_load_parameters<R: Rng, S: Storage>(
     let merkle_tree_hash_parameters = <CommitmentMerkleParameters as MerkleParameters>::H::from(crh_parameters);
     let ledger_merkle_tree_parameters = Arc::new(From::from(merkle_tree_hash_parameters));
 
-    let parameters = match <InstantiatedDPC as DPCScheme<MerkleTreeLedger<S>>>::NetworkParameters::load(verify_only) {
-        Ok(parameters) => parameters,
-        Err(err) => {
-            println!("error - {}, re-running parameter Setup", err);
-            <InstantiatedDPC as DPCScheme<MerkleTreeLedger<S>>>::setup(&ledger_merkle_tree_parameters, rng)
-                .expect("DPC setup failed")
-        }
-    };
+    // let parameters = match <InstantiatedDPC as DPCScheme<MerkleTreeLedger<S>>>::NetworkParameters::load(verify_only) {
+    //     Ok(parameters) => parameters,
+    //     Err(err) => {
+    //         println!("error - {}, re-running parameter Setup", err);
+    //         <InstantiatedDPC as DPCScheme<MerkleTreeLedger<S>>>::setup(&ledger_merkle_tree_parameters, rng)
+    //             .expect("DPC setup failed")
+    //     }
+    // };
+
+    let parameters = <InstantiatedDPC as DPCScheme<MerkleTreeLedger<S>>>::setup(&ledger_merkle_tree_parameters, rng)
+        .expect("DPC setup failed");
 
     (ledger_merkle_tree_parameters, parameters)
 }
