712c26195738d239af2b378dab155f216f6b719d
emmorais <eduardo.morais@gmail.com>
2021-07-01 22:03:19 -0300

    Continue migration

--- dpc/src/account/account_address.rs
+++ dpc/src/account/account_address.rs
@@ -21,7 +21,9 @@ use crate::{
     AccountError,
     AccountPrivateKey,
     AccountViewKey,
-    AddressError
+    AddressError,
+    ViewKey,
+    Signature,
 };
 use snarkvm_algorithms::traits::EncryptionScheme;
 use snarkvm_utilities::{FromBytes, ToBytes};
@@ -59,6 +61,32 @@ impl Address<Components> {
             &private_key,
         )?;
         Ok(Self { address })
+    }
+
+    pub fn from_view_key(view_key: &ViewKey) -> Result<Self, AddressError> {
+        let parameters = SystemParameters::<Components>::load()?;
+        let address = AccountAddress::<Components>::from_view_key(&parameters.account_encryption, &view_key.view_key)?;
+        Ok(Self { address })
+    }
+
+    /// Verify a signature signed by the view key
+    /// Returns `true` if the signature is verified correctly. Otherwise, returns `false`.
+    pub fn verify(&self, message: &[u8], signature: &Signature) -> Result<bool, AddressError> {
+        let parameters = SystemParameters::<Components>::load()?;
+
+        Ok(parameters
+            .account_encryption
+            .verify(&self.address.encryption_key, message, &signature.0)?)
+    }
+}
+
+impl FromStr for Address<Components> {
+    type Err = AddressError;
+
+    fn from_str(address: &str) -> Result<Self, Self::Err> {
+        Ok(Self {
+            address: AccountAddress::<Components>::from_str(address)?,
+        })
     }
 }
 
