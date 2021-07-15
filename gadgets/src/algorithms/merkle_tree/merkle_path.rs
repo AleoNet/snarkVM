477498b7b5c2a6f0788f8962bf49235d72315eab
Howard Wu <9260812+howardwu@users.noreply.github.com>
2021-07-14 19:07:47 -0700

    Update gadgets/src/algorithms/merkle_tree/merkle_path.rs
    Co-authored-by: ljedrz <ljedrz@users.noreply.github.com>

--- gadgets/src/algorithms/merkle_tree/merkle_path.rs
+++ gadgets/src/algorithms/merkle_tree/merkle_path.rs
@@ -169,7 +169,7 @@ where
         let merkle_path = value_gen()?.borrow().clone();
 
         let mut traversal = vec![];
-        for (i, position) in pos_list.iter().enumerate() {
+        for (i, position) in merkle_path.position_list().enumerate() {
             traversal.push(Boolean::alloc(cs.ns(|| format!("alloc_position_{}", i)), || {
                 Ok(position)
             })?);
