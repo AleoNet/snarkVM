ea7832ce77a66b0021d6722bc86ea05ab978bcba
raychu86 <14917648+raychu86@users.noreply.github.com>
2021-07-15 18:19:11 -0700

    Add tests for group gadget is_eq

--- gadgets/src/curves/edwards_bls12.rs
+++ gadgets/src/curves/edwards_bls12.rs
@@ -28,7 +28,8 @@ mod test {
             templates::twisted_edwards::test::{edwards_constraint_costs, edwards_test},
             tests_group::group_test,
         },
-        traits::alloc::AllocGadget,
+        traits::{alloc::AllocGadget, eq::EqGadget},
+        Boolean,
     };
     use snarkvm_curves::edwards_bls12::{EdwardsParameters, EdwardsProjective, Fq};
     use snarkvm_r1cs::{ConstraintSystem, TestConstraintSystem};
@@ -57,5 +58,30 @@ mod test {
         let a = EdwardsBls12Gadget::alloc(&mut cs.ns(|| "generate_a"), || Ok(a)).unwrap();
         let b = EdwardsBls12Gadget::alloc(&mut cs.ns(|| "generate_b"), || Ok(b)).unwrap();
         group_test::<_, EdwardsProjective, _, _>(&mut cs.ns(|| "GroupTest(a, b)"), a, b);
+    }
+
+    #[test]
+    fn edwards_bls12_group_gadgets_is_eq_test() {
+        let mut cs = TestConstraintSystem::<Fq>::new();
+
+        let a: EdwardsProjective = rand::random();
+        let b: EdwardsProjective = a.clone();
+        let c: EdwardsProjective = rand::random();
+
+        let a = EdwardsBls12Gadget::alloc(&mut cs.ns(|| "generate_a"), || Ok(a)).unwrap();
+        let b = EdwardsBls12Gadget::alloc(&mut cs.ns(|| "generate_b"), || Ok(b)).unwrap();
+        let c = EdwardsBls12Gadget::alloc(&mut cs.ns(|| "generate_c"), || Ok(c)).unwrap();
+
+        let a_is_eq_b = a.is_eq(cs.ns(|| "a_is_eq_b"), &b).unwrap();
+        let a_is_eq_c = a.is_eq(cs.ns(|| "a_is_eq_c"), &c).unwrap();
+
+        a_is_eq_b
+            .enforce_equal(cs.ns(|| " a_is_eq_b is true"), &Boolean::constant(true))
+            .unwrap();
+        a_is_eq_c
+            .enforce_equal(cs.ns(|| " a_is_eq_c is false"), &Boolean::constant(false))
+            .unwrap();
+
+        assert!(cs.is_satisfied());
     }
 }
