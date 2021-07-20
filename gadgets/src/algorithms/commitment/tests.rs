69aad159fcd78e74072bcfc6371073bea971ff34
howardwu <howard@aleo.org>
2021-07-20 00:48:15 -0700

    Revamp commitment gadget tests, add compressed commitment gadget tests

--- gadgets/src/algorithms/commitment/tests.rs
+++ gadgets/src/algorithms/commitment/tests.rs
@@ -21,8 +21,14 @@ use crate::{
     traits::{algorithms::CommitmentGadget, alloc::AllocGadget, FieldGadget},
 };
 use snarkvm_algorithms::{
-    commitment::{Blake2sCommitment, BoweHopwoodPedersenCommitment, PedersenCommitment},
-    traits::CommitmentScheme,
+    commitment::{
+        Blake2sCommitment,
+        BoweHopwoodPedersenCommitment,
+        BoweHopwoodPedersenCompressedCommitment,
+        PedersenCommitment,
+        PedersenCompressedCommitment,
+    },
+    CommitmentScheme,
 };
 use snarkvm_curves::edwards_bls12::{EdwardsProjective, Fq, Fr};
 use snarkvm_r1cs::{ConstraintSystem, TestConstraintSystem};
@@ -30,12 +36,100 @@ use snarkvm_utilities::rand::UniformRand;
 
 use rand::{thread_rng, Rng};
 
+const ITERATIONS: usize = 1000;
+
+fn native_and_gadget_equivalence_test<Native: CommitmentScheme, Gadget: CommitmentGadget<Native, Fq>>() -> (
+    <Native as CommitmentScheme>::Output,
+    <Gadget as CommitmentGadget<Native, Fq>>::OutputGadget,
+) {
+    let rng = &mut thread_rng();
+
+    // Generate the input message and randomness.
+    let input: [u8; 32] = rng.gen();
+    let randomness = <Native as CommitmentScheme>::Randomness::rand(rng);
+
+    // Compute the native commitment.
+    let commitment_scheme = Native::setup("commitment_test");
+    let native_output = commitment_scheme.commit(&input, &randomness).unwrap();
+
+    // Compute the gadget commitment.
+    let mut cs = TestConstraintSystem::<Fq>::new();
+    let mut input_bytes = vec![];
+    for (byte_i, input_byte) in input.iter().enumerate() {
+        let cs = cs.ns(|| format!("input_byte_gadget_{}", byte_i));
+        input_bytes.push(UInt8::alloc(cs, || Ok(*input_byte)).unwrap());
+    }
+    let randomness_gadget =
+        <Gadget as CommitmentGadget<Native, Fq>>::RandomnessGadget::alloc(&mut cs.ns(|| "randomness_gadget"), || {
+            Ok(&randomness)
+        })
+        .unwrap();
+    let commitment_gadget = Gadget::alloc(&mut cs.ns(|| "parameters_gadget"), || Ok(&commitment_scheme)).unwrap();
+    let gadget_output = commitment_gadget
+        .check_commitment_gadget(&mut cs.ns(|| "commitment_gadget"), &input_bytes, &randomness_gadget)
+        .unwrap();
+    assert!(cs.is_satisfied());
+
+    (native_output, gadget_output)
+}
+
+#[test]
+fn bhp_commitment_gadget_test() {
+    type TestCommitment = BoweHopwoodPedersenCommitment<EdwardsProjective, 32, 48>;
+    type TestCommitmentGadget = BoweHopwoodPedersenCommitmentGadget<EdwardsProjective, Fq, EdwardsBls12Gadget, 32, 48>;
+
+    for _ in 0..ITERATIONS {
+        let (native_output, gadget_output) =
+            native_and_gadget_equivalence_test::<TestCommitment, TestCommitmentGadget>();
+        assert_eq!(native_output.x, gadget_output.x.get_value().unwrap());
+        assert_eq!(native_output.y, gadget_output.y.get_value().unwrap());
+    }
+}
+
+#[test]
+fn bhp_compressed_commitment_gadget_test() {
+    type TestCommitment = BoweHopwoodPedersenCompressedCommitment<EdwardsProjective, 32, 48>;
+    type TestCommitmentGadget =
+        BoweHopwoodPedersenCompressedCommitmentGadget<EdwardsProjective, Fq, EdwardsBls12Gadget, 32, 48>;
+
+    for _ in 0..ITERATIONS {
+        let (native_output, gadget_output) =
+            native_and_gadget_equivalence_test::<TestCommitment, TestCommitmentGadget>();
+        assert_eq!(native_output, gadget_output.get_value().unwrap());
+    }
+}
+
+#[test]
+fn pedersen_commitment_gadget_test() {
+    type TestCommitment = PedersenCommitment<EdwardsProjective, 8, 32>;
+    type TestCommitmentGadget = PedersenCommitmentGadget<EdwardsProjective, Fq, EdwardsBls12Gadget, 8, 32>;
+
+    for _ in 0..ITERATIONS {
+        let (native_output, gadget_output) =
+            native_and_gadget_equivalence_test::<TestCommitment, TestCommitmentGadget>();
+        assert_eq!(native_output.x, gadget_output.x.get_value().unwrap());
+        assert_eq!(native_output.y, gadget_output.y.get_value().unwrap());
+    }
+}
+
+#[test]
+fn pedersen_compressed_commitment_gadget_test() {
+    type TestCommitment = PedersenCompressedCommitment<EdwardsProjective, 8, 32>;
+    type TestCommitmentGadget = PedersenCompressedCommitmentGadget<EdwardsProjective, Fq, EdwardsBls12Gadget, 8, 32>;
+
+    for _ in 0..ITERATIONS {
+        let (native_output, gadget_output) =
+            native_and_gadget_equivalence_test::<TestCommitment, TestCommitmentGadget>();
+        assert_eq!(native_output, gadget_output.get_value().unwrap());
+    }
+}
+
 #[test]
 fn blake2s_commitment_gadget_test() {
     let mut cs = TestConstraintSystem::<Fr>::new();
     let rng = &mut thread_rng();
 
-    let input = [1u8; 32];
+    let input: [u8; 32] = rng.gen();
 
     let mut randomness = [0u8; 32];
     rng.fill(&mut randomness);
@@ -65,87 +159,5 @@ fn blake2s_commitment_gadget_test() {
     for (i, nr) in native_result.iter().enumerate() {
         assert_eq!(*nr, gadget_result.0[i].value.unwrap());
     }
-    assert!(cs.is_satisfied());
-}
-
-#[test]
-fn pedersen_commitment_gadget_test() {
-    let mut cs = TestConstraintSystem::<Fq>::new();
-
-    const NUM_WINDOWS: usize = 8;
-    const WINDOW_SIZE: usize = 4;
-
-    type TestCommitment = PedersenCommitment<EdwardsProjective, NUM_WINDOWS, WINDOW_SIZE>;
-    type TestCommitmentGadget =
-        PedersenCommitmentGadget<EdwardsProjective, Fq, EdwardsBls12Gadget, NUM_WINDOWS, WINDOW_SIZE>;
-
-    let rng = &mut thread_rng();
-
-    let input = [1u8; 4];
-    let randomness = Fr::rand(rng);
-    let commitment = TestCommitment::setup("pedersen_commitment_gadget_test");
-    let native_output = commitment.commit(&input, &randomness).unwrap();
-
-    let mut input_bytes = vec![];
-    for (byte_i, input_byte) in input.iter().enumerate() {
-        let cs = cs.ns(|| format!("input_byte_gadget_{}", byte_i));
-        input_bytes.push(UInt8::alloc(cs, || Ok(*input_byte)).unwrap());
-    }
-
-    let randomness_gadget = <TestCommitmentGadget as CommitmentGadget<TestCommitment, Fq>>::RandomnessGadget::alloc(
-        &mut cs.ns(|| "randomness_gadget"),
-        || Ok(&randomness),
-    )
-    .unwrap();
-    let commitment_gadget =
-        TestCommitmentGadget::alloc(&mut cs.ns(|| "parameters_gadget"), || Ok(&commitment)).unwrap();
-    let output_gadget = commitment_gadget
-        .check_commitment_gadget(&mut cs.ns(|| "commitment_gadget"), &input_bytes, &randomness_gadget)
-        .unwrap();
-
-    let native_output = native_output;
-    assert_eq!(native_output.x, output_gadget.x.get_value().unwrap());
-    assert_eq!(native_output.y, output_gadget.y.get_value().unwrap());
-    assert!(cs.is_satisfied());
-}
-
-#[test]
-fn bhp_commitment_gadget_test() {
-    let mut cs = TestConstraintSystem::<Fq>::new();
-
-    const NUM_WINDOWS: usize = 32;
-    const WINDOW_SIZE: usize = 48;
-
-    type TestCommitment = BoweHopwoodPedersenCommitment<EdwardsProjective, NUM_WINDOWS, WINDOW_SIZE>;
-    type TestCommitmentGadget =
-        BoweHopwoodPedersenCommitmentGadget<EdwardsProjective, Fq, EdwardsBls12Gadget, NUM_WINDOWS, WINDOW_SIZE>;
-
-    let rng = &mut thread_rng();
-
-    let input = [5u8; 32];
-    let randomness = Fr::rand(rng);
-    let commitment = TestCommitment::setup("bhp_commitment_gadget_test");
-    let native_output = commitment.commit(&input, &randomness).unwrap();
-
-    let mut input_bytes = vec![];
-    for (byte_i, input_byte) in input.iter().enumerate() {
-        let cs = cs.ns(|| format!("input_byte_gadget_{}", byte_i));
-        input_bytes.push(UInt8::alloc(cs, || Ok(*input_byte)).unwrap());
-    }
-
-    let randomness_gadget = <TestCommitmentGadget as CommitmentGadget<TestCommitment, Fq>>::RandomnessGadget::alloc(
-        &mut cs.ns(|| "randomness_gadget"),
-        || Ok(&randomness),
-    )
-    .unwrap();
-    let commitment_gadget =
-        TestCommitmentGadget::alloc(&mut cs.ns(|| "parameters_gadget"), || Ok(&commitment)).unwrap();
-    let output_gadget = commitment_gadget
-        .check_commitment_gadget(&mut cs.ns(|| "commitment_gadget"), &input_bytes, &randomness_gadget)
-        .unwrap();
-
-    let native_output = native_output;
-    assert_eq!(native_output.x, output_gadget.x.get_value().unwrap());
-    assert_eq!(native_output.y, output_gadget.y.get_value().unwrap());
     assert!(cs.is_satisfied());
 }
