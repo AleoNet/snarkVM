d3ccc061575159c7e20fe8b15d68c2c8c1f506f3
howardwu <howard@aleo.org>
2021-07-24 18:57:14 -0700

    Fix block height convention

--- .integration/tests/dpc_testnet2.rs
+++ .integration/tests/dpc_testnet2.rs
@@ -210,7 +210,7 @@ fn dpc_testnet2_integration_test() {
     let block = Block { header, transactions };
 
     ledger.insert_and_commit(&block).unwrap();
-    assert_eq!(ledger.block_height(), 2);
+    assert_eq!(ledger.block_height(), 1);
 }
 
 #[test]
