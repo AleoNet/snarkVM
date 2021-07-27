484047b045599f3f39046f2be57cd29c7c8f786e
weikeng <w.k@berkeley.edu>
2021-07-26 18:56:45 -0700

    progress

--- dpc/src/traits/encoded.rs
+++ dpc/src/traits/encoded.rs
@@ -22,32 +22,11 @@ use snarkvm_curves::{
 use snarkvm_fields::{FieldParameters, PrimeField};
 
 pub trait EncodedRecordScheme: Sized {
-    /// The group is composed of base field elements in `Self::InnerField`.
-    type Group: ProjectiveCurve;
-    /// The inner field is equivalent to the base field in `Self::Group`.
-    type InnerField: PrimeField;
-    /// The outer field is unrelated to `Self::Group` and `Self::InnerField`.
-    type OuterField: PrimeField;
-    type Parameters: MontgomeryParameters + TwistedEdwardsParameters;
+    /// The field that the encoded record is over.
+    type Field: PrimeField;
     type Record: RecordScheme;
     type DecodedRecord;
 
-    /// This is the bitsize of the scalar field modulus in `Self::Group`.
-    const SCALAR_FIELD_BITSIZE: usize =
-        <<Self::Group as Group>::ScalarField as PrimeField>::Parameters::MODULUS_BITS as usize;
-    /// This is the bitsize of the base field modulus in `Self::Group` and equivalent to `Self::InnerField`.
-    const INNER_FIELD_BITSIZE: usize = <Self::InnerField as PrimeField>::Parameters::MODULUS_BITS as usize;
-    /// This is the bitsize of the field modulus in `Self::OuterField`.
-    const OUTER_FIELD_BITSIZE: usize = <Self::OuterField as PrimeField>::Parameters::MODULUS_BITS as usize;
-
-    /// This is the bitsize of each data ciphertext element serialized by this struct.
-    /// Represents a standard unit for packing bits into data elements for storage.
-    const DATA_ELEMENT_BITSIZE: usize = Self::INNER_FIELD_BITSIZE - 1;
-    /// This is the bitsize of each payload ciphertext element serialized by this struct.
-    /// Represents a standard unit for packing the payload into data elements for storage.
-    const PAYLOAD_ELEMENT_BITSIZE: usize = Self::DATA_ELEMENT_BITSIZE - 1;
-
     fn encode(record: &Self::Record) -> Result<Self, DPCError>;
-
     fn decode(&self) -> Result<Self::DecodedRecord, DPCError>;
 }
