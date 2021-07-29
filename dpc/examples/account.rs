be491aefe7f824ef34ed2458e051c3e2201e4a90
howardwu <howard@aleo.org>
2021-07-29 03:31:30 -0700

    Fix dpc example

--- dpc/examples/account.rs
+++ dpc/examples/account.rs
@@ -14,7 +14,7 @@
 // You should have received a copy of the GNU General Public License
 // along with the snarkVM library. If not, see <https://www.gnu.org/licenses/>.
 
-use snarkvm_dpc::{testnet2::parameters::Testnet2Parameters, Account, AccountScheme};
+use snarkvm_dpc::{testnet2::Testnet2Parameters, Account, AccountScheme};
 
 use rand::thread_rng;
 
