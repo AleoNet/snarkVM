0e059344d581b2f9fb644f614c79a6f62fe6cada
raychu86 <14917648+raychu86@users.noreply.github.com>
2021-09-17 16:35:35 -0700

    Implement tests for merkle trie inserts

--- algorithms/src/merkle_trie/tests.rs
+++ algorithms/src/merkle_trie/tests.rs
@@ -13,3 +13,48 @@
 
 // You should have received a copy of the GNU General Public License
 // along with the snarkVM library. If not, see <https://www.gnu.org/licenses/>.
+
+#[cfg(test)]
+mod tests {
+    use crate::{crypto_hash::PoseidonCryptoHash, merkle_trie::MerkleTrie, traits::CRH};
+
+    use snarkvm_curves::bls12_377::Fr;
+
+    use std::sync::Arc;
+
+    type H = PoseidonCryptoHash<Fr, 4, false>;
+
+    #[test]
+    fn test_basic_trie() {
+        let crh = Arc::new(H::setup("TEST_MERKLE_TRIE_CRH"));
+
+        let mut tree_1 = MerkleTrie::<_, u8>::new(crh.clone()).unwrap();
+        tree_1.insert(b"a", Some(1)).unwrap();
+        tree_1.insert(b"ab", Some(2)).unwrap();
+        tree_1.insert(b"abc", Some(3)).unwrap();
+
+        let mut tree_2 = MerkleTrie::<_, u8>::new(crh.clone()).unwrap();
+        tree_2.insert(b"a", Some(1)).unwrap();
+        tree_2.insert(b"ab", Some(2)).unwrap();
+        tree_2.insert(b"abc", Some(3)).unwrap();
+
+        assert_eq!(&tree_1.root(), &tree_2.root());
+    }
+
+    #[test]
+    fn test_basic_mismatched_trie() {
+        let crh = Arc::new(H::setup("TEST_MERKLE_TRIE_CRH"));
+
+        let mut tree_1 = MerkleTrie::<_, u8>::new(crh.clone()).unwrap();
+        tree_1.insert(b"a", Some(1)).unwrap();
+        tree_1.insert(b"ab", Some(2)).unwrap();
+        tree_1.insert(b"abc", Some(3)).unwrap();
+
+        let mut tree_2 = MerkleTrie::<_, u8>::new(crh.clone()).unwrap();
+        tree_2.insert(b"a", Some(1)).unwrap();
+        tree_2.insert(b"aa", Some(2)).unwrap();
+        tree_2.insert(b"abb", Some(3)).unwrap();
+
+        assert_ne!(&tree_1.root(), &tree_2.root());
+    }
+}
