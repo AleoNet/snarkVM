3efbac8a5b315678f0e4b1aa23944cd250465b44
raychu86 <14917648+raychu86@users.noreply.github.com>
2021-09-17 17:25:03 -0700

    Implement test for trie get

--- algorithms/src/merkle_trie/tests.rs
+++ algorithms/src/merkle_trie/tests.rs
@@ -24,19 +24,24 @@ mod tests {
 
     type H = PoseidonCryptoHash<Fr, 4, false>;
 
+    const VALUE_PAIR_1: (&'static [u8; 1], u8) = (b"a", 1u8);
+    const VALUE_PAIR_2: (&'static [u8; 2], u8) = (b"ab", 2u8);
+    const VALUE_PAIR_3: (&'static [u8; 3], u8) = (b"abc", 3u8);
+    const VALUE_PAIR_4: (&'static [u8; 4], u8) = (b"abcd", 4u8);
+
     #[test]
     fn test_basic_trie() {
         let crh = Arc::new(H::setup("TEST_MERKLE_TRIE_CRH"));
 
         let mut tree_1 = MerkleTrie::<_, u8>::new(crh.clone()).unwrap();
-        tree_1.insert(b"a", Some(1)).unwrap();
-        tree_1.insert(b"ab", Some(2)).unwrap();
-        tree_1.insert(b"abc", Some(3)).unwrap();
+        tree_1.insert(VALUE_PAIR_1.0, Some(VALUE_PAIR_1.1)).unwrap();
+        tree_1.insert(VALUE_PAIR_2.0, Some(VALUE_PAIR_2.1)).unwrap();
+        tree_1.insert(VALUE_PAIR_3.0, Some(VALUE_PAIR_3.1)).unwrap();
 
         let mut tree_2 = MerkleTrie::<_, u8>::new(crh.clone()).unwrap();
-        tree_2.insert(b"a", Some(1)).unwrap();
-        tree_2.insert(b"ab", Some(2)).unwrap();
-        tree_2.insert(b"abc", Some(3)).unwrap();
+        tree_2.insert(VALUE_PAIR_1.0, Some(VALUE_PAIR_1.1)).unwrap();
+        tree_2.insert(VALUE_PAIR_2.0, Some(VALUE_PAIR_2.1)).unwrap();
+        tree_2.insert(VALUE_PAIR_3.0, Some(VALUE_PAIR_3.1)).unwrap();
 
         assert_eq!(&tree_1.root(), &tree_2.root());
     }
@@ -46,15 +51,36 @@ mod tests {
         let crh = Arc::new(H::setup("TEST_MERKLE_TRIE_CRH"));
 
         let mut tree_1 = MerkleTrie::<_, u8>::new(crh.clone()).unwrap();
-        tree_1.insert(b"a", Some(1)).unwrap();
-        tree_1.insert(b"ab", Some(2)).unwrap();
-        tree_1.insert(b"abc", Some(3)).unwrap();
+        tree_1.insert(VALUE_PAIR_1.0, Some(VALUE_PAIR_1.1)).unwrap();
+        tree_1.insert(VALUE_PAIR_2.0, Some(VALUE_PAIR_2.1)).unwrap();
+        tree_1.insert(VALUE_PAIR_3.0, Some(VALUE_PAIR_3.1)).unwrap();
 
         let mut tree_2 = MerkleTrie::<_, u8>::new(crh.clone()).unwrap();
-        tree_2.insert(b"a", Some(1)).unwrap();
-        tree_2.insert(b"aa", Some(2)).unwrap();
-        tree_2.insert(b"abb", Some(3)).unwrap();
+        tree_2.insert(VALUE_PAIR_1.0, Some(VALUE_PAIR_1.1)).unwrap();
+        tree_2.insert(VALUE_PAIR_2.0, Some(VALUE_PAIR_2.1)).unwrap();
+        tree_2.insert(VALUE_PAIR_4.0, Some(VALUE_PAIR_4.1)).unwrap();
 
         assert_ne!(&tree_1.root(), &tree_2.root());
+    }
+
+    #[test]
+    fn test_trie_get() {
+        let crh = Arc::new(H::setup("TEST_MERKLE_TRIE_CRH"));
+
+        let mut tree_1 = MerkleTrie::<_, u8>::new(crh.clone()).unwrap();
+
+        tree_1.insert(VALUE_PAIR_1.0, Some(VALUE_PAIR_1.1)).unwrap();
+        tree_1.insert(VALUE_PAIR_2.0, Some(VALUE_PAIR_2.1)).unwrap();
+        tree_1.insert(VALUE_PAIR_3.0, Some(VALUE_PAIR_3.1)).unwrap();
+
+        let value_1 = tree_1.get(VALUE_PAIR_1.0);
+        let value_2 = tree_1.get(VALUE_PAIR_2.0);
+        let value_3 = tree_1.get(VALUE_PAIR_3.0);
+        let value_4 = tree_1.get(VALUE_PAIR_4.0);
+
+        assert_eq!(value_1, Some(&VALUE_PAIR_1.1));
+        assert_eq!(value_2, Some(&VALUE_PAIR_2.1));
+        assert_eq!(value_3, Some(&VALUE_PAIR_3.1));
+        assert_eq!(value_4, None);
     }
 }
