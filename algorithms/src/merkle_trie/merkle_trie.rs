ad52d283dd91e319186475b853ad1453eb44acae
raychu86 <14917648+raychu86@users.noreply.github.com>
2021-09-21 10:55:07 -0700

    Implement proof generation for merkle trie

--- algorithms/src/merkle_trie/merkle_trie.rs
+++ algorithms/src/merkle_trie/merkle_trie.rs
@@ -14,28 +14,28 @@
 // You should have received a copy of the GNU General Public License
 // along with the snarkVM library. If not, see <https://www.gnu.org/licenses/>.
 
-use crate::{errors::MerkleError, traits::CRH};
+use crate::{errors::MerkleTrieError, merkle_trie::MerkleTriePath, traits::CRH};
 use snarkvm_utilities::{to_bytes_le, ToBytes};
 
-use std::{collections::BTreeMap, fmt::Debug, sync::Arc};
+use std::{collections::BTreeMap, sync::Arc};
 
-#[derive(Default)]
-pub struct MerkleTrie<P: CRH, T: Debug> {
+#[derive(Default, Clone)]
+pub struct MerkleTrie<P: CRH, T: ToBytes + PartialEq + Clone> {
     /// The CRH used to create the root hash.
     parameters: Arc<P>,
     /// The root hash of the Merkle trie.
     root: [u8; 32],
     /// The key of the current Merkle trie.
-    key: Vec<u8>,
+    pub key: Vec<u8>, // TODO (raychu86): Enforce a max depth size.
     /// The value existing at the current Merkle trie node.
-    value: Option<T>,
+    pub value: Option<T>,
     /// Any child Merkle tries. Currently has u8::MAX potential branches. // TODO (raychu86): Allow for generic branch sizes.
-    children: BTreeMap<u8, MerkleTrie<P, T>>, // TODO (raychu86): Remove the current duplication of parameters.
+    pub children: BTreeMap<u8, MerkleTrie<P, T>>, // TODO (raychu86): Remove the current duplication of parameters.
 }
 
-impl<P: CRH, T: ToBytes + Debug> MerkleTrie<P, T> {
+impl<P: CRH, T: ToBytes + PartialEq + Clone> MerkleTrie<P, T> {
     /// Create a new Merkle trie.
-    pub fn new(parameters: Arc<P>) -> Result<Self, MerkleError> {
+    pub fn new(parameters: Arc<P>) -> Result<Self, MerkleTrieError> {
         let merkle_trie = Self {
             parameters,
             root: [0u8; 32],
@@ -53,7 +53,7 @@ impl<P: CRH, T: ToBytes + Debug> MerkleTrie<P, T> {
     }
 
     /// Insert a (key, value) pair into the Merkle trie.
-    pub fn insert(&mut self, key: &[u8], value: Option<T>) -> Result<(), MerkleError> {
+    pub fn insert(&mut self, key: &[u8], value: Option<T>) -> Result<(), MerkleTrieError> {
         // If the trie is currently empty, set the key value pair.
         if self.is_empty() {
             self.key = key.to_vec();
@@ -73,7 +73,7 @@ impl<P: CRH, T: ToBytes + Debug> MerkleTrie<P, T> {
             if prefix_length == key.len() {
                 if self.value.is_some() {
                     // TODO (raychu86): Update the Error type.
-                    return Err(MerkleError::Message("Key already exists".to_string()));
+                    return Err(MerkleTrieError::Message("Key already exists".to_string()));
                 }
 
                 self.value = value;
@@ -122,6 +122,7 @@ impl<P: CRH, T: ToBytes + Debug> MerkleTrie<P, T> {
     pub fn get(&self, key: &[u8]) -> Option<&T> {
         // If the key is the root, return the value.
         if self.key == key {
+            println!("self.root(): {:?}", self.root());
             return self.value.as_ref();
         } else if key.starts_with(&self.key) {
             // If the given key starts with the root key.
@@ -142,18 +143,88 @@ impl<P: CRH, T: ToBytes + Debug> MerkleTrie<P, T> {
         unimplemented!()
     }
 
+    pub fn generate_proof(&self, key: &[u8], value: &T) -> Result<MerkleTriePath<P, T>, MerkleTrieError> {
+        let prove_time = start_timer!(|| "MerkleTrie::generate_proof");
+
+        // Check that the key pair exists.
+        if let Some(expected_value) = self.get(key) {
+            if value != expected_value {
+                return Err(MerkleTrieError::IncorrectKey(key.to_vec()));
+            }
+        } else {
+            return Err(MerkleTrieError::MissingLeaf(key.to_vec()));
+        }
+
+        let mut path = vec![];
+        let mut parents = vec![(self.key.clone(), self.value.clone())];
+        let mut traversal = vec![];
+
+        let mut temp_key = self.key.clone();
+        let mut temp_children = &self.children;
+
+        let mut expected_key = key.clone();
+
+        let mut found = false;
+        while !found {
+            if temp_key == expected_key {
+                found = true;
+            } else {
+                println!("temp_key: {:?}", temp_key);
+                println!("expected_key: {:?}", expected_key);
+                // If the given key starts with the root key.
+                let suffix = &expected_key[temp_key.len()..];
+
+                let index = temp_children.keys().position(|&r| r == suffix[0]).unwrap();
+                let mut siblings: Vec<[u8; 32]> = temp_children.iter().map(|(_x, trie)| trie.root().clone()).collect();
+                siblings.remove(index);
+
+                traversal.push(index);
+                path.push(siblings);
+
+                if let Some(child_node) = temp_children.get(&suffix[0]) {
+                    parents.push((child_node.key.clone(), child_node.value.clone()));
+                    println!("child_node: {:?}", child_node.root());
+                    println!("child_node key: {:?}", &child_node.key);
+
+                    temp_children = &child_node.children;
+                    temp_key = child_node.key.clone();
+                }
+
+                expected_key = suffix;
+            }
+        }
+
+        end_timer!(prove_time);
+
+        // Do not include the provided key/value pair in the list of parents;
+        parents.pop();
+
+        path.reverse();
+        parents.reverse();
+        traversal.reverse();
+
+        let proof = MerkleTriePath {
+            parameters: self.parameters.clone(),
+            parents,
+            path,
+            traversal,
+        };
+
+        Ok(proof)
+    }
+
     /// Returns the root hash of the Merkle trie.
     #[inline]
     pub fn root(&self) -> &[u8; 32] {
         &self.root
     }
 
-    // pub fn generate_proof(&self, _key: &[u8], _value: T) -> Result<MerkleTriePath<P, T>, MerkleError> {
+    // pub fn generate_proof(&self, _key: &[u8], _value: T) -> Result<MerkleTriePath<P, T>, MerkleTrieError> {
     //     unimplemented!()
     // }
 
     /// Insert a (key, value) pair into the current Merkle trie node.
-    fn insert_child(&mut self, suffix: &Vec<u8>, value: T) -> Result<(), MerkleError> {
+    fn insert_child(&mut self, suffix: &Vec<u8>, value: T) -> Result<(), MerkleTrieError> {
         // Check the first element of the suffix.
         match self.children.get_mut(&suffix[0]) {
             None => {
@@ -176,29 +247,17 @@ impl<P: CRH, T: ToBytes + Debug> MerkleTrie<P, T> {
     }
 
     /// Update the root of the Merkle trie using its current children.
-    fn update_root(&mut self) -> Result<(), MerkleError> {
+    fn update_root(&mut self) -> Result<(), MerkleTrieError> {
         if self.is_empty() {
             self.root = [0; 32];
         } else {
-            // TODO (raychu86): Determine if hasing the key is required.
-
-            // Add the current node's key and value to the hash input.
-            let mut input = self.key.to_vec();
-            if let Some(value) = &self.value {
-                let value_bytes = to_bytes_le![value]?;
-                input.extend(value_bytes);
-            }
-
             // Add the children roots to the hash input.
+            let mut child_roots = vec![];
             for child in self.children.values() {
-                input.extend(child.root());
+                child_roots.push(child.root());
             }
 
-            // Hash the input
-            let hash = self.parameters.hash(&input)?;
-            let hash_bytes = to_bytes_le![hash]?;
-            let mut root = [0u8; 32];
-            root.copy_from_slice(&hash_bytes);
+            let root = calculate_root(&self.parameters, &self.key, &self.value, &child_roots)?;
 
             // Update the new root.
             self.root = root;
@@ -206,6 +265,51 @@ impl<P: CRH, T: ToBytes + Debug> MerkleTrie<P, T> {
 
         Ok(())
     }
+}
+
+pub fn calculate_root<P: CRH, T: ToBytes>(
+    parameters: &Arc<P>,
+    key: &[u8],
+    value: &Option<T>,
+    child_roots: &Vec<&[u8; 32]>,
+) -> Result<[u8; 32], MerkleTrieError> {
+    // Add the current node's key and value to the hash input.
+    let mut input = hash_leaf(&parameters, &key, &value)?.to_vec();
+
+    // Add the children roots to the hash input.
+    for child in child_roots {
+        input.extend(*child);
+    }
+
+    // Hash the input
+    let hash = parameters.hash(&input)?;
+    let hash_bytes = to_bytes_le![hash]?;
+    let mut root = [0u8; 32];
+    root.copy_from_slice(&hash_bytes);
+
+    Ok(root)
+}
+
+/// Returns the hash of a given key value pair.
+pub fn hash_leaf<P: CRH, T: ToBytes>(
+    parameters: &Arc<P>,
+    key: &[u8],
+    value: &Option<T>,
+) -> Result<[u8; 32], MerkleTrieError> {
+    let mut input = vec![];
+    if let Some(value) = &value {
+        let value_bytes = to_bytes_le![value]?;
+        input.extend(value_bytes);
+    }
+
+    // Hash the input
+    let hash = parameters.hash(&input)?;
+    let hash_bytes = to_bytes_le![hash]?;
+
+    let mut root = [0u8; 32];
+    root.copy_from_slice(&hash_bytes);
+
+    Ok(root)
 }
 
 /// Number of prefix elements the two keys have in common.
