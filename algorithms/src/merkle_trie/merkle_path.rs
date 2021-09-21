c8768d99f9d977f80e32579d9406d7e54cc24bd4
raychu86 <14917648+raychu86@users.noreply.github.com>
2021-09-21 10:55:21 -0700

    Implement merkle trie path

--- algorithms/src/merkle_trie/merkle_path.rs
+++ algorithms/src/merkle_trie/merkle_path.rs
@@ -13,3 +13,54 @@
 
 // You should have received a copy of the GNU General Public License
 // along with the snarkVM library. If not, see <https://www.gnu.org/licenses/>.
+
+use crate::{
+    errors::MerkleTrieError,
+    merkle_trie::{calculate_root, hash_leaf},
+    traits::CRH,
+};
+use snarkvm_utilities::ToBytes;
+
+use itertools::Itertools;
+use std::sync::Arc;
+
+pub struct MerkleTriePath<P: CRH, T: ToBytes> {
+    pub parameters: Arc<P>,
+    /// A Vector of existing sibling children from leaf to root.
+    /// (Does NOT including the parents of the leaf being proven)
+    pub path: Vec<Vec<[u8; 32]>>, // Vector of branch roots
+    pub parents: Vec<(Vec<u8>, Option<T>)>, // Vector of (key, value) pairs.
+    pub traversal: Vec<usize>,
+}
+
+impl<P: CRH, T: ToBytes> MerkleTriePath<P, T> {
+    pub fn verify(&self, root_hash: &[u8; 32], key: &[u8], value: &T) -> Result<bool, MerkleTrieError> {
+        assert_eq!(self.path.len(), self.traversal.len());
+        assert_eq!(self.parents.len(), self.traversal.len());
+
+        let mut curr_hash = calculate_root(&self.parameters, &key, &Some(value), &vec![])?;
+
+        println!("\n LEAF HASH: {:?}", curr_hash);
+
+        // Check that the given leaf matches the leaf in the membership proof.
+        if !self.path.is_empty() {
+            for (i, (index, siblings)) in self.traversal.iter().zip_eq(self.path.iter()).enumerate() {
+                let mut node_hashes: Vec<&[u8; 32]> = siblings.iter().map(|x| x).collect();
+                node_hashes.insert(*index, &curr_hash);
+
+                let (key, value) = &self.parents[i];
+
+                curr_hash = calculate_root(&self.parameters, key, value, &node_hashes)?;
+            }
+
+            // Check if final hash is root
+            if &curr_hash != root_hash {
+                return Ok(false);
+            }
+
+            Ok(true)
+        } else {
+            Ok(false)
+        }
+    }
+}
