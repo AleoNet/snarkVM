026409c6b8def2f14ac24a7e3782493a10f53353
howardwu <howard@aleo.org>
2021-09-21 19:41:56 -0700

    Add metadata in block header leaves

--- dpc/src/block/header.rs
+++ dpc/src/block/header.rs
@@ -71,6 +71,15 @@ impl BlockHeaderMetadata {
     }
 }
 
+impl ToBytes for BlockHeaderMetadata {
+    fn write_le<W: Write>(&self, mut writer: W) -> IoResult<()> {
+        self.height.to_le_bytes().write_le(&mut writer)?;
+        self.timestamp.to_le_bytes().write_le(&mut writer)?;
+        self.difficulty_target.to_le_bytes().write_le(&mut writer)?;
+        self.nonce.to_le_bytes().write_le(&mut writer)
+    }
+}
+
 /// Block header.
 #[derive(Clone, Debug, PartialEq, Eq, Serialize, Deserialize)]
 pub struct BlockHeader<N: Network> {
@@ -173,7 +182,7 @@ impl<N: Network> BlockHeader<N> {
             && N::posw().verify(&self)
     }
 
-    /// Returns the block header root.
+    /// Returns the block header leaves.
     pub fn to_header_leaves(&self) -> Result<Vec<[u8; 32]>> {
         let transactions_root_bytes = self.transactions_root.to_bytes_le()?;
         assert_eq!(transactions_root_bytes.len(), 32);
@@ -190,12 +199,16 @@ impl<N: Network> BlockHeader<N> {
         let mut commitments_root = [0u8; 32];
         commitments_root.copy_from_slice(&commitments_root_bytes);
 
-        // TODO (howardwu): CRITICAL - Implement a (masked) Merkle tree for the block header.
+        let metadata_bytes = self.metadata.to_bytes_le()?;
+        assert_eq!(metadata_bytes.len(), 24);
+        let mut metadata = [0u8; 32];
+        metadata.copy_from_slice(&metadata_bytes);
+
         let mut leaves: Vec<[u8; 32]> = Vec::with_capacity(N::POSW_NUM_LEAVES);
         leaves.push(transactions_root);
         leaves.push(serial_numbers_root);
         leaves.push(commitments_root);
-        leaves.push([0u8; 32]);
+        leaves.push(metadata);
         assert_eq!(N::POSW_NUM_LEAVES, leaves.len());
 
         Ok(leaves)
