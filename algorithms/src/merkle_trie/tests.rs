b05e3af822289fcdcad21497511dececee90a453
raychu86 <14917648+raychu86@users.noreply.github.com>
2021-09-24 16:52:01 -0700

    Add test for remove

--- algorithms/src/merkle_trie/tests.rs
+++ algorithms/src/merkle_trie/tests.rs
@@ -58,6 +58,22 @@ fn generate_merkle_trie<P: MerkleTrieParameters, L: ToBytes + Send + Sync + Clon
     trie
 }
 
+/// Generates a valid Merkle trie and gets/removes each pair.
+fn generate_merkle_trie_get_and_remove<P: MerkleTrieParameters, L: ToBytes + Send + Sync + Clone + Eq>(
+    key_pairs: Vec<(Vec<u8>, L)>,
+    parameters: &P,
+) -> MerkleTrie<P, L> {
+    let mut trie = MerkleTrie::<P, L>::new(Arc::new(parameters.clone()), key_pairs.clone()).unwrap();
+    for (_, (key, leaf)) in key_pairs.iter().enumerate() {
+        let value_bytes = trie.get(&key).unwrap().to_bytes_le().unwrap();
+        assert_eq!(&value_bytes, &leaf.to_bytes_le().unwrap());
+        let removed = trie.remove(&key).unwrap().unwrap();
+        assert_eq!(&value_bytes, &removed.to_bytes_le().unwrap());
+        assert!(trie.get(&key).is_none());
+    }
+    trie
+}
+
 /// Generates a valid Merkle trie and verifies the Merkle path witness for each leaf does not verify to an invalid root hash.
 fn bad_merkle_trie_verify<P: MerkleTrieParameters, L: ToBytes + Send + Sync + Clone + Eq>(
     key_pairs: Vec<(Vec<u8>, L)>,
@@ -72,7 +88,7 @@ fn bad_merkle_trie_verify<P: MerkleTrieParameters, L: ToBytes + Send + Sync + Cl
     trie
 }
 
-fn run_empty_merkle_tree_test<P: MerkleTrieParameters>() {
+fn run_empty_merkle_trie_test<P: MerkleTrieParameters>() {
     let parameters = &P::setup("merkle_trie_test");
     generate_merkle_trie::<P, Vec<u8>>(vec![], parameters);
 }
@@ -93,7 +109,7 @@ fn run_good_root_test<P: MerkleTrieParameters>() {
 }
 
 fn run_bad_root_test<P: MerkleTrieParameters>() {
-    let parameters = &P::setup("merkle_tree_test");
+    let parameters = &P::setup("merkle_trie_test");
 
     const KEY_SIZE: usize = 32;
     const VALUE_SIZE: usize = 32;
@@ -107,15 +123,37 @@ fn run_bad_root_test<P: MerkleTrieParameters>() {
     bad_merkle_trie_verify::<P, _>(key_pairs, parameters);
 }
 
+fn run_get_remove_test<P: MerkleTrieParameters>() {
+    let parameters = &P::setup("merkle_trie_test");
+
+    const KEY_SIZE: usize = 32;
+    const VALUE_SIZE: usize = 32;
+    assert_eq!(P::KEY_SIZE, KEY_SIZE);
+    assert_eq!(P::VALUE_SIZE, VALUE_SIZE);
+
+    let key_pairs = generate_random_key_pairs!(4, KEY_SIZE, VALUE_SIZE);
+    generate_merkle_trie_get_and_remove::<P, _>(key_pairs, parameters);
+
+    let key_pairs = generate_random_key_pairs!(16, KEY_SIZE, VALUE_SIZE);
+    generate_merkle_trie_get_and_remove::<P, _>(key_pairs, parameters);
+}
+
 mod poseidon_on_bls12_377_fr {
     use super::*;
     use snarkvm_curves::bls12_377::Fr;
 
     #[test]
-    fn empty_merkle_tree_test() {
+    fn empty_merkle_trie_test() {
         define_merkle_trie_parameters!(MerkleTrieParams, PoseidonCryptoHash<Fr, 4, false>, 32, 64, 32, 32);
 
-        run_empty_merkle_tree_test::<MerkleTrieParams>();
+        run_empty_merkle_trie_test::<MerkleTrieParams>();
+    }
+
+    #[test]
+    fn merkle_trie_get_remove_test() {
+        define_merkle_trie_parameters!(MerkleTrieParams, PoseidonCryptoHash<Fr, 4, false>, 32, 64, 32, 32);
+
+        run_get_remove_test::<MerkleTrieParams>();
     }
 
     #[test]
@@ -144,16 +182,16 @@ mod poseidon_on_bls12_377_fr {
 
         let crh = Arc::new(MerkleTrieParams::setup("TEST_MERKLE_TRIE_CRH"));
 
-        let mut tree_1 = MerkleTrie::<_, u8>::new(crh.clone(), vec![]).unwrap();
+        let mut trie_1 = MerkleTrie::<_, u8>::new(crh.clone(), vec![]).unwrap();
 
-        tree_1.insert(VALUE_PAIR_1.0, VALUE_PAIR_1.1).unwrap();
-        tree_1.insert(VALUE_PAIR_2.0, VALUE_PAIR_2.1).unwrap();
-        tree_1.insert(VALUE_PAIR_3.0, VALUE_PAIR_3.1).unwrap();
+        trie_1.insert(VALUE_PAIR_1.0, VALUE_PAIR_1.1).unwrap();
+        trie_1.insert(VALUE_PAIR_2.0, VALUE_PAIR_2.1).unwrap();
+        trie_1.insert(VALUE_PAIR_3.0, VALUE_PAIR_3.1).unwrap();
 
-        let value_1 = tree_1.get(VALUE_PAIR_1.0);
-        let value_2 = tree_1.get(VALUE_PAIR_2.0);
-        let value_3 = tree_1.get(VALUE_PAIR_3.0);
-        let value_4 = tree_1.get(VALUE_PAIR_4.0);
+        let value_1 = trie_1.get(VALUE_PAIR_1.0);
+        let value_2 = trie_1.get(VALUE_PAIR_2.0);
+        let value_3 = trie_1.get(VALUE_PAIR_3.0);
+        let value_4 = trie_1.get(VALUE_PAIR_4.0);
 
         assert_eq!(value_1, Some(&VALUE_PAIR_1.1));
         assert_eq!(value_2, Some(&VALUE_PAIR_2.1));
@@ -167,14 +205,14 @@ mod poseidon_on_bls12_377_fr {
 
         let crh = Arc::new(MerkleTrieParams::setup("TEST_MERKLE_TRIE_CRH"));
 
-        let mut tree_1 = MerkleTrie::<_, u8>::new(crh.clone(), vec![]).unwrap();
+        let mut trie_1 = MerkleTrie::<_, u8>::new(crh.clone(), vec![]).unwrap();
 
-        tree_1.insert(VALUE_PAIR_1.0, VALUE_PAIR_1.1).unwrap();
-        tree_1.insert(VALUE_PAIR_2.0, VALUE_PAIR_2.1).unwrap();
-        tree_1.insert(VALUE_PAIR_3.0, VALUE_PAIR_3.1).unwrap();
+        trie_1.insert(VALUE_PAIR_1.0, VALUE_PAIR_1.1).unwrap();
+        trie_1.insert(VALUE_PAIR_2.0, VALUE_PAIR_2.1).unwrap();
+        trie_1.insert(VALUE_PAIR_3.0, VALUE_PAIR_3.1).unwrap();
 
-        let proof = tree_1.generate_proof(VALUE_PAIR_3.0, &VALUE_PAIR_3.1).unwrap();
+        let proof = trie_1.generate_proof(VALUE_PAIR_3.0, &VALUE_PAIR_3.1).unwrap();
 
-        assert!(proof.verify(&tree_1.root(), VALUE_PAIR_3.0, &VALUE_PAIR_3.1).unwrap());
+        assert!(proof.verify(&trie_1.root(), VALUE_PAIR_3.0, &VALUE_PAIR_3.1).unwrap());
     }
 }
