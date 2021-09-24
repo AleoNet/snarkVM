75805a67d4f27eb9e26811a3e329ab383129a4a7
raychu86 <14917648+raychu86@users.noreply.github.com>
2021-09-23 20:49:14 -0700

    Initial implementation of Merkle Trie gadget

--- gadgets/src/algorithms/merkle_trie/merkle_trie_path.rs
+++ gadgets/src/algorithms/merkle_trie/merkle_trie_path.rs
@@ -28,8 +28,13 @@ use snarkvm_utilities::ToBytes;
 use crate::{
     bits::{boolean::Boolean, ToBytesGadget},
     integers::uint::UInt8,
-    traits::{algorithms::CRHGadget, alloc::AllocGadget, eq::ConditionalEqGadget, select::CondSelectGadget},
-    EqGadget,
+    traits::{
+        algorithms::CRHGadget,
+        alloc::AllocGadget,
+        eq::{ConditionalEqGadget, EqGadget},
+        select::CondSelectGadget,
+    },
+    EvaluateLtGadget,
 };
 
 pub type Key = Vec<UInt8>;
@@ -38,10 +43,13 @@ pub type Value = Vec<UInt8>;
 pub struct MerkleTriePathGadget<P: MerkleTrieParameters, HG: CRHGadget<P::H, F>, F: PrimeField> {
     /// `traversal[i]` is the location of the parent node among its siblings.
     traversal: Vec<UInt8>,
-    /// `path[i]`is the entry of siblings of ith depth from bottom to top.
-    path: Vec<Vec<HG::OutputGadget>>,
+    /// `path[i]`is the entry of siblings of ith depth from bottom to top and the number of non-filler siblings.
+    path: Vec<(UInt8, Vec<HG::OutputGadget>)>,
     /// `parents[i]`is the parent key value pair at the ith depth from bottom to top.
     parents: Vec<(Key, Value)>,
+    /// The depth of the key, value pair in the tree. All elements of `traversal`, `path`,
+    /// and `parents` past the `depth` index, is zeros.
+    depth: UInt8,
 }
 
 impl<P: MerkleTrieParameters, HG: CRHGadget<P::H, F>, F: PrimeField> MerkleTriePathGadget<P, HG, F> {
@@ -49,41 +57,105 @@ impl<P: MerkleTrieParameters, HG: CRHGadget<P::H, F>, F: PrimeField> MerkleTrieP
         &self,
         mut cs: CS,
         crh: &HG,
-        leaf: impl ToBytesGadget<F>,
+        key: impl ToBytesGadget<F>,
+        value: impl ToBytesGadget<F>,
     ) -> Result<HG::OutputGadget, SynthesisError> {
-        // let leaf_bytes = leaf.to_bytes(&mut cs.ns(|| "leaf_to_bytes"))?;
-        // let mut curr_hash = crh.check_evaluation_gadget(cs.ns(|| "leaf_hash"), leaf_bytes)?;
-        //
-        // // To traverse up a MT, we iterate over the path from bottom to top
-        //
-        // // At any given bit, the bit being 0 indicates our currently hashed value is the left,
-        // // and the bit being 1 indicates our currently hashed value is on the right.
-        // // Thus `left_hash` is the sibling if bit is 1, and it's the computed hash if bit is 0
-        // for (i, (bit, sibling)) in self.traversal.iter().zip_eq(self.path.iter()).enumerate() {
-        //     let left_hash = HG::OutputGadget::conditionally_select(
-        //         cs.ns(|| format!("cond_select_left_{}", i)),
-        //         &bit,
-        //         &sibling,
-        //         &curr_hash,
-        //     )?;
-        //     let right_hash = HG::OutputGadget::conditionally_select(
-        //         cs.ns(|| format!("cond_select_right_{}", i)),
-        //         &bit,
-        //         &curr_hash,
-        //         &sibling,
-        //     )?;
-        //
-        //     curr_hash = hash_inner_node_gadget::<P::H, HG, F, _>(
-        //         &mut cs.ns(|| format!("hash_inner_node_{}", i)),
-        //         crh,
-        //         &left_hash,
-        //         &right_hash,
-        //     )?;
-        // }
-        //
-        // Ok(curr_hash)
+        let mut curr_hash = Self::hash_node(cs.ns(|| "leaf_hash"), crh, key, value, &vec![])?;
 
-        unimplemented!()
+        for (i, (((parent_key, parent_value), position), siblings)) in self
+            .parents
+            .iter()
+            .zip_eq(self.traversal.iter())
+            .zip_eq(self.path.iter())
+            .enumerate()
+        {
+            let current_depth = UInt8::alloc(cs.ns(|| format!("depth_{}", i)), || Ok(i as u8))?;
+
+            // Select the correct sibling roots size.
+            let mut sibling_roots = vec![];
+
+            let num_valid_siblings = &siblings.0;
+            for (j, sibling) in siblings.1.iter().enumerate() {
+                let current_sibling_index =
+                    UInt8::alloc(cs.ns(|| format!("sibling_index_{}_{}", i, j)), || Ok(j as u8))?;
+
+                // Create a temporary vec and add the sibling.
+                let mut add_sibling = sibling_roots.clone();
+                add_sibling.push(sibling.clone());
+
+                // Check if the sibling is a filler and should be added or not.
+                let index_is_in_range = num_valid_siblings.less_than(
+                    cs.ns(|| format!("sibling_less_than_{}_{}", i, j)),
+                    &current_sibling_index,
+                )?;
+
+                let selected_siblings = Vec::<HG::OutputGadget>::conditionally_select(
+                    cs.ns(|| format!("conditionally_select_siblings_{}", i)),
+                    &index_is_in_range,
+                    &add_sibling,
+                    &sibling_roots,
+                )?;
+
+                sibling_roots = selected_siblings;
+            }
+
+            // Insert the current node into the siblings
+            let mut final_siblings = sibling_roots.clone();
+            for (j, sibling) in sibling_roots.iter().enumerate() {
+                let current_sibling_index =
+                    UInt8::alloc(cs.ns(|| format!("sibling_index_insert_{}_{}", i, j)), || Ok(j as u8))?;
+
+                // Create a temporary vec and add the sibling to a specific index.
+                let mut add_sibling = sibling_roots.clone();
+                add_sibling.insert(j, sibling.clone());
+
+                // Check if the sibling is a filler and should be added or not.
+                let index_is_correct =
+                    current_sibling_index.is_eq(cs.ns(|| format!("sibling_is_eq_{}_{}", i, j)), &position)?;
+                let selected_siblings = Vec::<HG::OutputGadget>::conditionally_select(
+                    cs.ns(|| format!("conditionally_select_siblings_insert_{}", i)),
+                    &index_is_correct,
+                    &add_sibling,
+                    &final_siblings,
+                )?;
+
+                final_siblings = selected_siblings;
+            }
+
+            // Create the new hash and select it as valid only if the current depth is less than or equal to the given depth.
+            let new_hash = Self::hash_node(cs.ns(|| "leaf_hash"), crh, parent_key, parent_value, &final_siblings)?;
+            let depth_is_in_range = self
+                .depth
+                .less_than(cs.ns(|| format!("less_than_{}", i)), &current_depth)?;
+            let selected_hash = HG::OutputGadget::conditionally_select(
+                cs.ns(|| format!("conditionally_select_hash_{}", i)),
+                &depth_is_in_range,
+                &new_hash,
+                &curr_hash,
+            )?;
+
+            curr_hash = selected_hash;
+        }
+
+        Ok(curr_hash)
+    }
+
+    pub(crate) fn hash_node<CS: ConstraintSystem<F>>(
+        mut cs: CS,
+        crh: &HG,
+        key: impl ToBytesGadget<F>,
+        value: impl ToBytesGadget<F>,
+        child_roots: &Vec<HG::OutputGadget>,
+    ) -> Result<HG::OutputGadget, SynthesisError> {
+        let mut bytes = vec![]; // TODO (raychu86): Add key to hashing.
+        bytes.extend_from_slice(&value.to_bytes(cs.ns(|| "value_to_bytes"))?);
+
+        for (i, child) in child_roots.iter().enumerate() {
+            let child_bytes = child.to_bytes(&mut cs.ns(|| format!("leaf_to_bytes_{}", i)))?;
+            bytes.extend_from_slice(&child_bytes);
+        }
+
+        crh.check_evaluation_gadget(cs, bytes)
     }
 
     pub fn check_membership<CS: ConstraintSystem<F>>(
@@ -91,9 +163,10 @@ impl<P: MerkleTrieParameters, HG: CRHGadget<P::H, F>, F: PrimeField> MerkleTrieP
         cs: CS,
         crh: &HG,
         root: &HG::OutputGadget,
-        leaf: impl ToBytesGadget<F>,
+        key: impl ToBytesGadget<F>,
+        value: impl ToBytesGadget<F>,
     ) -> Result<(), SynthesisError> {
-        self.conditionally_check_membership(cs, crh, root, leaf, &Boolean::Constant(true))
+        self.conditionally_check_membership(cs, crh, root, key, value, &Boolean::Constant(true))
     }
 
     pub fn conditionally_check_membership<CS: ConstraintSystem<F>>(
@@ -101,10 +174,11 @@ impl<P: MerkleTrieParameters, HG: CRHGadget<P::H, F>, F: PrimeField> MerkleTrieP
         mut cs: CS,
         crh: &HG,
         root: &HG::OutputGadget,
-        leaf: impl ToBytesGadget<F>,
+        key: impl ToBytesGadget<F>,
+        value: impl ToBytesGadget<F>,
         should_enforce: &Boolean,
     ) -> Result<(), SynthesisError> {
-        let expected_root = self.calculate_root(cs.ns(|| "calculate_root"), crh, leaf)?;
+        let expected_root = self.calculate_root(cs.ns(|| "calculate_root"), crh, key, value)?;
 
         root.conditional_enforce_equal(&mut cs.ns(|| "root_is_eq"), &expected_root, should_enforce)
     }
@@ -115,59 +189,108 @@ where
     P: MerkleTrieParameters,
     HGadget: CRHGadget<P::H, F>,
     F: PrimeField,
-    L: ToBytes,
+    L: ToBytes + Clone,
 {
     fn alloc<Fn, T, CS: ConstraintSystem<F>>(mut cs: CS, value_gen: Fn) -> Result<Self, SynthesisError>
     where
         Fn: FnOnce() -> Result<T, SynthesisError>,
         T: Borrow<MerkleTriePath<P, L>>,
     {
-        // let merkle_path = value_gen()?.borrow().clone();
-        //
-        // let mut traversal = vec![];
-        // for (i, position) in merkle_path.position_list().enumerate() {
-        //     traversal.push(Boolean::alloc(cs.ns(|| format!("alloc_position_{}", i)), || {
-        //         Ok(position)
-        //     })?);
-        // }
-        //
-        // let mut path = Vec::with_capacity(merkle_path.path.len());
-        // for (i, node) in merkle_path.path.iter().enumerate() {
-        //     path.push(HGadget::OutputGadget::alloc(
-        //         &mut cs.ns(|| format!("alloc_node_{}", i)),
-        //         || Ok(node.clone()),
-        //     )?);
-        // }
-        //
-        // Ok(MerkleTriePathGadget { traversal, path })
+        let merkle_trie_path = value_gen()?.borrow().clone();
 
-        unimplemented!()
+        assert_eq!(merkle_trie_path.parents.len(), merkle_trie_path.path.len());
+        assert_eq!(merkle_trie_path.path.len(), merkle_trie_path.traversal.len());
+
+        let mut traversal = Vec::with_capacity(P::MAX_DEPTH);
+        for (i, position) in merkle_trie_path.traversal.iter().enumerate() {
+            traversal.push(UInt8::alloc(
+                cs.ns(|| format!("alloc_traversal_position_{}", i)),
+                || Ok(*position as u8),
+            )?);
+        }
+
+        let filler_sibling = <P::H as CRH>::Output::default();
+
+        let mut path = Vec::with_capacity(P::MAX_DEPTH);
+        for (i, sibling_roots) in merkle_trie_path.path.iter().enumerate() {
+            let mut siblings = vec![];
+            for (j, sibling) in sibling_roots.iter().enumerate() {
+                siblings.push(HGadget::OutputGadget::alloc(
+                    &mut cs.ns(|| format!("alloc_sibling_{}_{}", i, j)),
+                    || Ok(sibling.clone()),
+                )?);
+            }
+
+            let num_real_siblings = UInt8::alloc(cs.ns(|| format!("num_siblings_{}", i)), || Ok(siblings.len() as u8))?;
+
+            // Add the filler siblings
+            for j in sibling_roots.len()..P::MAX_BRANCH {
+                siblings.push(HGadget::OutputGadget::alloc(
+                    &mut cs.ns(|| format!("alloc_sibling_{}_{}", i, j)),
+                    || Ok(filler_sibling.clone()),
+                )?);
+            }
+            path.push((num_real_siblings, siblings));
+        }
+
+        let mut parents = Vec::with_capacity(P::MAX_DEPTH);
+        for (i, (key, value)) in merkle_trie_path.parents.iter().enumerate() {
+            let key_gadget = UInt8::alloc_vec(cs.ns(|| format!("alloc_key_{}", i)), &key)?;
+            let value_gadget = match value {
+                Some(l) => UInt8::alloc_vec(cs.ns(|| format!("alloc_value_{}", i)), &l.to_bytes_le()?)?,
+                None => UInt8::alloc_vec(cs.ns(|| format!("alloc_value_{}", i)), &vec![0u8; P::VALUE_SIZE])?, // TODO (raychu86): Use the size of the value.
+            };
+
+            parents.push((key_gadget, value_gadget));
+        }
+
+        let depth = UInt8::alloc(cs.ns(|| "alloc_depth"), || Ok(merkle_trie_path.parents.len() as u8))?;
+
+        // Fill `traversal`, `path`, and `parents` to the max depth.
+
+        for i in traversal.len()..P::MAX_DEPTH {
+            traversal.push(UInt8::alloc(cs.ns(|| format!("alloc_filler_traversal_{}", i)), || {
+                Ok(0)
+            })?);
+        }
+
+        for i in path.len()..P::MAX_DEPTH {
+            let mut siblings = vec![];
+            for j in 0..P::MAX_BRANCH {
+                siblings.push(HGadget::OutputGadget::alloc(
+                    &mut cs.ns(|| format!("alloc_filler_sibling_{}_{}", i, j)),
+                    || Ok(<P::H as CRH>::Output::default()),
+                )?);
+            }
+            let filler_depth = UInt8::alloc(cs.ns(|| format!("filler_depth_{}", i)), || Ok(0))?;
+            path.push((filler_depth, siblings));
+        }
+
+        for i in parents.len()..P::MAX_DEPTH {
+            let key_gadget = UInt8::alloc_vec(cs.ns(|| format!("alloc_filler_key_{}", i)), &vec![0u8; P::KEY_SIZE])?; // TODO (raychu86): Use the size of the key.
+            let value_gadget =
+                UInt8::alloc_vec(cs.ns(|| format!("alloc_filler_value_{}", i)), &vec![0u8; P::VALUE_SIZE])?; // TODO (raychu86): Use the size of the value.
+            parents.push((key_gadget, value_gadget));
+        }
+
+        assert_eq!(traversal.len(), P::MAX_DEPTH);
+        assert_eq!(path.len(), P::MAX_DEPTH);
+        assert_eq!(parents.len(), P::MAX_DEPTH);
+
+        Ok(MerkleTriePathGadget {
+            traversal,
+            path,
+            parents,
+            depth,
+        })
     }
 
-    fn alloc_input<Fn, T, CS: ConstraintSystem<F>>(mut cs: CS, value_gen: Fn) -> Result<Self, SynthesisError>
+    fn alloc_input<Fn, T, CS: ConstraintSystem<F>>(_cs: CS, _value_gen: Fn) -> Result<Self, SynthesisError>
     where
         Fn: FnOnce() -> Result<T, SynthesisError>,
         T: Borrow<MerkleTriePath<P, L>>,
     {
-        // let merkle_path = value_gen()?.borrow().clone();
-        //
-        // let mut traversal = vec![];
-        // for (i, position) in merkle_path.position_list().enumerate() {
-        //     traversal.push(Boolean::alloc_input(
-        //         cs.ns(|| format!("alloc_input_position_{}", i)),
-        //         || Ok(position),
-        //     )?);
-        // }
-        //
-        // let mut path = Vec::with_capacity(merkle_path.path.len());
-        // for (i, node) in merkle_path.path.iter().enumerate() {
-        //     path.push(HGadget::OutputGadget::alloc_input(
-        //         &mut cs.ns(|| format!("alloc_input_node_{}", i)),
-        //         || Ok(node.clone()),
-        //     )?);
-        // }
-        //
-        // Ok(MerkleTriePathGadget { traversal, path, parents })
+        // let merkle_trie_path = value_gen()?.borrow().clone();
 
         unimplemented!()
     }
--- gadgets/src/algorithms/merkle_trie/tests.rs
+++ gadgets/src/algorithms/merkle_trie/tests.rs
@@ -13,3 +13,160 @@
 
 // You should have received a copy of the GNU General Public License
 // along with the snarkVM library. If not, see <https://www.gnu.org/licenses/>.
+
+use std::sync::Arc;
+
+use rand::{thread_rng, Rng};
+
+use snarkvm_algorithms::{
+    crypto_hash::PoseidonCryptoHash,
+    define_merkle_trie_parameters,
+    merkle_trie::MerkleTrie,
+    traits::{MerkleTrieParameters, CRH},
+};
+use snarkvm_curves::bls12_377::Fr;
+use snarkvm_fields::PrimeField;
+use snarkvm_r1cs::{ConstraintSystem, TestConstraintSystem};
+use snarkvm_utilities::ToBytes;
+
+use crate::{
+    algorithms::merkle_trie::*,
+    integers::uint::UInt8,
+    traits::{algorithms::CRHGadget, alloc::AllocGadget},
+};
+
+/// Generates the specified number of random Merkle trie key_pairs.
+macro_rules! generate_random_key_pairs {
+    ($num_leaves:expr, $key_size:expr, $leaf_size:expr) => {{
+        let mut rng = thread_rng();
+
+        let mut key_pairs = Vec::with_capacity($num_leaves);
+        for _ in 0..$num_leaves {
+            let mut key = [0u8; $key_size];
+            rng.fill(&mut key);
+
+            let mut val = [0u8; $leaf_size];
+            rng.fill(&mut val);
+
+            key_pairs.push((key.to_vec(), val));
+        }
+
+        key_pairs
+    }};
+}
+
+fn generate_merkle_tree<
+    P: MerkleTrieParameters,
+    F: PrimeField,
+    HG: CRHGadget<P::H, F>,
+    L: ToBytes + Send + Sync + Clone + Eq,
+>(
+    key_pairs: Vec<(Vec<u8>, L)>,
+    use_bad_root: bool,
+) {
+    let parameters = P::setup("merkle_trie_test");
+    let trie = MerkleTrie::<P, _>::new(Arc::new(parameters.clone()), key_pairs.clone()).unwrap();
+    let root = trie.root();
+    let mut satisfied = true;
+    for (i, (key, value)) in key_pairs.iter().enumerate() {
+        let mut cs = TestConstraintSystem::<F>::new();
+        let proof = trie.generate_proof(key, value).unwrap();
+        assert!(proof.verify(&root, &key, value).unwrap());
+
+        // Allocate Merkle tree root
+        let root = <HG as CRHGadget<_, _>>::OutputGadget::alloc(&mut cs.ns(|| format!("new_digest_{}", i)), || {
+            if use_bad_root {
+                Ok(<P::H as CRH>::Output::default())
+            } else {
+                Ok(root.clone())
+            }
+        })
+        .unwrap();
+
+        let constraints_from_digest = cs.num_constraints();
+        println!("constraints from digest: {}", constraints_from_digest);
+
+        // Allocate CRH
+        let crh_parameters = HG::alloc_constant(&mut cs.ns(|| format!("new_parameters_{}", i)), || {
+            Ok(parameters.crh().clone())
+        })
+        .unwrap();
+
+        let constraints_from_parameters = cs.num_constraints() - constraints_from_digest;
+        println!("constraints from parameters: {}", constraints_from_parameters);
+
+        // Allocate key
+        let key = UInt8::constant_vec(&key);
+
+        let constraints_from_key = cs.num_constraints() - constraints_from_parameters - constraints_from_digest;
+        println!("constraints from key: {}", constraints_from_key);
+
+        // Allocate value
+        let value = UInt8::constant_vec(&value.to_bytes_le().unwrap());
+
+        let constraints_from_value =
+            cs.num_constraints() - constraints_from_parameters - constraints_from_digest - constraints_from_key;
+        println!("constraints from value: {}", constraints_from_value);
+
+        // Allocate Merkle tree path
+        let cw =
+            MerkleTriePathGadget::<_, HG, _>::alloc(&mut cs.ns(|| format!("new_witness_{}", i)), || Ok(proof)).unwrap();
+
+        let constraints_from_path = cs.num_constraints()
+            - constraints_from_parameters
+            - constraints_from_digest
+            - constraints_from_key
+            - constraints_from_value;
+        println!("constraints from path: {}", constraints_from_path);
+        cw.check_membership(
+            &mut cs.ns(|| format!("new_witness_check_{}", i)),
+            &crh_parameters,
+            &root,
+            &key,
+            &value,
+        )
+        .unwrap();
+        if !cs.is_satisfied() {
+            satisfied = false;
+            println!("Unsatisfied constraint: {}", cs.which_is_unsatisfied().unwrap());
+        }
+        let setup_constraints = constraints_from_key
+            + constraints_from_value
+            + constraints_from_digest
+            + constraints_from_parameters
+            + constraints_from_path;
+        println!("number of constraints: {}", cs.num_constraints() - setup_constraints);
+    }
+
+    assert!(satisfied);
+}
+
+mod merkle_trie_poseidon {
+    use super::*;
+    use crate::algorithms::crypto_hash::PoseidonCryptoHashGadget;
+
+    type H = PoseidonCryptoHash<Fr, 4, false>;
+    type HG = PoseidonCryptoHashGadget<Fr, 4, false>;
+
+    const KEY_SIZE: usize = 32;
+    const VALUE_SIZE: usize = 32;
+
+    #[test]
+    fn good_root_test() {
+        define_merkle_trie_parameters!(MerkleTrieParams, H, 32, 64, KEY_SIZE, VALUE_SIZE);
+
+        let key_pairs = generate_random_key_pairs!(4, KEY_SIZE, VALUE_SIZE);
+
+        generate_merkle_tree::<MerkleTrieParams, Fr, HG, _>(key_pairs, false);
+    }
+
+    #[should_panic]
+    #[test]
+    fn bad_root_test() {
+        define_merkle_trie_parameters!(MerkleTrieParams, H, 32, 64, KEY_SIZE, VALUE_SIZE);
+
+        let key_pairs = generate_random_key_pairs!(4, KEY_SIZE, VALUE_SIZE);
+
+        generate_merkle_tree::<MerkleTrieParams, Fr, HG, _>(key_pairs, true);
+    }
+}
