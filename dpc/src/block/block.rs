fa1a983241f2a3faa8d5208a2e32ede699b53a16
howardwu <howard@aleo.org>
2021-10-03 00:01:02 -0700

    Removes ledger module, updates CI, adds Block partialeq and eq impl

--- dpc/src/block/block.rs
+++ dpc/src/block/block.rs
@@ -27,7 +27,7 @@ use std::{
     time::Instant,
 };
 
-#[derive(Debug, Clone, Eq, PartialEq)]
+#[derive(Debug, Clone)]
 pub struct Block<N: Network> {
     /// Hash of the previous block - 32 bytes
     previous_hash: N::BlockHash,
@@ -143,6 +143,33 @@ impl<N: Network> Block<N> {
             return false;
         }
 
+        // Ensure the header are valid.
+        if !self.header.is_valid() {
+            eprintln!("Invalid block header");
+            return false;
+        }
+
+        // Ensure the transactions are valid.
+        if !self.transactions.is_valid() {
+            eprintln!("Invalid block transactions");
+            return false;
+        }
+
+        // Fetch the transactions root.
+        let transactions_root = match self.transactions.to_transactions_root() {
+            Ok(transactions_root) => transactions_root,
+            Err(error) => {
+                eprintln!("{}", error);
+                return false;
+            }
+        };
+
+        // Ensure the transactions root matches the computed root from the transactions list.
+        if self.header.transactions_root() != transactions_root {
+            eprintln!("Invalid block transactions does not match transactions root in header");
+            return false;
+        }
+
         // Retrieve the coinbase transaction.
         let coinbase_transaction = match self.to_coinbase_transaction() {
             Ok(coinbase_transaction) => coinbase_transaction,
@@ -175,8 +202,7 @@ impl<N: Network> Block<N> {
             }
         };
 
-        // Ensure the header and transactions are valid.
-        self.header.is_valid() && self.transactions.is_valid()
+        true
     }
 
     /// Returns `true` if the block is a genesis block.
@@ -312,6 +338,16 @@ impl<N: Network> Hash for Block<N> {
             .hash(state);
     }
 }
+
+impl<N: Network> PartialEq for Block<N> {
+    fn eq(&self, other: &Self) -> bool {
+        self.previous_hash == other.previous_hash
+            && self.header == other.header
+            && self.transactions == other.transactions
+    }
+}
+
+impl<N: Network> Eq for Block<N> {}
 
 #[cfg(test)]
 mod tests {
