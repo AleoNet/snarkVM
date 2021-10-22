0f2ac6a3dd54e3c255a2158164eb863ba7cf1e60
raychu86 <14917648+raychu86@users.noreply.github.com>
2021-10-22 12:13:21 -0700

    Update test serialization sizes

--- dpc/src/circuits/tests.rs
+++ dpc/src/circuits/tests.rs
@@ -160,6 +160,6 @@ mod testnet2 {
 
     #[test]
     fn test_dpc_execute_circuits() {
-        dpc_execute_circuits_test::<Testnet2>(274298, 240277);
+        dpc_execute_circuits_test::<Testnet2>(274298, 243613);
     }
 }
