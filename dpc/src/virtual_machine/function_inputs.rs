5b1455bcc5f960ce141256840ed70f967f4ffac8
raychu86 <14917648+raychu86@users.noreply.github.com>
2021-11-05 22:08:48 -0700

    Implement (De)Serialization for function inputs

--- dpc/src/virtual_machine/function_inputs.rs
+++ dpc/src/virtual_machine/function_inputs.rs
@@ -17,25 +17,27 @@
 use crate::{virtual_machine::AleoAmount, Address, Network, Payload};
 use snarkvm_algorithms::CRH;
 use snarkvm_fields::{ConstraintFieldError, ToConstraintField};
-use snarkvm_utilities::{FromBytes, ToBytes};
+use snarkvm_utilities::{FromBytes, FromBytesDeserializer, ToBytes, ToBytesSerializer};
 
 use anyhow::Result;
-use serde::{Deserialize, Serialize};
-use std::io::{Read, Result as IoResult, Write};
+use serde::{de, Deserialize, Deserializer, Serialize, Serializer};
+use std::{
+    fmt,
+    io::{Read, Result as IoResult, Write},
+    str::FromStr,
+};
 
 type Caller<N> = Address<N>;
 type Recipient<N> = Address<N>;
 
-#[derive(Derivative, Serialize, Deserialize)]
+#[derive(Derivative)]
 #[derivative(
     Clone(bound = "N: Network"),
     Debug(bound = "N: Network"),
     PartialEq(bound = "N: Network")
 )]
 pub struct FunctionInputs<N: Network> {
-    #[serde(skip)]
     pub(crate) caller: Caller<N>,
-    #[serde(skip)]
     pub(crate) recipient: Recipient<N>,
     pub(crate) amount: AleoAmount,
     pub(crate) record_payload: Payload<N>,
@@ -56,6 +58,10 @@ impl<N: Network> FunctionInputs<N> {
         Ok(N::FunctionInputsCRH::setup("UnusedInPoseidon")
             .hash_field_elements(&self.to_field_elements()?)?
             .into())
+    }
+
+    fn size_in_bytes() -> usize {
+        N::ADDRESS_SIZE_IN_BYTES + N::ADDRESS_SIZE_IN_BYTES + 8 + N::RECORD_PAYLOAD_SIZE_IN_BYTES
     }
 }
 
@@ -83,6 +89,53 @@ impl<N: Network> ToBytes for FunctionInputs<N> {
         self.recipient.write_le(&mut writer)?;
         self.amount.write_le(&mut writer)?;
         self.record_payload.write_le(&mut writer)
+    }
+}
+
+impl<N: Network> FromStr for FunctionInputs<N> {
+    type Err = anyhow::Error;
+
+    fn from_str(function_inputs: &str) -> Result<Self, Self::Err> {
+        let function_inputs = serde_json::Value::from_str(function_inputs)?;
+
+        // Recover the function inputs.
+        Ok(Self::new(
+            &serde_json::from_value(function_inputs["caller"].clone())?,
+            &serde_json::from_value(function_inputs["recipient"].clone())?,
+            serde_json::from_value(function_inputs["amount"].clone())?,
+            serde_json::from_value(function_inputs["record_payload"].clone())?,
+        ))
+    }
+}
+
+impl<N: Network> fmt::Display for FunctionInputs<N> {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        let function_inputs = serde_json::json!({
+            "caller": self.caller,
+            "recipient": self.recipient,
+            "amount": self.amount,
+            "record_payload": self.record_payload,
+        });
+
+        write!(f, "{}", function_inputs)
+    }
+}
+
+impl<N: Network> Serialize for FunctionInputs<N> {
+    fn serialize<S: Serializer>(&self, serializer: S) -> Result<S::Ok, S::Error> {
+        match serializer.is_human_readable() {
+            true => serializer.collect_str(self),
+            false => ToBytesSerializer::serialize(self, serializer),
+        }
+    }
+}
+
+impl<'de, N: Network> Deserialize<'de> for FunctionInputs<N> {
+    fn deserialize<D: Deserializer<'de>>(deserializer: D) -> Result<Self, D::Error> {
+        match deserializer.is_human_readable() {
+            true => FromStr::from_str(&String::deserialize(deserializer)?).map_err(de::Error::custom),
+            false => FromBytesDeserializer::<Self>::deserialize(deserializer, "function_inputs", Self::size_in_bytes()),
+        }
     }
 }
 
