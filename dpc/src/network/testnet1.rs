e7266f993378bcf5b08111edd3b580cd63c218da
howardwu <howard@aleo.org>
2021-11-11 03:37:12 -0800

    Bring block times back to 20

--- dpc/src/network/testnet1.rs
+++ dpc/src/network/testnet1.rs
@@ -130,7 +130,7 @@ impl Network for Testnet1 {
     const TRANSITION_TREE_DEPTH: usize = 3;
     const TRANSACTION_TREE_DEPTH: usize = 7;
 
-    const ALEO_BLOCK_TIME_IN_SECS: i64 = 15i64;
+    const ALEO_BLOCK_TIME_IN_SECS: i64 = 20i64;
     const ALEO_MAXIMUM_FORK_DEPTH: u32 = 1024;
     const ALEO_STARTING_SUPPLY_IN_CREDITS: i64 = 500_000;
 
