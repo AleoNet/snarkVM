6d329768875a987b24d46be583c34fc7e4ae0373
howardwu <howard@aleo.org>
2021-11-27 02:15:14 -0800

    Update size

--- dpc/src/circuits/tests.rs
+++ dpc/src/circuits/tests.rs
@@ -185,7 +185,7 @@ mod testnet1 {
 
     #[test]
     fn test_dpc_execute_circuits() {
-        dpc_execute_circuits_test::<Testnet1>(252160, 147941);
+        dpc_execute_circuits_test::<Testnet1>(251452, 147941);
     }
 }
 
@@ -195,6 +195,6 @@ mod testnet2 {
 
     #[test]
     fn test_dpc_execute_circuits() {
-        dpc_execute_circuits_test::<Testnet2>(252160, 238013);
+        dpc_execute_circuits_test::<Testnet2>(251452, 238013);
     }
 }
