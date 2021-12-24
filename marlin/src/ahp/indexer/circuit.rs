ccae0bb906090db315c5bedea9c08ac1328ca402
Pratyush Mishra <pratyushmishra@berkeley.edu>
2021-12-23 18:04:24 -0800

    Fix std import

--- marlin/src/ahp/indexer/circuit.rs
+++ marlin/src/ahp/indexer/circuit.rs
@@ -14,7 +14,7 @@
 // You should have received a copy of the GNU General Public License
 // along with the snarkVM library. If not, see <https://www.gnu.org/licenses/>.
 
-use std::marker::PhantomData;
+use core::marker::PhantomData;
 
 use crate::{ahp::matrices::MatrixArithmetization, marlin::MarlinMode, CircuitInfo, Matrix};
 use snarkvm_fields::PrimeField;
--- marlin/src/marlin/circuit_verifying_key.rs
+++ marlin/src/marlin/circuit_verifying_key.rs
@@ -14,7 +14,7 @@
 // You should have received a copy of the GNU General Public License
 // along with the snarkVM library. If not, see <https://www.gnu.org/licenses/>.
 
-use std::marker::PhantomData;
+use core::marker::PhantomData;
 
 use crate::{
     ahp::indexer::*,
