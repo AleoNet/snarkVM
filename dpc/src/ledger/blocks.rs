9cb0fdb66c12e34a5e3819ea61d456b0b80b5839
howardwu <howard@aleo.org>
2021-12-28 13:04:37 -0800

    Switch to a triple conditional

--- dpc/src/ledger/blocks.rs
+++ dpc/src/ledger/blocks.rs
@@ -245,9 +245,11 @@ impl<N: Network> Blocks<N> {
         let expected_difficulty_target =
             if N::NETWORK_ID == 2 && block.height() <= crate::testnet2::V12_UPGRADE_BLOCK_HEIGHT {
                 Blocks::<N>::compute_difficulty_target(current_block.header(), block.timestamp(), block.height())
-            } else {
+            } else if N::NETWORK_ID == 2 {
                 let anchor_block_header = self.get_block_header(crate::testnet2::V12_UPGRADE_BLOCK_HEIGHT)?;
                 Blocks::<N>::compute_difficulty_target(&anchor_block_header, block.timestamp(), block.height())
+            } else {
+                Blocks::<N>::compute_difficulty_target(&N::genesis_block().header(), block.timestamp(), block.height())
             };
         if block.difficulty_target() != expected_difficulty_target {
             return Err(anyhow!(
--- dpc/src/ledger/ledger.rs
+++ dpc/src/ledger/ledger.rs
@@ -163,11 +163,13 @@ impl<N: Network> Ledger<N> {
         // Compute the block difficulty target.
         let difficulty_target = if N::NETWORK_ID == 2 && block_height <= crate::testnet2::V12_UPGRADE_BLOCK_HEIGHT {
             Blocks::<N>::compute_difficulty_target(self.latest_block()?.header(), block_timestamp, block_height)
-        } else {
+        } else if N::NETWORK_ID == 2 {
             let anchor_block_header = self
                 .canon_blocks
                 .get_block_header(crate::testnet2::V12_UPGRADE_BLOCK_HEIGHT)?;
             Blocks::<N>::compute_difficulty_target(&anchor_block_header, block_timestamp, block_height)
+        } else {
+            Blocks::<N>::compute_difficulty_target(&N::genesis_block().header(), block.timestamp(), block.height())
         };
 
         // Compute the cumulative weight.
