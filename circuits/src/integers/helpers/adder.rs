2cbce7a64b3c67c46c84a38c050c939aad5b2ac3
howardwu <9260812+howardwu@users.noreply.github.com>
2022-01-17 21:32:53 -0800

    WIP adder tests

--- circuits/src/integers/helpers/adder.rs
+++ circuits/src/integers/helpers/adder.rs
@@ -61,6 +61,7 @@ mod tests {
     use super::*;
     use crate::{Circuit, Mode};
 
+    #[rustfmt::skip]
     fn check_adder(
         name: &str,
         expected_sum: bool,
@@ -74,39 +75,37 @@ mod tests {
         num_constraints: usize,
     ) {
         Circuit::scoped(name, |scope| {
+            let case = format!("({} ADD {} WITH {})", a.eject_value(), b.eject_value(), c.eject_value());
+
             let (candidate_sum, candidate_carry) = a.adder(&b, &c);
+
             assert_eq!(
                 expected_sum,
                 candidate_sum.eject_value(),
-                "{} != {} := ({} ADDER {} WITH {} CARRY)",
+                "SUM {} != {} := {}",
                 expected_sum,
                 candidate_sum.eject_value(),
-                a.eject_value(),
-                b.eject_value(),
-                c.eject_value()
+                case
             );
             assert_eq!(
                 expected_carry,
                 candidate_carry.eject_value(),
-                "{} != {} := ({} ADDER {} WITH {} CARRY)",
+                "CARRY {} != {} := {}",
                 expected_carry,
                 candidate_carry.eject_value(),
-                a.eject_value(),
-                b.eject_value(),
-                c.eject_value()
+                case
             );
 
-            assert_eq!(num_constants, scope.num_constants_in_scope());
-            assert_eq!(num_public, scope.num_public_in_scope());
-            assert_eq!(num_private, scope.num_private_in_scope());
-            assert_eq!(num_constraints, scope.num_constraints_in_scope());
-            assert!(Circuit::is_satisfied());
+            assert_eq!(num_constants, scope.num_constants_in_scope(), "{} (num_constants)", case);
+            assert_eq!(num_public, scope.num_public_in_scope(), "{} (num_public)", case);
+            assert_eq!(num_private, scope.num_private_in_scope(), "{} (num_private)", case);
+            assert_eq!(num_constraints, scope.num_constraints_in_scope(), "{} (num_constraints)", case);
+            assert!(Circuit::is_satisfied(), "{}(is_satisfied)", case);
         });
     }
 
-    /// This method checks the truth table for the full adder.
     #[rustfmt::skip]
-    fn run_test(
+    fn check_false_add_false_with_false(
         mode_a: Mode,
         mode_b: Mode,
         mode_c: Mode,
@@ -115,96 +114,208 @@ mod tests {
         num_private: usize,
         num_constraints: usize
     ) {
-        // false AND false WITH false => (false, false)
+        // false ADD false WITH false => (false, false)
         let expected_sum = false;
         let expected_carry = false;
         let a = Boolean::<Circuit>::new(mode_a, false);
         let b = Boolean::<Circuit>::new(mode_b, false);
         let c = Boolean::<Circuit>::new(mode_c, false);
-        check_adder("false AND false WITH false", expected_sum, expected_carry, a, b, c, num_constants, num_public, num_private, num_constraints);
+        check_adder("false ADD false WITH false", expected_sum, expected_carry, a, b, c, num_constants, num_public, num_private, num_constraints);
+    }
 
-        // false AND false WITH true => (true, false)
+    #[rustfmt::skip]
+    fn check_false_add_false_with_true(
+        mode_a: Mode,
+        mode_b: Mode,
+        mode_c: Mode,
+        num_constants: usize,
+        num_public: usize,
+        num_private: usize,
+        num_constraints: usize
+    ) {
+        // false ADD false WITH true => (true, false)
         let expected_sum = true;
         let expected_carry = false;
         let a = Boolean::<Circuit>::new(mode_a, false);
         let b = Boolean::<Circuit>::new(mode_b, false);
         let c = Boolean::<Circuit>::new(mode_c, true);
-        check_adder("false AND false WITH true", expected_sum, expected_carry, a, b, c, num_constants, num_public, num_private, num_constraints);
+        check_adder("false ADD false WITH true", expected_sum, expected_carry, a, b, c, num_constants, num_public, num_private, num_constraints);
+    }
 
-        // false AND true WITH false => (true, false)
+    #[rustfmt::skip]
+    fn check_false_add_true_with_false(
+        mode_a: Mode,
+        mode_b: Mode,
+        mode_c: Mode,
+        num_constants: usize,
+        num_public: usize,
+        num_private: usize,
+        num_constraints: usize
+    ) {
+        // false ADD true WITH false => (true, false)
         let expected_sum = true;
         let expected_carry = false;
         let a = Boolean::<Circuit>::new(mode_a, false);
         let b = Boolean::<Circuit>::new(mode_b, true);
         let c = Boolean::<Circuit>::new(mode_c, false);
-        check_adder("false AND true WITH false", expected_sum, expected_carry, a, b, c, num_constants, num_public, num_private, num_constraints);
+        check_adder("false ADD true WITH false", expected_sum, expected_carry, a, b, c, num_constants, num_public, num_private, num_constraints);
+    }
 
-        // false AND true WITH true => (false, true)
+    #[rustfmt::skip]
+    fn check_false_add_true_with_true(
+        mode_a: Mode,
+        mode_b: Mode,
+        mode_c: Mode,
+        num_constants: usize,
+        num_public: usize,
+        num_private: usize,
+        num_constraints: usize
+    ) {
+        // false ADD true WITH true => (false, true)
         let expected_sum = false;
         let expected_carry = true;
         let a = Boolean::<Circuit>::new(mode_a, false);
         let b = Boolean::<Circuit>::new(mode_b, true);
         let c = Boolean::<Circuit>::new(mode_c, true);
-        check_adder("false AND true WITH true", expected_sum, expected_carry, a, b, c, num_constants, num_public, num_private, num_constraints);
+        check_adder("false ADD true WITH true", expected_sum, expected_carry, a, b, c, num_constants, num_public, num_private, num_constraints);
+    }
 
-        // true AND false WITH false => (true, false)
+    #[rustfmt::skip]
+    fn check_true_add_false_with_false(
+        mode_a: Mode,
+        mode_b: Mode,
+        mode_c: Mode,
+        num_constants: usize,
+        num_public: usize,
+        num_private: usize,
+        num_constraints: usize
+    ) {
+        // true ADD false WITH false => (true, false)
         let expected_sum = true;
         let expected_carry = false;
         let a = Boolean::<Circuit>::new(mode_a, true);
         let b = Boolean::<Circuit>::new(mode_b, false);
         let c = Boolean::<Circuit>::new(mode_c, false);
-        check_adder("true AND false WITH false", expected_sum, expected_carry, a, b, c, num_constants, num_public, num_private, num_constraints);
+        check_adder("true ADD false WITH false", expected_sum, expected_carry, a, b, c, num_constants, num_public, num_private, num_constraints);
+    }
 
-        // true AND false WITH true => (false, true)
+    #[rustfmt::skip]
+    fn check_true_add_false_with_true(
+        mode_a: Mode,
+        mode_b: Mode,
+        mode_c: Mode,
+        num_constants: usize,
+        num_public: usize,
+        num_private: usize,
+        num_constraints: usize
+    ) {
+        // true ADD false WITH true => (false, true)
         let expected_sum = false;
         let expected_carry = true;
         let a = Boolean::<Circuit>::new(mode_a, true);
         let b = Boolean::<Circuit>::new(mode_b, false);
         let c = Boolean::<Circuit>::new(mode_c, true);
-        check_adder("true AND false WITH true", expected_sum, expected_carry, a, b, c, num_constants, num_public, num_private, num_constraints);
+        check_adder("true ADD false WITH true", expected_sum, expected_carry, a, b, c, num_constants, num_public, num_private, num_constraints);
+    }
 
-        // true AND true WITH false => (false, true)
+    #[rustfmt::skip]
+    fn check_true_add_true_with_false(
+        mode_a: Mode,
+        mode_b: Mode,
+        mode_c: Mode,
+        num_constants: usize,
+        num_public: usize,
+        num_private: usize,
+        num_constraints: usize
+    ) {
+        // true ADD true WITH false => (false, true)
         let expected_sum = false;
         let expected_carry = true;
         let a = Boolean::<Circuit>::new(mode_a, true);
         let b = Boolean::<Circuit>::new(mode_b, true);
         let c = Boolean::<Circuit>::new(mode_c, false);
-        check_adder("true AND true WITH false", expected_sum, expected_carry, a, b, c, num_constants, num_public, num_private, num_constraints);
+        check_adder("true ADD true WITH false", expected_sum, expected_carry, a, b, c, num_constants, num_public, num_private, num_constraints);
+    }
 
-        // true AND true WITH true => (true, true)
+    #[rustfmt::skip]
+    fn check_true_add_true_with_true(
+        mode_a: Mode,
+        mode_b: Mode,
+        mode_c: Mode,
+        num_constants: usize,
+        num_public: usize,
+        num_private: usize,
+        num_constraints: usize
+    ) {
+        // true ADD true WITH true => (true, true)
         let expected_sum = true;
         let expected_carry = true;
         let a = Boolean::<Circuit>::new(mode_a, true);
         let b = Boolean::<Circuit>::new(mode_b, true);
         let c = Boolean::<Circuit>::new(mode_c, true);
-        check_adder("true AND true WITH true", expected_sum, expected_carry, a, b, c, num_constants, num_public, num_private, num_constraints);
+        check_adder("true ADD true WITH true", expected_sum, expected_carry, a, b, c, num_constants, num_public, num_private, num_constraints);
     }
 
     #[test]
     fn test_constant_add_constant_with_constant() {
-        run_test(Mode::Constant, Mode::Constant, Mode::Constant, 0, 0, 0, 0)
+        check_false_add_false_with_false(Mode::Constant, Mode::Constant, Mode::Constant, 0, 0, 0, 0);
+        check_false_add_false_with_true(Mode::Constant, Mode::Constant, Mode::Constant, 0, 0, 0, 0);
+        check_false_add_true_with_false(Mode::Constant, Mode::Constant, Mode::Constant, 0, 0, 0, 0);
+        check_false_add_true_with_true(Mode::Constant, Mode::Constant, Mode::Constant, 0, 0, 0, 0);
+        check_true_add_false_with_false(Mode::Constant, Mode::Constant, Mode::Constant, 0, 0, 0, 0);
+        check_true_add_false_with_true(Mode::Constant, Mode::Constant, Mode::Constant, 0, 0, 0, 0);
+        check_true_add_true_with_false(Mode::Constant, Mode::Constant, Mode::Constant, 0, 0, 0, 0);
+        check_true_add_true_with_true(Mode::Constant, Mode::Constant, Mode::Constant, 0, 0, 0, 0);
     }
 
     #[test]
     fn test_constant_add_constant_with_public() {
-        run_test(Mode::Constant, Mode::Constant, Mode::Public, 0, 0, 0, 0)
+        check_false_add_false_with_false(Mode::Constant, Mode::Constant, Mode::Public, 0, 0, 0, 0);
+        check_false_add_false_with_true(Mode::Constant, Mode::Constant, Mode::Public, 0, 0, 0, 0);
+        check_false_add_true_with_false(Mode::Constant, Mode::Constant, Mode::Public, 0, 0, 0, 0);
+        check_false_add_true_with_true(Mode::Constant, Mode::Constant, Mode::Public, 0, 0, 0, 0);
+        check_true_add_false_with_false(Mode::Constant, Mode::Constant, Mode::Public, 0, 0, 0, 0);
+        check_true_add_false_with_true(Mode::Constant, Mode::Constant, Mode::Public, 0, 0, 0, 0);
+        check_true_add_true_with_false(Mode::Constant, Mode::Constant, Mode::Public, 0, 0, 0, 0);
+        check_true_add_true_with_true(Mode::Constant, Mode::Constant, Mode::Public, 0, 0, 0, 0);
     }
 
     #[test]
     fn test_constant_add_constant_with_private() {
-        run_test(Mode::Constant, Mode::Constant, Mode::Private, 0, 0, 0, 0)
+        check_false_add_false_with_false(Mode::Constant, Mode::Constant, Mode::Private, 0, 0, 0, 0);
+        check_false_add_false_with_true(Mode::Constant, Mode::Constant, Mode::Private, 0, 0, 0, 0);
+        check_false_add_true_with_false(Mode::Constant, Mode::Constant, Mode::Private, 0, 0, 0, 0);
+        check_false_add_true_with_true(Mode::Constant, Mode::Constant, Mode::Private, 0, 0, 0, 0);
+        check_true_add_false_with_false(Mode::Constant, Mode::Constant, Mode::Private, 0, 0, 0, 0);
+        check_true_add_false_with_true(Mode::Constant, Mode::Constant, Mode::Private, 0, 0, 0, 0);
+        check_true_add_true_with_false(Mode::Constant, Mode::Constant, Mode::Private, 0, 0, 0, 0);
+        check_true_add_true_with_true(Mode::Constant, Mode::Constant, Mode::Private, 0, 0, 0, 0);
     }
 
     #[test]
     fn test_constant_add_public_with_constant() {
-        run_test(Mode::Constant, Mode::Public, Mode::Constant, 0, 0, 1, 0)
+        check_false_add_false_with_false(Mode::Constant, Mode::Public, Mode::Constant, 0, 0, 0, 0);
+        check_false_add_false_with_true(Mode::Constant, Mode::Public, Mode::Constant, 0, 0, 0, 0);
+        check_false_add_true_with_false(Mode::Constant, Mode::Public, Mode::Constant, 0, 0, 0, 0);
+        check_false_add_true_with_true(Mode::Constant, Mode::Public, Mode::Constant, 0, 0, 0, 0);
+        check_true_add_false_with_false(Mode::Constant, Mode::Public, Mode::Constant, 0, 0, 0, 0);
+        check_true_add_false_with_true(Mode::Constant, Mode::Public, Mode::Constant, 0, 0, 1, 2); // <- Differs
+        check_true_add_true_with_false(Mode::Constant, Mode::Public, Mode::Constant, 0, 0, 0, 0);
+        check_true_add_true_with_true(Mode::Constant, Mode::Public, Mode::Constant, 0, 0, 1, 2); // <- Differs
     }
 
-    // #[test]
-    // fn test_constant_add_public_with_public() {
-    //     run_test(Mode::Constant, Mode::Public, Mode::Public, 0, 0, 0, 0)
-    // }
-    //
+    #[test]
+    fn test_constant_add_public_with_public() {
+        check_false_add_false_with_false(Mode::Constant, Mode::Public, Mode::Public, 0, 0, 2, 4);
+        check_false_add_false_with_true(Mode::Constant, Mode::Public, Mode::Public, 0, 0, 2, 4);
+        check_false_add_true_with_false(Mode::Constant, Mode::Public, Mode::Public, 0, 0, 2, 4);
+        check_false_add_true_with_true(Mode::Constant, Mode::Public, Mode::Public, 0, 0, 2, 4);
+        check_true_add_false_with_false(Mode::Constant, Mode::Public, Mode::Public, 0, 0, 3, 6); // <- Differs
+        check_true_add_false_with_true(Mode::Constant, Mode::Public, Mode::Public, 0, 0, 3, 6); // <- Differs
+        check_true_add_true_with_false(Mode::Constant, Mode::Public, Mode::Public, 0, 0, 3, 6); // <- Differs
+        check_true_add_true_with_true(Mode::Constant, Mode::Public, Mode::Public, 0, 0, 3, 6); // <- Differs
+    }
+
     // #[test]
     // fn test_constant_add_public_with_private() {
     //     run_test(Mode::Constant, Mode::Public, Mode::Private, 0, 0, 0, 0)
