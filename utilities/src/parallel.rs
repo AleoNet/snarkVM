4a838e136403458f9b3bc0f7c1254e3bb69320dc
Pratyush Mishra <pratyushmishra@berkeley.edu>
2022-01-17 23:01:00 -0800

    Try new parallelization strategy

--- utilities/src/parallel.rs
+++ utilities/src/parallel.rs
@@ -49,54 +49,30 @@ impl<'a, T> ExecutionPool<'a, T> {
     {
         #[cfg(feature = "parallel")]
         {
-            use itertools::Itertools;
-            // At most 12 threads per task
-            const THREADS_PER_TASK: usize = 12;
-            let threads_per_task = if rayon::current_num_threads() > self.tasks.len() * THREADS_PER_TASK {
-                rayon::current_num_threads() / self.tasks.len()
-            } else {
-                THREADS_PER_TASK.min(rayon::current_num_threads())
-            };
-            let task_chunk_size = rayon::current_num_threads() / threads_per_task
-                + usize::from(rayon::current_num_threads() % threads_per_task != 0);
-
-            if (threads_per_task as f64 / rayon::current_num_threads() as f64) < 1.0 {
+            const THRESHOLD: usize = 12;
+            if max_available_threads() > THRESHOLD {
+                /// We have more threads than the threshold
                 use std::sync::{Arc, RwLock};
                 let mut task_results = Vec::with_capacity(self.tasks.len());
                 for _ in 0..self.tasks.len() {
                     task_results.push(RwLock::new(None));
                 }
                 let task_results = Arc::new(task_results);
-                self.tasks
-                    .into_iter()
-                    .chunks(task_chunk_size)
-                    .into_iter()
-                    .enumerate()
-                    .for_each(|(i, task_set): (usize, _)| {
-                        let task_set = task_set.collect::<Vec<_>>();
-                        rayon::scope(|s| {
-                            let mut remaining_threads = rayon::current_num_threads();
-                            for (j, task) in task_set.into_iter().enumerate() {
-                                let num_threads = remaining_threads
-                                    .checked_sub(threads_per_task)
-                                    .map_or(remaining_threads, |_| threads_per_task);
-                                let task_results_scoped = Arc::clone(&task_results);
-                                s.spawn(move |_| {
-                                    rayon::ThreadPoolBuilder::new()
-                                        .num_threads(num_threads)
-                                        .build()
-                                        .expect("could not build threadpool")
-                                        .install(|| {
-                                            let result = task();
-                                            *task_results_scoped[i * task_chunk_size + j]
-                                                .write()
-                                                .expect("could not get mutex") = Some(result);
-                                        })
-                                });
-                                remaining_threads = remaining_threads.checked_sub(num_threads).unwrap();
-                            }
+                rayon::scope(|s| {
+                    for (j, task) in self.tasks.into_iter().enumerate() {
+                        let task_results = Arc::clone(&task_results);
+                        s.spawn(move |_| {
+                            rayon::ThreadPoolBuilder::new()
+                                .num_threads(THRESHOLD)
+                                .build()
+                                .expect("could not build threadpool")
+                                .install(|| {
+                                    let result = task();
+                                    *task_results[j].write().expect("could not get mutex") = Some(result);
+                                })
                         });
-                    });
+                    }
+                });
                 match Arc::try_unwrap(task_results) {
                     Ok(inner) => inner
                         .into_iter()
@@ -111,6 +87,19 @@ impl<'a, T> ExecutionPool<'a, T> {
             } else {
                 self.tasks.into_iter().map(|f| f()).collect()
             }
+            // // At most 12 threads per task
+            // const THREADS_PER_TASK: usize = 10;
+            // let threads_per_task = if rayon::current_num_threads() > self.tasks.len() * THREADS_PER_TASK {
+            //     rayon::current_num_threads() / self.tasks.len()
+            // } else {
+            //     THREADS_PER_TASK.min(rayon::current_num_threads())
+            // };
+            // let task_chunk_size = rayon::current_num_threads() / threads_per_task
+            //     + usize::from(rayon::current_num_threads() % threads_per_task != 0);
+
+            // if (threads_per_task as f64 / rayon::current_num_threads() as f64) < 1.0 {
+            // } else {
+            // }
         }
         #[cfg(not(feature = "parallel"))]
         {
