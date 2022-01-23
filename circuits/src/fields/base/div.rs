1d0ee83754d2e235c0710836ff4b86c812554486
howardwu <9260812+howardwu@users.noreply.github.com>
2022-01-23 11:11:20 -0800

    Update the BaseField::div test infra

--- circuits/src/fields/base/div.rs
+++ circuits/src/fields/base/div.rs
@@ -54,77 +54,84 @@ mod tests {
 
     const ITERATIONS: usize = 25;
 
-    #[test]
-    fn test_div() {
+    fn check_div(
+        mode_a: Mode,
+        mode_b: Mode,
+        num_constants: usize,
+        num_public: usize,
+        num_private: usize,
+        num_constraints: usize
+    ) {
         let one = <Circuit as Environment>::BaseField::one();
 
-        // Constant variables
         let mut dividend = one;
         for i in 0..ITERATIONS {
             let mut divisor = one;
             for j in 0..ITERATIONS {
-                Circuit::scoped(&format!("Constant - ({}, {})", i, j), |scope| {
+                let a = BaseField::<Circuit>::new(mode_a, dividend);
+                let b = BaseField::new(mode_b, divisor);
+
+                Circuit::scoped(&format!("{:?} / {:?} - ({}, {})", mode_a, mode_b, i, j), |scope| {
                     let expected_quotient = dividend / divisor;
-                    let candidate_quotient =
-                        BaseField::<Circuit>::new(Mode::Constant, dividend) / BaseField::new(Mode::Constant, divisor);
+                    let candidate_quotient = a / b;
                     assert_eq!(expected_quotient, candidate_quotient.eject_value());
 
-                    assert_eq!(3, scope.num_constants_in_scope());
-                    assert_eq!(0, scope.num_public_in_scope());
-                    assert_eq!(0, scope.num_private_in_scope());
-                    assert_eq!(0, scope.num_constraints_in_scope());
+                    assert_eq!(num_constants, scope.num_constants_in_scope(), "(num_constants)");
+                    assert_eq!(num_public, scope.num_public_in_scope(), "(num_public)");
+                    assert_eq!(num_private, scope.num_private_in_scope(), "(num_private)");
+                    assert_eq!(num_constraints, scope.num_constraints_in_scope(), "(num_constraints)");
+                    assert!(Circuit::is_satisfied(), "(is_satisfied)");
 
                     divisor += one;
                 });
             }
             dividend += one;
         }
+    }
 
-        // Public variables
-        let mut dividend = one;
-        for i in 0..ITERATIONS {
-            let mut divisor = one;
-            for j in 0..ITERATIONS {
-                Circuit::scoped(&format!("Public - ({}, {})", i, j), |scope| {
-                    let expected_quotient = dividend / divisor;
-                    let candidate_quotient =
-                        BaseField::<Circuit>::new(Mode::Public, dividend) / BaseField::new(Mode::Public, divisor);
-                    assert_eq!(expected_quotient, candidate_quotient.eject_value());
+    #[test]
+    fn test_constant_div_constant() {
+        check_div(Mode::Constant, Mode::Constant, 1, 0, 0, 0);
+    }
 
-                    assert_eq!(0, scope.num_constants_in_scope());
-                    assert_eq!(2, scope.num_public_in_scope());
-                    assert_eq!(2, scope.num_private_in_scope());
-                    assert_eq!(2, scope.num_constraints_in_scope());
-                    assert!(scope.is_satisfied());
+    #[test]
+    fn test_constant_div_public() {
+        check_div(Mode::Constant, Mode::Public, 0, 0, 1, 1);
+    }
 
-                    divisor += one;
-                });
-            }
-            dividend += one;
-        }
+    #[test]
+    fn test_constant_div_private() {
+        check_div(Mode::Constant, Mode::Private, 0, 0, 1, 1);
+    }
 
-        // Private variables
-        let mut dividend = one;
-        for i in 0..ITERATIONS {
-            let mut divisor = one;
-            for j in 0..ITERATIONS {
-                Circuit::scoped(&format!("Private - ({}, {})", i, j), |scope| {
-                    let expected_quotient = dividend / divisor;
-                    let candidate_quotient =
-                        BaseField::<Circuit>::new(Mode::Private, dividend) / BaseField::new(Mode::Private, divisor);
-                    assert_eq!(expected_quotient, candidate_quotient.eject_value());
+    #[test]
+    fn test_public_div_constant() {
+        check_div(Mode::Public, Mode::Constant, 1, 0, 0, 0);
+    }
 
-                    assert_eq!(0, scope.num_constants_in_scope());
-                    assert_eq!(0, scope.num_public_in_scope());
-                    assert_eq!(4, scope.num_private_in_scope());
-                    assert_eq!(2, scope.num_constraints_in_scope());
-                    assert!(scope.is_satisfied());
+    #[test]
+    fn test_public_div_public() {
+        check_div(Mode::Public, Mode::Public, 0, 0, 2, 2);
+    }
 
-                    divisor += one;
-                });
-            }
-            dividend += one;
-        }
+    #[test]
+    fn test_public_div_private() {
+        check_div(Mode::Public, Mode::Private, 0, 0, 2, 2);
+    }
+
+    #[test]
+    fn test_private_div_constant() {
+        check_div(Mode::Private, Mode::Constant, 1, 0, 0, 0);
+    }
+
+    #[test]
+    fn test_private_div_public() {
+        check_div(Mode::Private, Mode::Public, 0, 0, 2, 2);
+    }
+
+    #[test]
+    fn test_private_div_private() {
+        check_div(Mode::Private, Mode::Private, 0, 0, 2, 2);
     }
 
     #[test]
