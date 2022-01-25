9faa8162e43aaa33e9dc73ae2a1d4348373c8ad9
ljedrz <ljedrz@gmail.com>
2022-01-25 12:43:26 +0100

    refactor: split msm_standard into 3 functions
    Signed-off-by: ljedrz <ljedrz@gmail.com>

--- algorithms/src/msm/variable_base/standard.rs
+++ algorithms/src/msm/variable_base/standard.rs
@@ -21,6 +21,67 @@ use snarkvm_utilities::BigInteger;
 #[cfg(feature = "parallel")]
 use rayon::prelude::*;
 
+fn update_buckets<G: AffineCurve>(
+    base: &G,
+    mut scalar: <G::ScalarField as PrimeField>::BigInteger,
+    w_start: usize,
+    c: usize,
+    buckets: &mut [G::Projective],
+) {
+    // We right-shift by w_start, thus getting rid of the
+    // lower bits.
+    scalar.divn(w_start as u32);
+
+    // We mod the remaining bits by the window size.
+    let scalar = scalar.as_ref()[0] % (1 << c);
+
+    // If the scalar is non-zero, we update the corresponding
+    // bucket.
+    // (Recall that `buckets` doesn't have a zero bucket.)
+    if scalar != 0 {
+        buckets[(scalar - 1) as usize].add_assign_mixed(base);
+    }
+}
+
+fn process_window<G: AffineCurve>(
+    bases: &[G],
+    scalars: &[<G::ScalarField as PrimeField>::BigInteger],
+    w_start: usize,
+    c: usize,
+) -> G::Projective {
+    let mut res = G::Projective::zero();
+    let fr_one = G::ScalarField::one().to_repr();
+
+    // We only process unit scalars once in the first window.
+    if w_start == 0 {
+        scalars
+            .iter()
+            .zip(bases)
+            .filter(|(&s, _)| s == fr_one)
+            .for_each(|(_, base)| {
+                res.add_assign_mixed(base);
+            });
+    }
+
+    // We don't need the "zero" bucket, so we only have 2^c - 1 buckets
+    let mut buckets = vec![G::Projective::zero(); (1 << c) - 1];
+    scalars
+        .iter()
+        .zip(bases)
+        .filter(|(&s, _)| s > fr_one)
+        .for_each(|(&scalar, base)| update_buckets(base, scalar, w_start, c, &mut buckets));
+    // G::Projective::batch_normalization(&mut buckets);
+
+    for running_sum in buckets.into_iter().rev().scan(G::Projective::zero(), |sum, b| {
+        *sum += b;
+        Some(*sum)
+    }) {
+        res += running_sum;
+    }
+
+    res
+}
+
 pub fn msm_standard<G: AffineCurve>(
     bases: &[G],
     scalars: &[<G::ScalarField as PrimeField>::BigInteger],
@@ -32,9 +93,6 @@ pub fn msm_standard<G: AffineCurve>(
     };
 
     let num_bits = <G::ScalarField as PrimeField>::Parameters::MODULUS_BITS as usize;
-    let fr_one = G::ScalarField::one().to_repr();
-
-    let zero = G::zero().into_projective();
 
     // Each window is of size `c`.
     // We divide up the bits 0..num_bits into windows of size `c`, and
@@ -42,60 +100,22 @@ pub fn msm_standard<G: AffineCurve>(
     // window_starts.into_iter() //
     let window_sums: Vec<_> = crate::cfg_into_iter!(0..num_bits)
         .step_by(c)
-        .map(|w_start| {
-            let mut res = zero;
-            // We don't need the "zero" bucket, so we only have 2^c - 1 buckets
-            let mut buckets = vec![zero; (1 << c) - 1];
-            scalars
-                .iter()
-                .zip(bases)
-                .filter(|(s, _)| !s.is_zero())
-                .for_each(|(&scalar, base)| {
-                    if scalar == fr_one {
-                        // We only process unit scalars once in the first window.
-                        if w_start == 0 {
-                            res.add_assign_mixed(base);
-                        }
-                    } else {
-                        let mut scalar = scalar;
-
-                        // We right-shift by w_start, thus getting rid of the
-                        // lower bits.
-                        scalar.divn(w_start as u32);
-
-                        // We mod the remaining bits by the window size.
-                        let scalar = scalar.as_ref()[0] % (1 << c);
-
-                        // If the scalar is non-zero, we update the corresponding
-                        // bucket.
-                        // (Recall that `buckets` doesn't have a zero bucket.)
-                        if scalar != 0 {
-                            buckets[(scalar - 1) as usize].add_assign_mixed(base);
-                        }
-                    }
-                });
-            // G::Projective::batch_normalization(&mut buckets);
-
-            let mut running_sum = G::Projective::zero();
-            for b in buckets.into_iter().rev() {
-                running_sum += b;
-                // running_sum.add_assign_mixed(&b);
-                res += running_sum;
-            }
-
-            res
-        })
+        .map(|w_start| process_window(bases, scalars, w_start, c))
         .collect();
 
     // We store the sum for the lowest window.
-    let lowest = window_sums.first().unwrap();
+    let (lowest, window_sums) = window_sums.split_first().unwrap();
 
     // We're traversing windows from high to low.
-    window_sums[1..].iter().rev().fold(zero, |mut total, sum_i| {
-        total += sum_i;
-        for _ in 0..c {
-            total.double_in_place();
-        }
-        total
-    }) + lowest
+    window_sums
+        .iter()
+        .rev()
+        .fold(G::Projective::zero(), |mut total, sum_i| {
+            total += sum_i;
+            for _ in 0..c {
+                total.double_in_place();
+            }
+            total
+        })
+        + lowest
 }
