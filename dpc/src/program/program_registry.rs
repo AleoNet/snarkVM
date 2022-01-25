5811f0153dc5c321735b42e3e123ca6d739bd655
raychu86 <14917648+raychu86@users.noreply.github.com>
2022-01-24 17:27:45 -0800

    Minor update to program registry structure

--- dpc/src/program/program_registry.rs
+++ dpc/src/program/program_registry.rs
@@ -26,10 +26,10 @@ use std::{collections::HashMap, sync::Arc};
 #[derivative(Debug(bound = "N: Network"))]
 pub struct ProgramRegistry<N: Network> {
     #[derivative(Debug = "ignore")]
-    tree: MerkleTree<N::ProgramIDParameters>,
+    tree: MerkleTree<N::ProgramIDParameters>, // TODO (raychu86): Possibly introduce a new parameter here.
     #[derivative(Debug = "ignore")]
-    functions: HashMap<N::FunctionID, <N::ProgramSNARK as SNARK>::VerifyingKey>,
-    num_functions: u32,
+    programs: HashMap<N::ProgramID, HashMap<N::FunctionID, <N::ProgramSNARK as SNARK>::VerifyingKey>>,
+    num_programs: u32,
 }
 
 // TODO: Implement program deployment transaction, storage of vks, etc.
@@ -46,37 +46,57 @@ pub struct ProgramRegistry<N: Network> {
 // 3. Standard transactions must check that the vk exists in the program registry
 
 impl<N: Network> ProgramRegistry<N> {
-    pub fn new(function_vks: Vec<<N::ProgramSNARK as SNARK>::VerifyingKey>) -> Result<Self> {
+    pub fn new() -> Result<Self> {
         // Initialize a new functions tree, and add all functions to the tree.
         let mut registry = Self {
             tree: MerkleTree::<N::ProgramIDParameters>::new::<N::FunctionID>(
                 Arc::new(N::program_id_parameters().clone()),
                 &[],
             )?,
-            functions: Default::default(),
-            num_functions: 0,
+            programs: Default::default(),
+            num_programs: 0,
         };
-        registry.add_all(function_vks)?;
+
+        // Add the noop program.
+        registry.add((
+            *N::noop_program_id(),
+            [(*N::noop_function_id(), N::noop_circuit_verifying_key().clone())].into(),
+        ))?;
 
         Ok(registry)
     }
 
-    /// Returns `true` if the given function ID exists in the program.
-    pub fn contains_function(&self, function_id: &N::FunctionID) -> bool {
-        self.functions.get(function_id).is_some()
+    /// Returns `true` if the given program ID exists in the program registry.
+    pub fn contains_program(&self, program_id: &N::ProgramID) -> bool {
+        self.programs.get(program_id).is_some()
     }
 
-    /// Adds all given function vks to the tree
-    fn add_all(&mut self, function_vks: Vec<<N::ProgramSNARK as SNARK>::VerifyingKey>) -> Result<()> {
+    /// Add a program vks to the tree
+    fn add(
+        &mut self,
+        program: (
+            N::ProgramID,
+            HashMap<N::FunctionID, <N::ProgramSNARK as SNARK>::VerifyingKey>,
+        ),
+    ) -> Result<()> {
         // Ensure the list of given function vks is non-empty.
-        if function_vks.is_empty() {
+        if program.1.is_empty() {
             return Err(anyhow!("The list of given function vks must be non-empty"));
         }
 
+        // TODO (raychu86): Enforce that the program vks match the program id.
+
         // Construct a list of function IDs.
-        let function_ids: Vec<_> = function_vks
+        let function_ids: Vec<N::FunctionID> = program
+            .1
             .iter()
-            .map(|vk| N::function_id_crh().hash_bits(&vk.to_minimal_bits()).unwrap().into())
+            .map(|(id, vk)| {
+                let function_id = N::function_id_crh().hash_bits(&vk.to_minimal_bits()).unwrap().into();
+
+                assert_eq!(&function_id, id);
+
+                function_id
+            })
             .collect();
 
         // Ensure the list of given function IDs is unique.
@@ -84,24 +104,14 @@ impl<N: Network> ProgramRegistry<N> {
             return Err(anyhow!("The list of given functions contains duplicates"));
         }
 
-        // Ensure the functions do not already exist in the tree.
-        if function_ids.iter().any(|id| self.contains_function(id)) {
-            return Err(anyhow!("Some given functions already exist in the program"));
+        // Ensure the program doe not already exist in the tree.
+        if self.contains_program(&program.0) {
+            return Err(anyhow!("The program already exists in the registry"));
         }
 
-        self.tree = self.tree.rebuild(self.num_functions as usize, &function_ids)?;
-
-        let num_functions = function_vks.len();
-
-        self.functions.extend(
-            function_ids
-                .into_iter()
-                .zip(function_vks)
-                .enumerate()
-                .map(|(_index, (id, vk))| (id, vk)),
-        );
-
-        self.num_functions += num_functions as u32;
+        self.tree = self.tree.rebuild(self.num_programs as usize, &[program.0])?;
+        self.programs.insert(program.0, program.1);
+        self.num_programs += 1;
 
         Ok(())
     }
