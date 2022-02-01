70c6bf28a2de0cc2a58751a04390fec2450dae52
howardwu <9260812+howardwu@users.noreply.github.com>
2022-01-31 21:45:08 -0800

    Remove basic constraints from polycommit

--- polycommit/src/lib.rs
+++ polycommit/src/lib.rs
@@ -69,10 +69,6 @@ use std::{
 pub mod data_structures;
 pub use data_structures::*;
 
-/// R1CS constraints for polynomial constraints.
-mod constraints;
-pub use constraints::*;
-
 /// Errors pertaining to query sets.
 pub mod error;
 pub use error::*;
