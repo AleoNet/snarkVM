c56794f6209bfe3ddf512b016af08243b8239efe
howardwu <9260812+howardwu@users.noreply.github.com>
2022-02-13 14:22:22 -0800

    Update the integer helper design

--- circuits/src/helpers.rs
+++ circuits/src/helpers.rs
@@ -75,8 +75,7 @@ pub(crate) mod integers {
     impl IntegerType for u64 {}
     impl IntegerType for u128 {}
 
-    // TODO (@pranav) Fold this macro into wrapping_impl.
-    macro_rules! checked_pow_impl {
+    macro_rules! checked_impl {
         ($trait_name:ident, $method:ident, $t:ty) => {
             impl $trait_name for $t {
                 #[inline]
@@ -86,48 +85,6 @@ pub(crate) mod integers {
             }
         };
     }
-
-    // TODO (@pranav) Fold this macro into wrapping_impl.
-    macro_rules! wrapping_pow_impl {
-        ($trait_name:ident, $method:ident, $t:ty) => {
-            impl $trait_name for $t {
-                #[inline]
-                fn $method(&self, rhs: u32) -> $t {
-                    <$t>::$method(*self, rhs)
-                }
-            }
-        };
-    }
-
-    pub trait CheckedPow: Sized {
-        fn checked_pow(&self, v: u32) -> Option<Self>;
-    }
-
-    pub trait WrappingPow: Sized {
-        fn wrapping_pow(&self, v: u32) -> Self;
-    }
-
-    checked_pow_impl!(CheckedPow, checked_pow, u8);
-    checked_pow_impl!(CheckedPow, checked_pow, u16);
-    checked_pow_impl!(CheckedPow, checked_pow, u32);
-    checked_pow_impl!(CheckedPow, checked_pow, u64);
-    checked_pow_impl!(CheckedPow, checked_pow, u128);
-    checked_pow_impl!(CheckedPow, checked_pow, i8);
-    checked_pow_impl!(CheckedPow, checked_pow, i16);
-    checked_pow_impl!(CheckedPow, checked_pow, i32);
-    checked_pow_impl!(CheckedPow, checked_pow, i64);
-    checked_pow_impl!(CheckedPow, checked_pow, i128);
-
-    wrapping_pow_impl!(WrappingPow, wrapping_pow, u8);
-    wrapping_pow_impl!(WrappingPow, wrapping_pow, u16);
-    wrapping_pow_impl!(WrappingPow, wrapping_pow, u32);
-    wrapping_pow_impl!(WrappingPow, wrapping_pow, u64);
-    wrapping_pow_impl!(WrappingPow, wrapping_pow, u128);
-    wrapping_pow_impl!(WrappingPow, wrapping_pow, i8);
-    wrapping_pow_impl!(WrappingPow, wrapping_pow, i16);
-    wrapping_pow_impl!(WrappingPow, wrapping_pow, i32);
-    wrapping_pow_impl!(WrappingPow, wrapping_pow, i64);
-    wrapping_pow_impl!(WrappingPow, wrapping_pow, i128);
 
     macro_rules! wrapping_impl {
         ($trait_name:ident, $method:ident, $t:ty) => {
@@ -139,44 +96,14 @@ pub(crate) mod integers {
             }
         };
         ($trait_name:ident, $method:ident, $t:ty, $rhs:ty) => {
-            impl $trait_name<$rhs> for $t {
+            impl $trait_name for $t {
                 #[inline]
-                fn $method(&self, v: &$rhs) -> Self {
-                    <$t>::$method(*self, *v)
+                fn $method(&self, v: $rhs) -> Self {
+                    <$t>::$method(*self, v)
                 }
             }
         };
     }
-
-    pub trait WrappingDiv: Sized + Div<Self, Output = Self> {
-        fn wrapping_div(&self, v: &Self) -> Self;
-    }
-
-    pub trait WrappingRem: Sized + Rem<Self, Output = Self> {
-        fn wrapping_rem(&self, v: &Self) -> Self;
-    }
-
-    wrapping_impl!(WrappingDiv, wrapping_div, u8);
-    wrapping_impl!(WrappingDiv, wrapping_div, u16);
-    wrapping_impl!(WrappingDiv, wrapping_div, u32);
-    wrapping_impl!(WrappingDiv, wrapping_div, u64);
-    wrapping_impl!(WrappingDiv, wrapping_div, u128);
-    wrapping_impl!(WrappingDiv, wrapping_div, i8);
-    wrapping_impl!(WrappingDiv, wrapping_div, i16);
-    wrapping_impl!(WrappingDiv, wrapping_div, i32);
-    wrapping_impl!(WrappingDiv, wrapping_div, i64);
-    wrapping_impl!(WrappingDiv, wrapping_div, i128);
-
-    wrapping_impl!(WrappingRem, wrapping_rem, u8);
-    wrapping_impl!(WrappingRem, wrapping_rem, u16);
-    wrapping_impl!(WrappingRem, wrapping_rem, u32);
-    wrapping_impl!(WrappingRem, wrapping_rem, u64);
-    wrapping_impl!(WrappingRem, wrapping_rem, u128);
-    wrapping_impl!(WrappingRem, wrapping_rem, i8);
-    wrapping_impl!(WrappingRem, wrapping_rem, i16);
-    wrapping_impl!(WrappingRem, wrapping_rem, i32);
-    wrapping_impl!(WrappingRem, wrapping_rem, i64);
-    wrapping_impl!(WrappingRem, wrapping_rem, i128);
 
     macro_rules! integer_properties_impl {
         ($t:ty, $dual:ty, $is_signed:expr) => {
@@ -195,6 +122,80 @@ pub(crate) mod integers {
         };
     }
 
+    pub trait CheckedPow: Sized {
+        fn checked_pow(&self, v: u32) -> Option<Self>;
+    }
+
+    checked_impl!(CheckedPow, checked_pow, u8);
+    checked_impl!(CheckedPow, checked_pow, u16);
+    checked_impl!(CheckedPow, checked_pow, u32);
+    checked_impl!(CheckedPow, checked_pow, u64);
+    checked_impl!(CheckedPow, checked_pow, u128);
+    checked_impl!(CheckedPow, checked_pow, i8);
+    checked_impl!(CheckedPow, checked_pow, i16);
+    checked_impl!(CheckedPow, checked_pow, i32);
+    checked_impl!(CheckedPow, checked_pow, i64);
+    checked_impl!(CheckedPow, checked_pow, i128);
+
+    pub trait WrappingDiv: Sized + Div<Self, Output = Self> {
+        fn wrapping_div(&self, v: &Self) -> Self;
+    }
+
+    wrapping_impl!(WrappingDiv, wrapping_div, u8);
+    wrapping_impl!(WrappingDiv, wrapping_div, u16);
+    wrapping_impl!(WrappingDiv, wrapping_div, u32);
+    wrapping_impl!(WrappingDiv, wrapping_div, u64);
+    wrapping_impl!(WrappingDiv, wrapping_div, u128);
+    wrapping_impl!(WrappingDiv, wrapping_div, i8);
+    wrapping_impl!(WrappingDiv, wrapping_div, i16);
+    wrapping_impl!(WrappingDiv, wrapping_div, i32);
+    wrapping_impl!(WrappingDiv, wrapping_div, i64);
+    wrapping_impl!(WrappingDiv, wrapping_div, i128);
+
+    pub trait WrappingRem: Sized + Rem<Self, Output = Self> {
+        fn wrapping_rem(&self, v: &Self) -> Self;
+    }
+
+    wrapping_impl!(WrappingRem, wrapping_rem, u8);
+    wrapping_impl!(WrappingRem, wrapping_rem, u16);
+    wrapping_impl!(WrappingRem, wrapping_rem, u32);
+    wrapping_impl!(WrappingRem, wrapping_rem, u64);
+    wrapping_impl!(WrappingRem, wrapping_rem, u128);
+    wrapping_impl!(WrappingRem, wrapping_rem, i8);
+    wrapping_impl!(WrappingRem, wrapping_rem, i16);
+    wrapping_impl!(WrappingRem, wrapping_rem, i32);
+    wrapping_impl!(WrappingRem, wrapping_rem, i64);
+    wrapping_impl!(WrappingRem, wrapping_rem, i128);
+
+    pub trait WrappingPow: Sized {
+        fn wrapping_pow(&self, v: u32) -> Self;
+    }
+
+    wrapping_impl!(WrappingPow, wrapping_pow, u8, u32);
+    wrapping_impl!(WrappingPow, wrapping_pow, u16, u32);
+    wrapping_impl!(WrappingPow, wrapping_pow, u32, u32);
+    wrapping_impl!(WrappingPow, wrapping_pow, u64, u32);
+    wrapping_impl!(WrappingPow, wrapping_pow, u128, u32);
+    wrapping_impl!(WrappingPow, wrapping_pow, i8, u32);
+    wrapping_impl!(WrappingPow, wrapping_pow, i16, u32);
+    wrapping_impl!(WrappingPow, wrapping_pow, i32, u32);
+    wrapping_impl!(WrappingPow, wrapping_pow, i64, u32);
+    wrapping_impl!(WrappingPow, wrapping_pow, i128, u32);
+
+    /// Properties common to all integer types.
+    pub trait IntegerProperties: PrimInt + Debug + Display {
+        type Dual: IntegerType;
+        /// Returns the number of bits required to represent this integer.
+        const BITS: usize;
+        /// Returns the maximum value representable by this integer.
+        const MAX: Self;
+        /// Returns the minimum value representable by this integer.
+        const MIN: Self;
+
+        /// Returns `true` if `Self` is a signed integer and `false` otherwise.
+        fn is_signed() -> bool;
+    }
+
     integer_properties_impl!(u8, i8, false);
     integer_properties_impl!(u16, i16, false);
     integer_properties_impl!(u32, i32, false);
@@ -205,20 +206,4 @@ pub(crate) mod integers {
     integer_properties_impl!(i32, u32, true);
     integer_properties_impl!(i64, u64, true);
     integer_properties_impl!(i128, u128, true);
-
-    /// Properties common to all integer types.
-    /// Note that `PrimInt` implements `Bounded` which implements
-    /// `min_value` and `max_value`.
-    pub trait IntegerProperties: PrimInt + Debug + Display {
-        type Dual: IntegerType;
-        /// Returns the number of bits required to represent this integer.
-        const BITS: usize;
-        /// Returns the maximum value representable by this integer.
-        const MAX: Self;
-        /// Returns the minimum value representable by this integer.
-        const MIN: Self;
-
-        /// Returns `true` if Self is a primitive signed integer and `false` otherwise.
-        fn is_signed() -> bool;
-    }
 }
