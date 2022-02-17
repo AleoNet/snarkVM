ab9ab9d34e8e65ea71d6389d5b9655b2e23957b5
Howard Wu <9260812+howardwu@users.noreply.github.com>
2022-02-16 16:51:30 -0800

    wip batch

--- algorithms/src/msm/variable_base/standard.rs
+++ algorithms/src/msm/variable_base/standard.rs
@@ -181,6 +181,7 @@ pub(super) fn msm_standard_batched<G: AffineCurve>(
 pub const BATCH_SIZE: usize = 4096;
 
 use core::cmp::Ordering;
+use voracious_radix_sort::*;
 
 #[cfg(not(feature = "std"))]
 use crate::fft::domain::log2;
@@ -197,10 +198,166 @@ impl PartialOrd for BucketPosition {
     }
 }
 
+impl Radixable<u32> for BucketPosition {
+    type Key = u32;
+
+    #[inline]
+    fn key(&self) -> Self::Key {
+        self.bucket
+    }
+}
+
 impl PartialEq for BucketPosition {
     fn eq(&self, other: &Self) -> bool {
         self.bucket == other.bucket
     }
+}
+
+#[inline]
+pub fn batch_bucketed_add_option_a<C: AffineCurve>(
+    buckets: usize,
+    elems: &[C],
+    bucket_positions: &mut [BucketPosition],
+) -> Vec<C> {
+    // assert_eq!(elems.len(), bucket_positions.len());
+    assert!(elems.len() > 0);
+
+    dlsd_radixsort(bucket_positions, 8);
+
+    let mut len = bucket_positions.len();
+    let mut all_ones = true;
+    let mut new_len = 0; // len counter
+    let mut glob = 0; // global counters
+    let mut loc = 1; // local counter
+    let mut batch = 0; // batch counter
+    let mut instr = Vec::<(u32, u32)>::with_capacity(BATCH_SIZE);
+    let mut new_elems = Vec::<C>::with_capacity(elems.len() * 3 / 8);
+
+    let mut scratch_space = Vec::<Option<C>>::with_capacity(BATCH_SIZE / 2);
+
+    // In the first loop, we copy the results of the first in place addition tree
+    // to a local vector, new_elems
+    // Subsequently, we perform all the operations in place
+    while glob < len {
+        let current_bucket = bucket_positions[glob].bucket;
+        while glob + 1 < len && bucket_positions[glob + 1].bucket == current_bucket {
+            glob += 1;
+            loc += 1;
+        }
+        if current_bucket >= buckets as u32 {
+            loc = 1;
+        } else if loc > 1 {
+            // all ones is false if next len is not 1
+            if loc > 2 {
+                all_ones = false;
+            }
+            let is_odd = loc % 2 == 1;
+            let half = loc / 2;
+            for i in 0..half {
+                instr.push((
+                    bucket_positions[glob - (loc - 1) + 2 * i].position,
+                    bucket_positions[glob - (loc - 1) + 2 * i + 1].position,
+                ));
+                bucket_positions[new_len + i] =
+                    BucketPosition { bucket: current_bucket, position: (new_len + i) as u32 };
+            }
+            if is_odd {
+                instr.push((bucket_positions[glob].position, !0u32));
+                bucket_positions[new_len + half] =
+                    BucketPosition { bucket: current_bucket, position: (new_len + half) as u32 };
+            }
+            // Reset the local_counter and update state
+            new_len += half + (loc % 2);
+            batch += half;
+            loc = 1;
+
+            if batch >= BATCH_SIZE / 2 {
+                // We need instructions for copying data in the case
+                // of noops. We encode noops/copies as !0u32
+                C::batch_add_write(&elems[..], &instr[..], &mut new_elems, &mut scratch_space);
+
+                instr.clear();
+                batch = 0;
+            }
+        } else {
+            instr.push((bucket_positions[glob].position, !0u32));
+            bucket_positions[new_len] = BucketPosition { bucket: current_bucket, position: new_len as u32 };
+            new_len += 1;
+        }
+        glob += 1;
+    }
+    if instr.len() > 0 {
+        C::batch_add_write(&elems[..], &instr[..], &mut new_elems, &mut scratch_space);
+        instr.clear();
+    }
+    glob = 0;
+    batch = 0;
+    loc = 1;
+    len = new_len;
+    new_len = 0;
+
+    while !all_ones {
+        all_ones = true;
+        while glob < len {
+            let current_bucket = bucket_positions[glob].bucket;
+            while glob + 1 < len && bucket_positions[glob + 1].bucket == current_bucket {
+                glob += 1;
+                loc += 1;
+            }
+            if current_bucket >= buckets as u32 {
+                loc = 1;
+            } else if loc > 1 {
+                // all ones is false if next len is not 1
+                if loc != 2 {
+                    all_ones = false;
+                }
+                let is_odd = loc % 2 == 1;
+                let half = loc / 2;
+                for i in 0..half {
+                    instr.push((
+                        bucket_positions[glob - (loc - 1) + 2 * i].position,
+                        bucket_positions[glob - (loc - 1) + 2 * i + 1].position,
+                    ));
+                    bucket_positions[new_len + i] = bucket_positions[glob - (loc - 1) + 2 * i];
+                }
+                if is_odd {
+                    bucket_positions[new_len + half] = bucket_positions[glob];
+                }
+                // Reset the local_counter and update state
+                new_len += half + (loc % 2);
+                batch += half;
+                loc = 1;
+
+                if batch >= BATCH_SIZE / 2 {
+                    C::batch_add_in_place_same_slice(&mut new_elems[..], &instr[..]);
+                    instr.clear();
+                    batch = 0;
+                }
+            } else {
+                bucket_positions[new_len] = bucket_positions[glob];
+                new_len += 1;
+            }
+            glob += 1;
+        }
+        if instr.len() > 0 {
+            C::batch_add_in_place_same_slice(&mut new_elems[..], &instr[..]);
+            instr.clear();
+        }
+        glob = 0;
+        batch = 0;
+        loc = 1;
+        len = new_len;
+        new_len = 0;
+    }
+
+    let zero = C::zero();
+    let mut res = vec![zero; buckets];
+
+    for i in 0..len {
+        let (pos, buc) = (bucket_positions[i].position, bucket_positions[i].bucket);
+        res[buc as usize] = new_elems[pos as usize];
+    }
+    res
 }
 
 /// Chunks vectorised instructions into a size that does not require
