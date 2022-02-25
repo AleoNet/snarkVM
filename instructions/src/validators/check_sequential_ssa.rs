4d8e5a8882fb65f3dcb5b31474c4e52711a11940
d0cd <pranavsaig@gmail.com>
2022-02-25 09:24:39 -0800

    Clean up sequential SSA validator

--- instructions/src/validators/check_sequential_ssa.rs
+++ instructions/src/validators/check_sequential_ssa.rs
@@ -14,10 +14,23 @@
 // You should have received a copy of the GNU General Public License
 // along with the snarkVM library. If not, see <https://www.gnu.org/licenses/>.
 
-use crate::{Program, Type};
+use crate::{Instruction, Program, Type};
+use std::{fmt, fmt::Formatter};
+
+#[derive(Debug, Clone)]
+pub struct SSAError {
+    instruction_id: usize,
+    typ: Type,
+}
+
+impl fmt::Display for SSAError {
+    fn fmt(&self, f: &mut Formatter<'_>) -> fmt::Result {
+        write!(f, "Non-sequential SSA for {:?} at instruction #{:?}", self.typ, self.instruction_id)
+    }
+}
 
 // TODO (@pranav) Clean up implementation. Currently a rough prototype.
-pub fn check_sequential_ssa(program: Program) {
+pub fn check_sequential_ssa(program: &Program) -> Result<(), SSAError> {
     let mut next_base_field_register: u64 = 0;
     let mut next_boolean_register: u64 = 0;
     let mut next_group_register: u64 = 0;
@@ -33,108 +46,64 @@ pub fn check_sequential_ssa(program: Program) {
     let mut next_u128_register: u64 = 0;
     let mut next_scalar_field_register: u64 = 0;
 
-    for instruction in program.instructions {
-        for register in instruction.destinations {
+    for instruction in &program.instructions {
+        for register in &instruction.destinations {
+            // Helper to check and increment register counts.
+            let check_register = |counter: &mut u64| {
+                if *counter == register.id {
+                    *counter += 1;
+                    Ok(())
+                } else {
+                    Err(SSAError { instruction_id: register.id as usize, typ: register.typ })
+                }
+            };
+
             match register.typ {
-                Type::BaseField => {
-                    if next_base_field_register == register.id {
-                        next_base_field_register += 1;
-                    } else {
-                        panic!("Non-sequential SSA for BaseField");
-                    }
-                }
-                Type::Boolean => {
-                    if next_boolean_register == register.id {
-                        next_boolean_register += 1;
-                    } else {
-                        panic!("Non-sequential SSA for Boolean");
-                    }
-                }
-                Type::Group => {
-                    if next_group_register == register.id {
-                        next_group_register += 1;
-                    } else {
-                        panic!("Non-sequential SSA for Group");
-                    }
-                }
-                Type::I8 => {
-                    if next_i8_register == register.id {
-                        next_i8_register += 1;
-                    } else {
-                        panic!("Non-sequential SSA for I8");
-                    }
-                }
-                Type::I16 => {
-                    if next_i16_register == register.id {
-                        next_i16_register += 1;
-                    } else {
-                        panic!("Non-sequential SSA for I16");
-                    }
-                }
-                Type::I32 => {
-                    if next_i32_register == register.id {
-                        next_i32_register += 1;
-                    } else {
-                        panic!("Non-sequential SSA for I32");
-                    }
-                }
-                Type::I64 => {
-                    if next_i64_register == register.id {
-                        next_i64_register += 1;
-                    } else {
-                        panic!("Non-sequential SSA for I64");
-                    }
-                }
-                Type::I128 => {
-                    if next_i128_register == register.id {
-                        next_i128_register += 1;
-                    } else {
-                        panic!("Non-sequential SSA for I128");
-                    }
-                }
-                Type::ScalarField => {
-                    if next_scalar_field_register == register.id {
-                        next_scalar_field_register += 1;
-                    } else {
-                        panic!("Non-sequential SSA for ScalarField");
-                    }
-                }
-                Type::U8 => {
-                    if next_u8_register == register.id {
-                        next_u8_register += 1;
-                    } else {
-                        panic!("Non-sequential SSA for U8");
-                    }
-                }
-                Type::U16 => {
-                    if next_u16_register == register.id {
-                        next_u16_register += 1;
-                    } else {
-                        panic!("Non-sequential SSA for U16");
-                    }
-                }
-                Type::U32 => {
-                    if next_u32_register == register.id {
-                        next_u32_register += 1;
-                    } else {
-                        panic!("Non-sequential SSA for U32");
-                    }
-                }
-                Type::U64 => {
-                    if next_u64_register == register.id {
-                        next_u64_register += 1;
-                    } else {
-                        panic!("Non-sequential SSA for U64");
-                    }
-                }
-                Type::U128 => {
-                    if next_u128_register == register.id {
-                        next_u128_register += 1;
-                    } else {
-                        panic!("Non-sequential SSA for U128");
-                    }
-                }
-            }
+                Type::BaseField => check_register(&mut next_base_field_register),
+                Type::Boolean => check_register(&mut next_boolean_register),
+                Type::Group => check_register(&mut next_group_register),
+                Type::I8 => check_register(&mut next_i8_register),
+                Type::I16 => check_register(&mut next_i16_register),
+                Type::I32 => check_register(&mut next_i32_register),
+                Type::I64 => check_register(&mut next_i64_register),
+                Type::I128 => check_register(&mut next_i128_register),
+                Type::ScalarField => check_register(&mut next_scalar_field_register),
+                Type::U8 => check_register(&mut next_u8_register),
+                Type::U16 => check_register(&mut next_u16_register),
+                Type::U32 => check_register(&mut next_u32_register),
+                Type::U64 => check_register(&mut next_u64_register),
+                Type::U128 => check_register(&mut next_u128_register),
+            }?;
         }
+    }
+    Ok(())
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use core::str::FromStr;
+    use snarkvm_circuits::Circuit;
+
+    #[test]
+    fn test_sequential_ssa_valid() {
+        let (input, program) = Program::new(
+            "u8.r0 := addw.u8 0u8, 1u8;\n\
+                   u8.r1 := addw.u8 0u8, 2u8;\n\
+                   bool.r0 := eq.u8 u8.r0, u8.r1;",
+        )
+        .unwrap();
+        assert!(check_sequential_ssa(&program).is_ok());
+    }
+
+    #[test]
+    fn test_sequential_ssa_invalid() {
+        let (input, program) = Program::new(
+            "u8.r3 := addw.u8 u8.r2, u8.r1;\n\
+                   u8.r6 := addw.u8 u8.r5, u8.r4;\n\
+                   bool.r1 := eq.u8 u8.r5, u8.r6;",
+        )
+        .unwrap();
+        assert!(check_sequential_ssa(&program).is_err());
     }
 }
