58396fad1fb4f58c3faa86cdc2431aa8170e3052
howardwu <9260812+howardwu@users.noreply.github.com>
2022-02-26 23:12:31 -0800

    Clean up

--- circuits/programs/src/lib.rs
+++ circuits/programs/src/lib.rs
@@ -203,64 +203,6 @@ impl<E: Environment> Function<E> {
     }
 }
 
-pub struct HelloWorld<E: Environment> {
-    function: Function<E>,
-    outputs: Registers<E>,
-}
-
-impl<E: Environment> HelloWorld<E> {
-    /// Initializes a new instance of `HelloWorld` with the given inputs.
-    pub fn new(inputs: [Operand<E>; 2]) -> Self {
-        let mut function = Function::new();
-        let mut outputs = Registers::new();
-
-        // Allocate a new register for each input, and store each input in the register.
-        let mut registers = Registers::with_capacity(2);
-        for input in inputs {
-            registers.push(function.new_input(input));
-        }
-
-        // Add the values in the registers, storing the result in a newly allocated register.
-        for pair in registers.chunks(2) {
-            let first = Operand::Register(pair[0].clone());
-            let second = Operand::Register(pair[1].clone());
-            let output = function.new_register();
-
-            let instruction = Instruction::Add(output.clone(), first, second);
-
-            function.push_instruction(instruction);
-            outputs.push(output);
-        }
-
-        Self { function, outputs }
-    }
-
-    pub fn run(&self) {
-        self.function.evaluate();
-    }
-}
-
-#[cfg(test)]
-mod tests {
-    use super::*;
-    use snarkvm_circuits::Circuit;
-
-    #[test]
-    fn test_hello_world() {
-        let first = Immediate::BaseField(BaseField::<Circuit>::one());
-        let second = Immediate::BaseField(BaseField::one());
-
-        let function = HelloWorld::new([first.into(), second.into()]);
-        function.run();
-
-        let expected = BaseField::one() + BaseField::one();
-        match function.outputs[0].load() {
-            Immediate::BaseField(output) => assert!(output.is_eq(&expected).eject_value()),
-            _ => panic!("Failed to load output"),
-        }
-    }
-}
-
 // pub struct Memory<E: Environment> {
 //     registers: Registers<E>,
 // }
