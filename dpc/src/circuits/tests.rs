cc48a3754f1b79c53493b5eea40d0a7135d71e32
howardwu <9260812+howardwu@users.noreply.github.com>
2022-03-19 20:40:50 -0700

    Update dpc circuit count

--- dpc/src/circuits/tests.rs
+++ dpc/src/circuits/tests.rs
@@ -136,7 +136,7 @@ mod testnet1 {
 
     #[test]
     fn test_dpc_execute_circuits() {
-        dpc_execute_circuits_test::<Testnet1>(262081);
+        dpc_execute_circuits_test::<Testnet1>(259161);
     }
 }
 
@@ -146,6 +146,6 @@ mod testnet2 {
 
     #[test]
     fn test_dpc_execute_circuits() {
-        dpc_execute_circuits_test::<Testnet2>(262081);
+        dpc_execute_circuits_test::<Testnet2>(259161);
     }
 }
