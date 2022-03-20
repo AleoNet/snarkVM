ebcfea4981adf86b11131736abf30139ad5aba8c
howardwu <9260812+howardwu@users.noreply.github.com>
2022-03-19 18:28:55 -0700

    Remove unused errors

--- algorithms/src/errors/crh.rs
+++ algorithms/src/errors/crh.rs
@@ -30,14 +30,8 @@ pub enum CRHError {
     #[error("incorrect input length {} x 8 for window params {}x{}", _0, _1, _2)]
     IncorrectInputLength(usize, usize, usize),
 
-    #[error("incorrect parameter size {}x{} for window params {}x{}", _0, _1, _2, _3)]
-    IncorrectParameterSize(usize, usize, usize, usize),
-
     #[error("{}", _0)]
     Message(String),
-
-    #[error("Unable to hash to curve on message: {}", _0)]
-    UnableToHashToCurve(String),
 }
 
 impl From<Error> for CRHError {
