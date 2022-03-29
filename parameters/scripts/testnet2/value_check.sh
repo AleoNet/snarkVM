c8d9a8d8056769b36296c4379db7c3ee69050216
raychu86 <14917648+raychu86@users.noreply.github.com>
2022-03-28 19:54:50 -0700

    Remove value check parameters

--- parameters/scripts/testnet2/value_check.sh
+++
@@ -1,9 +0,0 @@
-# This script will run the value check SNARK setup and move the resulting `.params`
-# and `.checksum` files to `params` folder under the `src` directory.
-# If the parameter size has changed, you will need to manually update these in each corresponding struct.
-
-RUST_BACKTRACE=1 cargo run --release --example setup value_check testnet2 || exit
-
-mv value_check.metadata ../../src/testnet2/resources
-mv value_check.proving* ~/.aleo/resources
-mv value_check.verifying ../../src/testnet2/resources
