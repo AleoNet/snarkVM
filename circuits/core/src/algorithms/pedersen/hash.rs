149f3dd569583d13c88639f0c3a7847331145eb1
howardwu <9260812+howardwu@users.noreply.github.com>
2022-04-03 13:42:38 -0700

    Refactor the Pedersen hash tests

--- circuits/core/src/algorithms/pedersen/hash.rs
+++ circuits/core/src/algorithms/pedersen/hash.rs
@@ -15,11 +15,76 @@
 // along with the snarkVM library. If not, see <https://www.gnu.org/licenses/>.
 
 use super::*;
-use snarkvm_circuits_types::{Boolean, Environment, Field};
 
 impl<E: Environment, const NUM_WINDOWS: usize, const WINDOW_SIZE: usize> Pedersen<E, NUM_WINDOWS, WINDOW_SIZE> {
-    /// Returns the affine x-coordinate as the collision-resistant hash output.
+    /// Returns the Pedersen hash of the given input as a field element.
     pub fn hash(&self, input: &[Boolean<E>]) -> Field<E> {
+        // Compute the Pedersen hash as an affine group element, and return the x-coordinate.
         self.hash_uncompressed(input).to_x_coordinate()
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use snarkvm_algorithms::{crh::PedersenCompressedCRH, CRH};
+    use snarkvm_circuits_environment::Circuit;
+    use snarkvm_curves::AffineCurve;
+    use snarkvm_utilities::{test_rng, UniformRand};
+
+    const ITERATIONS: usize = 10;
+    const MESSAGE: &str = "PedersenCircuit0";
+    const WINDOW_SIZE_MULTIPLIER: usize = 8;
+
+    type Projective = <<Circuit as Environment>::Affine as AffineCurve>::Projective;
+
+    fn check_hash<const NUM_WINDOWS: usize, const WINDOW_SIZE: usize>(mode: Mode) {
+        // Initialize the Pedersen hash.
+        let native = PedersenCompressedCRH::<Projective, NUM_WINDOWS, WINDOW_SIZE>::setup(MESSAGE);
+        let circuit = Pedersen::<Circuit, NUM_WINDOWS, WINDOW_SIZE>::setup(MESSAGE);
+        // Determine the number of inputs.
+        let num_input_bits = NUM_WINDOWS * WINDOW_SIZE;
+
+        for i in 0..ITERATIONS {
+            // Sample a random input.
+            let input = (0..num_input_bits).map(|_| bool::rand(&mut test_rng())).collect::<Vec<bool>>();
+            // Compute the expected hash.
+            let expected = native.hash(&input).expect("Failed to hash native input");
+            // Prepare the circuit input.
+            let circuit_input: Vec<Boolean<_>> = Inject::new(mode, input);
+
+            Circuit::scope(format!("Pedersen {mode} {i}"), || {
+                // Perform the hash operation.
+                let candidate = circuit.hash(&circuit_input);
+                assert_eq!(expected, candidate.eject_value());
+            });
+        }
+    }
+
+    #[test]
+    fn test_hash_constant() {
+        check_hash::<1, WINDOW_SIZE_MULTIPLIER>(Mode::Constant);
+        check_hash::<2, { 2 * WINDOW_SIZE_MULTIPLIER }>(Mode::Constant);
+        check_hash::<3, { 3 * WINDOW_SIZE_MULTIPLIER }>(Mode::Constant);
+        check_hash::<4, { 4 * WINDOW_SIZE_MULTIPLIER }>(Mode::Constant);
+        check_hash::<5, { 5 * WINDOW_SIZE_MULTIPLIER }>(Mode::Constant);
+    }
+
+    #[test]
+    fn test_hash_public() {
+        check_hash::<1, WINDOW_SIZE_MULTIPLIER>(Mode::Public);
+        check_hash::<2, { 2 * WINDOW_SIZE_MULTIPLIER }>(Mode::Public);
+        check_hash::<3, { 3 * WINDOW_SIZE_MULTIPLIER }>(Mode::Public);
+        check_hash::<4, { 4 * WINDOW_SIZE_MULTIPLIER }>(Mode::Public);
+        check_hash::<5, { 5 * WINDOW_SIZE_MULTIPLIER }>(Mode::Public);
+    }
+
+    #[test]
+    fn test_hash_private() {
+        check_hash::<1, WINDOW_SIZE_MULTIPLIER>(Mode::Private);
+        check_hash::<2, { 2 * WINDOW_SIZE_MULTIPLIER }>(Mode::Private);
+        check_hash::<3, { 3 * WINDOW_SIZE_MULTIPLIER }>(Mode::Private);
+        check_hash::<4, { 4 * WINDOW_SIZE_MULTIPLIER }>(Mode::Private);
+        check_hash::<5, { 5 * WINDOW_SIZE_MULTIPLIER }>(Mode::Private);
     }
 }
