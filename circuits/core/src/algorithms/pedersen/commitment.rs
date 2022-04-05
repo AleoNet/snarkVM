6fb6d8fea88c40be4a4e01e6425b96628851fb27
Jules de Smit <julesdesmit@gmail.com>
2022-04-05 18:41:18 +0200

    Add commitment equality tests

--- circuits/core/src/algorithms/pedersen/commitment.rs
+++ circuits/core/src/algorithms/pedersen/commitment.rs
@@ -22,7 +22,9 @@ use snarkvm_circuits_types::prelude::*;
 /// PedersenCommitment is an additively-homomorphic commitment scheme that takes variable-length
 /// input.
 pub struct PedersenCommitment<E: Environment, const NUM_WINDOWS: usize, const WINDOW_SIZE: usize> {
+    /// The underlying Pedersen gadget, used to produce hashes of input bits.
     pedersen_gadget: Pedersen<E, NUM_WINDOWS, WINDOW_SIZE>,
+    /// A vector of random bases, used for computing the commitment.
     random_base: Vec<Group<E>>,
 }
 
@@ -67,12 +69,14 @@ mod tests {
     use snarkvm_algorithms::{commitment::PedersenCommitment as NativePedersenCommitment, CommitmentScheme};
     use snarkvm_circuits_environment::Circuit;
     use snarkvm_curves::{AffineCurve, ProjectiveCurve};
+    use snarkvm_utilities::{test_rng, ToBits, UniformRand};
 
     const ITERATIONS: usize = 10;
     const MESSAGE: &str = "PedersenCommitmentCircuit0";
     const WINDOW_SIZE_MULTIPLIER: usize = 8;
 
     type Projective = <<Circuit as Environment>::Affine as AffineCurve>::Projective;
+    type ScalarField = <Circuit as Environment>::ScalarField;
 
     fn check_setup<const NUM_WINDOWS: usize, const WINDOW_SIZE: usize>(
         num_constants: usize,
@@ -89,7 +93,7 @@ mod tests {
                 let circuit = PedersenCommitment::<Circuit, NUM_WINDOWS, WINDOW_SIZE>::setup(MESSAGE);
                 assert_scope!(num_constants, num_public, num_private, num_constraints);
 
-                // Check for equivalency of the random base.
+                // Check for equality of the random base.
                 native.random_base.iter().zip_eq(circuit.random_base.iter()).for_each(|(expected, candidate)| {
                     assert_eq!(expected.to_affine(), candidate.eject_value());
                 });
@@ -112,5 +116,90 @@ mod tests {
         check_setup::<3, WINDOW_SIZE_MULTIPLIER>(841, 0, 0, 0);
         check_setup::<4, WINDOW_SIZE_MULTIPLIER>(869, 0, 0, 0);
         check_setup::<5, WINDOW_SIZE_MULTIPLIER>(897, 0, 0, 0);
+    }
+
+    fn check_commitment<const NUM_WINDOWS: usize, const WINDOW_SIZE: usize>(
+        mode: Mode,
+        num_constants: usize,
+        num_public: usize,
+        num_private: usize,
+        num_constraints: usize,
+    ) {
+        // Initialize the Pedersen hash.
+        let native = NativePedersenCommitment::<Projective, NUM_WINDOWS, WINDOW_SIZE>::setup(MESSAGE);
+        let circuit = PedersenCommitment::<Circuit, NUM_WINDOWS, WINDOW_SIZE>::setup(MESSAGE);
+        // Determine the number of inputs.
+        let num_input_bits = NUM_WINDOWS * WINDOW_SIZE;
+
+        for i in 0..ITERATIONS {
+            // Sample a random input.
+            let input = (0..num_input_bits).map(|_| bool::rand(&mut test_rng())).collect::<Vec<bool>>();
+            // Sample randomness
+            let randomness = ScalarField::rand(&mut test_rng());
+            // Compute the expected hash.
+            let expected = native.commit(&input, &randomness).expect("Failed to hash native input");
+            // Prepare the circuit input.
+            let circuit_input: Vec<Boolean<_>> = Inject::new(mode, input);
+            // Prepare the circuit randomness.
+            let circuit_randomness: Vec<Boolean<_>> = Inject::new(mode, randomness.to_bits_le());
+
+            Circuit::scope(format!("Pedersen {mode} {i}"), || {
+                // Perform the hash operation.
+                let candidate = circuit.commit(&circuit_input, &circuit_randomness);
+                assert_scope!(num_constants, num_public, num_private, num_constraints);
+                assert_eq!(expected, candidate.eject_value());
+            });
+        }
+    }
+
+    #[test]
+    fn test_commitment_constant() {
+        // Set the number of windows, and modulate the window size.
+        check_commitment::<1, WINDOW_SIZE_MULTIPLIER>(Mode::Constant, 1036, 0, 0, 0);
+        check_commitment::<1, { 2 * WINDOW_SIZE_MULTIPLIER }>(Mode::Constant, 1068, 0, 0, 0);
+        check_commitment::<1, { 3 * WINDOW_SIZE_MULTIPLIER }>(Mode::Constant, 1100, 0, 0, 0);
+        check_commitment::<1, { 4 * WINDOW_SIZE_MULTIPLIER }>(Mode::Constant, 1132, 0, 0, 0);
+        check_commitment::<1, { 5 * WINDOW_SIZE_MULTIPLIER }>(Mode::Constant, 1164, 0, 0, 0);
+
+        // Set the window size, and modulate the number of windows.
+        check_commitment::<1, WINDOW_SIZE_MULTIPLIER>(Mode::Constant, 1036, 0, 0, 0);
+        check_commitment::<2, WINDOW_SIZE_MULTIPLIER>(Mode::Constant, 1068, 0, 0, 0);
+        check_commitment::<3, WINDOW_SIZE_MULTIPLIER>(Mode::Constant, 1100, 0, 0, 0);
+        check_commitment::<4, WINDOW_SIZE_MULTIPLIER>(Mode::Constant, 1132, 0, 0, 0);
+        check_commitment::<5, WINDOW_SIZE_MULTIPLIER>(Mode::Constant, 1164, 0, 0, 0);
+    }
+
+    #[test]
+    fn test_commitment_public() {
+        // Set the number of windows, and modulate the window size.
+        check_commitment::<1, WINDOW_SIZE_MULTIPLIER>(Mode::Public, 518, 0, 1551, 1551);
+        check_commitment::<1, { 2 * WINDOW_SIZE_MULTIPLIER }>(Mode::Public, 534, 0, 1599, 1599);
+        check_commitment::<1, { 3 * WINDOW_SIZE_MULTIPLIER }>(Mode::Public, 550, 0, 1647, 1647);
+        check_commitment::<1, { 4 * WINDOW_SIZE_MULTIPLIER }>(Mode::Public, 566, 0, 1695, 1695);
+        check_commitment::<1, { 5 * WINDOW_SIZE_MULTIPLIER }>(Mode::Public, 582, 0, 1743, 1743);
+
+        // Set the window size, and modulate the number of windows.
+        check_commitment::<1, WINDOW_SIZE_MULTIPLIER>(Mode::Public, 518, 0, 1551, 1551);
+        check_commitment::<2, WINDOW_SIZE_MULTIPLIER>(Mode::Public, 534, 0, 1599, 1599);
+        check_commitment::<3, WINDOW_SIZE_MULTIPLIER>(Mode::Public, 550, 0, 1647, 1647);
+        check_commitment::<4, WINDOW_SIZE_MULTIPLIER>(Mode::Public, 566, 0, 1695, 1695);
+        check_commitment::<5, WINDOW_SIZE_MULTIPLIER>(Mode::Public, 582, 0, 1743, 1743);
+    }
+
+    #[test]
+    fn test_commitment_private() {
+        // Set the number of windows, and modulate the window size.
+        check_commitment::<1, WINDOW_SIZE_MULTIPLIER>(Mode::Private, 518, 0, 1551, 1551);
+        check_commitment::<1, { 2 * WINDOW_SIZE_MULTIPLIER }>(Mode::Private, 534, 0, 1599, 1599);
+        check_commitment::<1, { 3 * WINDOW_SIZE_MULTIPLIER }>(Mode::Private, 550, 0, 1647, 1647);
+        check_commitment::<1, { 4 * WINDOW_SIZE_MULTIPLIER }>(Mode::Private, 566, 0, 1695, 1695);
+        check_commitment::<1, { 5 * WINDOW_SIZE_MULTIPLIER }>(Mode::Private, 582, 0, 1743, 1743);
+
+        // Set the window size, and modulate the number of windows.
+        check_commitment::<1, WINDOW_SIZE_MULTIPLIER>(Mode::Private, 518, 0, 1551, 1551);
+        check_commitment::<2, WINDOW_SIZE_MULTIPLIER>(Mode::Private, 534, 0, 1599, 1599);
+        check_commitment::<3, WINDOW_SIZE_MULTIPLIER>(Mode::Private, 550, 0, 1647, 1647);
+        check_commitment::<4, WINDOW_SIZE_MULTIPLIER>(Mode::Private, 566, 0, 1695, 1695);
+        check_commitment::<5, WINDOW_SIZE_MULTIPLIER>(Mode::Private, 582, 0, 1743, 1743);
     }
 }
