26a1cc925b22c9690adf48e6f98cb6d60d5c8ebe
raychu86 <14917648+raychu86@users.noreply.github.com>
2022-04-06 10:45:38 -0700

    Update merkle path to_root tests

--- circuits/core/src/algorithms/merkle_path/to_root.rs
+++ circuits/core/src/algorithms/merkle_path/to_root.rs
@@ -27,22 +27,26 @@ where
     pub fn to_root(&self, crh: &H, leaf: &[H::Input]) -> H::Output {
         let mut curr_hash = crh.hash(leaf);
 
+        // Padding used to match the native merkle tre.
+        let padding = H::Input::blank(Mode::Constant);
+
         // To traverse up a MT, we iterate over the path from bottom to top
 
         // At any given bit, the bit being 0 indicates our currently hashed value is the left,
         // and the bit being 1 indicates our currently hashed value is on the right.
-        // Thus `left_hash` is the sibling if bit is 1, and it's the computed hash if bit is 0
+        // Thus `left_hash` is the sibling if bit is 1, and it's the computed hash if bit is 0.
         for (bit, sibling) in self.traversal.iter().zip_eq(&self.path) {
             let left_hash: H::Output = H::Output::ternary(&bit.clone().into(), sibling, &curr_hash).into();
             let right_hash: H::Output = H::Output::ternary(&bit.clone().into(), &curr_hash, sibling).into();
 
-            let left_input: Vec<H::Input> = left_hash.into();
-            let right_input: Vec<H::Input> = right_hash.into();
+            let mut left_input: Vec<H::Input> = left_hash.into();
+            let mut right_input: Vec<H::Input> = right_hash.into();
 
-            // TODO (raychu86): Handle issue with merkle tree inner node hashing.
-            //  The native tree hashes bytes, whereas the circuit tree hashes bits, which means the padding is incorrectly done.
-            //  Native input = left_hash bits + padding (to fill bytes) + right_hash bits + padding
-            //  Circuit input = left_hash bits + right_hash bits + padding.
+            // Pad the inputs to the closest number of bytes. This is required due to the nature of the
+            // native merkle tree hashing.
+            let num_bytes = ((left_input.len() + 7) / 8) * 8;
+            left_input.resize(num_bytes, padding.clone());
+            right_input.resize(num_bytes, padding.clone());
 
             curr_hash = crh.hash(&[left_input, right_input].concat());
         }
@@ -57,7 +61,7 @@ mod tests {
     use crate::algorithms::Pedersen;
 
     use snarkvm_algorithms::{
-        crh::PedersenCompressedCRH as NativePedersen,
+        crh::PedersenCompressedCRH as NativePedersenCompressed,
         merkle_tree::{MaskedMerkleTreeParameters, MerkleTree},
         traits::MerkleParameters,
     };
@@ -68,12 +72,12 @@ mod tests {
 
     use std::sync::Arc;
 
-    const PEDERSEN_NUM_WINDOWS: usize = 256;
+    const PEDERSEN_NUM_WINDOWS: usize = 128;
     const PEDERSEN_WINDOW_SIZE: usize = 4;
     const TREE_DEPTH: usize = 4;
     const MESSAGE: &str = "Pedersen merkle path test";
 
-    type NativeH = NativePedersen<EdwardsProjective, PEDERSEN_NUM_WINDOWS, PEDERSEN_WINDOW_SIZE>;
+    type NativeH = NativePedersenCompressed<EdwardsProjective, PEDERSEN_NUM_WINDOWS, PEDERSEN_WINDOW_SIZE>;
     type Parameters = MaskedMerkleTreeParameters<NativeH, TREE_DEPTH>;
 
     type H = Pedersen<Circuit, PEDERSEN_NUM_WINDOWS, PEDERSEN_WINDOW_SIZE>;
@@ -128,28 +132,28 @@ mod tests {
 
     #[test]
     fn test_good_root_constant() {
-        check_to_root(Mode::Constant, false, 0, 2773, 0, 0, 0);
+        check_to_root(Mode::Constant, false, 0, 12526, 0, 0, 0);
     }
 
     #[test]
     fn test_good_root_public() {
-        check_to_root(Mode::Public, false, 0, 487, 9, 4005, 4018);
+        check_to_root(Mode::Public, false, 0, 5121, 261, 16528, 16793);
     }
 
     #[test]
     fn test_good_root_private() {
-        check_to_root(Mode::Private, false, 0, 487, 0, 4014, 4018);
+        check_to_root(Mode::Private, false, 0, 5121, 0, 16789, 16793);
     }
 
     #[should_panic]
     #[test]
     fn test_bad_root_public() {
-        check_to_root(Mode::Public, true, 0, 487, 9, 4005, 4018);
+        check_to_root(Mode::Public, true, 0, 5121, 9, 4005, 16793);
     }
 
     #[should_panic]
     #[test]
     fn test_bad_root_private() {
-        check_to_root(Mode::Private, true, 0, 487, 0, 4014, 4018);
+        check_to_root(Mode::Private, true, 0, 5121, 0, 4014, 16793);
     }
 }
