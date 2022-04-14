eb751f60f65763ac3249b2adb8066ec91a85f2d1
raychu86 <14917648+raychu86@users.noreply.github.com>
2022-04-13 18:03:23 -0700

    Introduce TwoToOneCRH and LeafCRH

--- algorithms/src/traits/merkle_tree.rs
+++ algorithms/src/traits/merkle_tree.rs
@@ -20,7 +20,8 @@ use snarkvm_utilities::{to_bytes_le, ToBytes};
 use std::fmt::Debug;
 
 pub trait MerkleParameters: Clone + Debug + PartialEq + Eq + Send + Sync {
-    type H: CRH;
+    type LeafCRH: CRH;
+    type TwoToOneCRH: CRH<Output = <Self::LeafCRH as CRH>::Output>;
 
     const DEPTH: usize;
 
@@ -37,34 +38,42 @@ pub trait MerkleParameters: Clone + Debug + PartialEq + Eq + Send + Sync {
     fn setup_message(&self) -> &str;
 
     /// Returns the collision-resistant hash function used by the Merkle tree.
-    fn crh(&self) -> &Self::H;
+    fn crh(&self) -> &Self::LeafCRH;
+
+    /// Returns the collision-resistant hash function used by the Merkle tree for parent nodes.
+    fn two_to_one_crh(&self) -> &Self::TwoToOneCRH;
 
     /// Returns the hash of a given leaf.
-    fn hash_leaf<L: ToBytes>(&self, leaf: &L) -> Result<<Self::H as CRH>::Output, MerkleError> {
+    fn hash_leaf<L: ToBytes>(&self, leaf: &L) -> Result<<Self::LeafCRH as CRH>::Output, MerkleError> {
         Ok(self.crh().hash_bytes(&leaf.to_bytes_le()?)?)
+    }
+
+    /// Returns the parent hash of two children.
+    fn hash_two_to_one<L: ToBytes>(&self, children: &L) -> Result<<Self::LeafCRH as CRH>::Output, MerkleError> {
+        Ok(self.two_to_one_crh().hash_bytes(&children.to_bytes_le()?)?)
     }
 
     /// Returns the output hash, given a left and right hash value.
     fn hash_inner_node(
         &self,
-        left: &<Self::H as CRH>::Output,
-        right: &<Self::H as CRH>::Output,
-    ) -> Result<<Self::H as CRH>::Output, MerkleError> {
-        Ok(self.crh().hash_bytes(&to_bytes_le![left, right]?)?)
+        left: &<Self::LeafCRH as CRH>::Output,
+        right: &<Self::LeafCRH as CRH>::Output,
+    ) -> Result<<Self::LeafCRH as CRH>::Output, MerkleError> {
+        Ok(self.two_to_one_crh().hash_bytes(&to_bytes_le![left, right]?)?)
     }
 
-    fn hash_empty(&self) -> Result<<Self::H as CRH>::Output, MerkleError> {
+    fn hash_empty(&self) -> Result<<Self::LeafCRH as CRH>::Output, MerkleError> {
         // TODO (howardwu): TEMPORARY - This choice of a 64 byte buffer is a temporary fix.
         //  Previously, the size was `<Self::H as CRH>::INPUT_SIZE_BITS / 8` which was also incorrect.
         //  One needs to define a `LeafCRH` and a `TwoToOneCRH` in order to set proper size expectations.
         //  64 bytes was chosen as a temporary fix, to at least ensure the `TwoToOneCRH` preimage size fits,
         //  however this temporary fix does not technically address the issue in a meaningful sense.
         let empty_buffer = &[0u8; 64];
-        Ok(self.crh().hash_bytes(&*empty_buffer)?)
+        Ok(self.two_to_one_crh().hash_bytes(&*empty_buffer)?)
     }
 }
 
 pub trait MaskedMerkleParameters: MerkleParameters {
     /// Returns the collision-resistant hash function masking parameters used by the Merkle tree.
-    fn mask_crh(&self) -> &Self::H;
+    fn mask_crh(&self) -> &Self::LeafCRH;
 }
