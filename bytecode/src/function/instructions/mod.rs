9db62d83d1747007db8abba112725d3ec5498f60
Jules de Smit <julesdesmit@gmail.com>
2022-04-30 12:42:06 +0200

    Update instruction boilerplate macro visibility

--- bytecode/src/function/instructions/mod.rs
+++ bytecode/src/function/instructions/mod.rs
@@ -14,6 +14,65 @@
 // You should have received a copy of the GNU General Public License
 // along with the snarkVM library. If not, see <https://www.gnu.org/licenses/>.
 
+macro_rules! impl_instruction_boilerplate {
+    ($instruction:ident, $op_type:ident, $op_code:expr) => {
+        impl<P: Program> $instruction<P> {
+            /// Returns the operands of the instruction.
+            pub fn operands(&self) -> Vec<Operand<P>> {
+                self.operation.operands()
+            }
+
+            /// Returns the destination register of the instruction.
+            pub fn destination(&self) -> &Register<P> {
+                self.operation.destination()
+            }
+        }
+
+        impl<P: Program> Opcode for $instruction<P> {
+            /// Returns the opcode as a string.
+            #[inline]
+            fn opcode() -> &'static str {
+                $op_code
+            }
+        }
+
+        impl<P: Program> Parser for $instruction<P> {
+            type Environment = P::Environment;
+
+            #[inline]
+            fn parse(string: &str) -> ParserResult<Self> {
+                map($op_type::parse, |operation| Self { operation })(string)
+            }
+        }
+
+        impl<P: Program> fmt::Display for $instruction<P> {
+            fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+                write!(f, "{}", self.operation)
+            }
+        }
+
+        impl<P: Program> FromBytes for $instruction<P> {
+            fn read_le<R: Read>(mut reader: R) -> IoResult<Self> {
+                Ok(Self { operation: $op_type::read_le(&mut reader)? })
+            }
+        }
+
+        impl<P: Program> ToBytes for $instruction<P> {
+            fn write_le<W: Write>(&self, mut writer: W) -> IoResult<()> {
+                self.operation.write_le(&mut writer)
+            }
+        }
+
+        #[allow(clippy::from_over_into)]
+        impl<P: Program> Into<Instruction<P>> for $instruction<P> {
+            /// Converts the operation into an instruction.
+            fn into(self) -> Instruction<P> {
+                Instruction::$instruction(self)
+            }
+        }
+    };
+}
+
 pub(super) mod add;
 pub(super) use add::*;
 
@@ -251,66 +310,6 @@ impl<P: Program> ToBytes for Instruction<P> {
             }
         }
     }
-}
-
-#[macro_export]
-macro_rules! impl_instruction_boilerplate {
-    ($instruction:ident, $op_type:ident, $op_code:expr) => {
-        impl<P: Program> $instruction<P> {
-            /// Returns the operands of the instruction.
-            pub fn operands(&self) -> Vec<Operand<P>> {
-                self.operation.operands()
-            }
-
-            /// Returns the destination register of the instruction.
-            pub fn destination(&self) -> &Register<P> {
-                self.operation.destination()
-            }
-        }
-
-        impl<P: Program> Opcode for $instruction<P> {
-            /// Returns the opcode as a string.
-            #[inline]
-            fn opcode() -> &'static str {
-                $op_code
-            }
-        }
-
-        impl<P: Program> Parser for $instruction<P> {
-            type Environment = P::Environment;
-
-            #[inline]
-            fn parse(string: &str) -> ParserResult<Self> {
-                map($op_type::parse, |operation| Self { operation })(string)
-            }
-        }
-
-        impl<P: Program> fmt::Display for $instruction<P> {
-            fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-                write!(f, "{}", self.operation)
-            }
-        }
-
-        impl<P: Program> FromBytes for $instruction<P> {
-            fn read_le<R: Read>(mut reader: R) -> IoResult<Self> {
-                Ok(Self { operation: $op_type::read_le(&mut reader)? })
-            }
-        }
-
-        impl<P: Program> ToBytes for $instruction<P> {
-            fn write_le<W: Write>(&self, mut writer: W) -> IoResult<()> {
-                self.operation.write_le(&mut writer)
-            }
-        }
-
-        #[allow(clippy::from_over_into)]
-        impl<P: Program> Into<Instruction<P>> for $instruction<P> {
-            /// Converts the operation into an instruction.
-            fn into(self) -> Instruction<P> {
-                Instruction::$instruction(self)
-            }
-        }
-    };
 }
 
 #[cfg(test)]
--- bytecode/src/function/instructions/mul.rs
+++ bytecode/src/function/instructions/mul.rs
@@ -17,7 +17,6 @@
 use crate::{
     function::{parsers::*, Instruction, Opcode, Operation, Registers},
     helpers::Register,
-    impl_instruction_boilerplate,
     Program,
     Value,
 };
