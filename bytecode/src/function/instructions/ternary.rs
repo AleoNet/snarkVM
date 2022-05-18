f690419b3e72d14d9cdd1b8809ac682e6e2ef141
howardwu <9260812+howardwu@users.noreply.github.com>
2022-05-17 22:06:23 -0700

    Fix test

--- bytecode/src/function/instructions/ternary.rs
+++ bytecode/src/function/instructions/ternary.rs
@@ -293,7 +293,7 @@ mod tests {
     }
 
     #[test]
-    #[should_panic(expected = "Invalid 'ter' instruction")]
+    #[should_panic(expected = "Invalid 'ternary' instruction")]
     fn test_ternary_halts_on_mismatched_operand_types() {
         ternary_test("true.private", "1scalar.private", "1field.private", "\"unreachable\".private");
     }
