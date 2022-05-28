e6a92cb5310c293475b14cb5f7fe6ff043365b90
howardwu <9260812+howardwu@users.noreply.github.com>
2022-05-28 13:21:52 -0700

    Remove TODO

--- console/program/src/record/mod.rs
+++ console/program/src/record/mod.rs
@@ -60,7 +60,6 @@ impl<N: Network> Record<N> {
     pub fn to_id(&self, program: &N::Field, process: &N::Field) -> Result<N::Field> {
         // Retrieve the x-coordinate of the nonce.
         let nonce = self.nonce.to_x_coordinate();
-        // TODO (howardwu): Abstraction - add support for a custom BHP hash size.
         // Compute the BHP hash of the program state.
         N::hash_bhp1024(
             &[*program, *process, self.owner, self.balance, self.data.to_id()?, nonce, self.mac, self.bcm].to_bits_le(),
