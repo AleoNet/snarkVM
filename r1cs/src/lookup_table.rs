0e17a271c6aa4c94b7daef7cb5c0208fb83fc1ff
Jules de Smit <julesdesmit@gmail.com>
2022-06-27 19:10:32 +0200

    Modify lookup table to always be 3-arity
    Since we only have 3 matrices, we can not enforce larger lookups

--- r1cs/src/lookup_table.rs
+++ r1cs/src/lookup_table.rs
@@ -14,62 +14,54 @@
 // You should have received a copy of the GNU General Public License
 // along with the snarkVM library. If not, see <https://www.gnu.org/licenses/>.
 
-use crate::errors::SynthesisError;
-
 use indexmap::IndexMap;
 use snarkvm_fields::Field;
 use snarkvm_utilities::serialize::*;
 
+const DEFAULT_KEY_SIZE: usize = 2;
+
 #[derive(Clone, Debug)]
 pub struct LookupTable<F: Field> {
-    arity: usize,
-    table: IndexMap<Vec<F>, F>,
+    pub table: IndexMap<[F; DEFAULT_KEY_SIZE], F>,
+}
+
+impl<F: Field> Default for LookupTable<F> {
+    fn default() -> Self {
+        Self { table: IndexMap::new() }
+    }
 }
 
 impl<F: Field> LookupTable<F> {
-    pub fn new(arity: usize) -> Self {
-        Self { arity, table: IndexMap::new() }
-    }
-
-    pub fn fill(&mut self, key: Vec<F>, val: F) -> Result<Option<F>, SynthesisError> {
-        if key.len() != self.arity {
-            Err(SynthesisError::LookupKeyWrongLength(key.len(), self.arity))
-        } else {
-            Ok(self.table.insert(key, val))
-        }
+    pub fn fill(&mut self, key: [F; DEFAULT_KEY_SIZE], val: F) -> Option<F> {
+        self.table.insert(key, val)
     }
 
     pub fn lookup(&self, key: &[F]) -> Option<&F> {
         self.table.get(key)
     }
-
-    pub fn arity(&self) -> usize {
-        self.arity
-    }
 }
 
 impl<F: Field> CanonicalSerialize for LookupTable<F> {
     fn serialize_with_mode<W: Write>(&self, mut writer: W, compress: Compress) -> Result<(), SerializationError> {
-        self.arity.serialize_with_mode(&mut writer, compress)?;
         self.table.len().serialize_with_mode(&mut writer, compress)?;
         for (k, v) in self.table.iter() {
-            k.serialize_with_mode(&mut writer, compress)?;
+            for el in k {
+                el.serialize_with_mode(&mut writer, compress)?;
+            }
             v.serialize_with_mode(&mut writer, compress)?;
         }
         Ok(())
     }
 
     fn serialized_size(&self, compress: Compress) -> usize {
-        self.arity.serialized_size(compress)
-            + self.table.len().serialized_size(compress)
+        self.table.len().serialized_size(compress)
             + self.table.iter().map(|(k, v)| k.serialized_size(compress) + v.serialized_size(compress)).sum::<usize>()
     }
 }
 
 impl<F: Field> Valid for LookupTable<F> {
     fn check(&self) -> Result<(), SerializationError> {
-        Vec::<F>::batch_check(self.table.keys())?;
-        F::batch_check(self.table.values())
+        F::batch_check(self.table.keys().flatten().chain(self.table.values()))
     }
 }
 
@@ -79,15 +71,20 @@ impl<F: Field> CanonicalDeserialize for LookupTable<F> {
         compress: Compress,
         validate: Validate,
     ) -> Result<Self, SerializationError> {
-        let arity = usize::deserialize_with_mode(&mut reader, compress, validate)?;
         let mut table = IndexMap::new();
         let len = usize::deserialize_with_mode(&mut reader, compress, validate)?;
         for _ in 0..len {
             table.insert(
-                Vec::<F>::deserialize_with_mode(&mut reader, compress, validate)?,
+                {
+                    let mut key = [F::zero(); DEFAULT_KEY_SIZE];
+                    for el in key.iter_mut() {
+                        *el = F::deserialize_with_mode(&mut reader, compress, validate)?;
+                    }
+                    key
+                },
                 F::deserialize_with_mode(&mut reader, compress, validate)?,
             );
         }
-        Ok(Self { arity, table })
+        Ok(Self { table })
     }
 }
