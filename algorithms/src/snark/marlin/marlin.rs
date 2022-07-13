69e31bc7b853ac694e8117d80f8648b85c7009df
Jules de Smit <julesdesmit@gmail.com>
2022-07-13 10:35:16 +0200

    Fix clippy

--- algorithms/src/snark/marlin/marlin.rs
+++ algorithms/src/snark/marlin/marlin.rs
@@ -849,7 +849,7 @@ mod lookup_test {
     use snarkvm_curves::bls12_377::{Bls12_377, Fq, Fr};
     use snarkvm_fields::Field;
     use snarkvm_r1cs::{ConstraintSystem, LinearCombination, LookupTable, SynthesisError};
-    use snarkvm_utilities::{test_crypto_rng, UniformRand};
+    use snarkvm_utilities::{test_crypto_rng, Uniform};
 
     use core::ops::MulAssign;
 
