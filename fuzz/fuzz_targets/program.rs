1ee49fc4ac84a54f0eeb4b08f6c3b06aced395eb
ljedrz <ljedrz@gmail.com>
2022-07-29 13:51:58 +0200

    feat: add a libfuzzer fuzz test
    Signed-off-by: ljedrz <ljedrz@gmail.com>

---
+++ fuzz/fuzz_targets/program.rs
@@ -0,0 +1,36 @@
+// Copyright (C) 2019-2022 Aleo Systems Inc.
+// This file is part of the snarkVM library.
+
+// The snarkVM library is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+
+// The snarkVM library is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU General Public License for more details.
+
+// You should have received a copy of the GNU General Public License
+// along with the snarkVM library. If not, see <https://www.gnu.org/licenses/>.
+
+#![no_main]
+use libfuzzer_sys::fuzz_target;
+
+use snarkvm::{compiler::Program, console::network::Testnet3, prelude::test_crypto_rng, VM};
+
+type CurrentNetwork = Testnet3;
+
+fuzz_target!(|program: Program<CurrentNetwork>| {
+    // Initialize the VM.
+    if let Ok(vm) = VM::<CurrentNetwork>::new() {
+        // Initialize the RNG.
+        let rng = &mut test_crypto_rng();
+
+        // Deploy.
+        if let Ok(transaction) = vm.deploy(&program, rng) {
+            // Verify.
+            vm.verify(&transaction);
+        }
+    }
+});
