3c12c81514c711cb76bec54eeb25390664f060bb
Maximilian Ammann <max@maxammann.org>
2022-08-05 17:07:35 +0200

    Experiment further and try to improve speed

--- fuzz/fuzz_targets/program.rs
+++ fuzz/fuzz_targets/program.rs
@@ -14,25 +14,50 @@
 // You should have received a copy of the GNU General Public License
 // along with the snarkVM library. If not, see <https://www.gnu.org/licenses/>.
 
+
+
 #![no_main]
 use libfuzzer_sys::fuzz_target;
 
 use snarkvm::{compiler::Program, console::network::Testnet3, prelude::test_crypto_rng};
-use snarkvm::prelude::VM;
-use digest::Mac;
+use snarkvm::prelude::{PrivateKey, VM};
 
 type CurrentNetwork = Testnet3;
 
-fuzz_target!(|program: Program<CurrentNetwork>| {
-    // Initialize the VM.
-    if let Ok(vm) = VM::<CurrentNetwork>::new() {
-        // Initialize the RNG.
-        let rng = &mut test_crypto_rng();
+use std::sync::Once;
+use once_cell::sync::OnceCell;
+use snarkvm::circuit;
+use snarkvm::circuit::Mode;
+use crate::circuit::{AleoV0, Inject};
 
-        // Deploy.
-        if let Ok(deployment) = vm.deploy(&program, rng) {
-            // Verify.
-            vm.verify_deployment(&deployment);
+static INSTANCE: OnceCell<VM<CurrentNetwork>> = OnceCell::new();
+
+fn fuzz(program: Program<CurrentNetwork>) {
+    let vm = INSTANCE.get_or_init(||  VM::<CurrentNetwork>::new().unwrap());
+
+    // Initialize the RNG.
+    let rng = &mut test_crypto_rng();
+
+    //let private_key = circuit::PrivateKey::<AleoV0>::new(Mode::Private, pkey);
+
+    // Deploy.
+    if let Ok(deployment) = vm.deploy(&program, rng) {
+        vm.verify_deployment(&deployment);
+    }
+
+    // Execute
+    if let Some(f) = program.functions().first() {
+        // Initialize the private key.
+        let pkey = PrivateKey::new(rng).unwrap();
+
+        if let Ok(auth) = vm.authorize(&pkey, program.id(), f.0.clone(), &[], rng) {
+            vm.execute(auth, rng); // ignore unwrap
+        } else {
+            // ignore
         }
     }
+}
+
+fuzz_target!(|program: Program<CurrentNetwork>| {
+    fuzz(program);
 });
