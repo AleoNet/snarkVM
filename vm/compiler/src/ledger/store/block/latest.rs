07d6cf40a34767c73c5d8f94320b2af9ad7b9385
raychu86 <14917648+raychu86@users.noreply.github.com>
2022-08-04 22:19:01 -0700

    Implement BlockStore latest

---
+++ vm/compiler/src/ledger/store/block/latest.rs
@@ -0,0 +1,89 @@
+// Copyright (C) 2019-2022 Aleo Systems Inc.
+// This file is part of the snarkVM library.
+
+// The snarkVM library is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+
+// The snarkVM library is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU General Public License for more details.
+
+// You should have received a copy of the GNU General Public License
+// along with the snarkVM library. If not, see <https://www.gnu.org/licenses/>.
+
+use super::*;
+
+impl<
+    N: Network,
+    HashesMap: for<'a> Map<'a, u32, N::BlockHash>,
+    HeadersMap: for<'a> Map<'a, N::BlockHash, Header<N>>,
+    SignaturesMap: for<'a> Map<'a, N::BlockHash, Signature<N>>,
+    BlockTransactionsMap: for<'a> Map<'a, N::BlockHash, Vec<N::TransactionID>>,
+    DeploymentsMap: for<'a> Map<'a, N::TransactionID, (Deployment<N>, N::TransitionID)>,
+    ExecutionsMap: for<'a> Map<'a, N::TransactionID, (Vec<N::TransitionID>, Option<N::TransitionID>)>,
+    TransitionsMap: for<'a> Map<'a, N::TransitionID, Transition<N>>,
+    PublicKeysMap: for<'a> Map<'a, Group<N>, N::TransitionID>,
+    SerialNumbersMap: for<'a> Map<'a, Field<N>, N::TransitionID>,
+    CommitmentsMap: for<'a> Map<'a, Field<N>, N::TransitionID>,
+    OriginsMap: for<'a> Map<'a, Origin<N>, N::TransitionID>,
+    NonceMap: for<'a> Map<'a, Group<N>, N::TransitionID>,
+>
+    BlockStore<
+        N,
+        HashesMap,
+        HeadersMap,
+        SignaturesMap,
+        BlockTransactionsMap,
+        DeploymentsMap,
+        ExecutionsMap,
+        TransitionsMap,
+        PublicKeysMap,
+        SerialNumbersMap,
+        CommitmentsMap,
+        OriginsMap,
+        NonceMap,
+    >
+{
+    /// Returns the latest state root.
+    pub const fn latest_state_root(&self) -> &Field<N> {
+        self.block_tree.root()
+    }
+
+    /// Returns the latest block.
+    pub fn latest_block(&self) -> Result<Block<N>> {
+        self.get_block(self.current_height)
+    }
+
+    /// Returns the latest block hash.
+    pub const fn latest_hash(&self) -> N::BlockHash {
+        self.current_hash
+    }
+
+    /// Returns the latest block height.
+    pub const fn latest_height(&self) -> u32 {
+        self.current_height
+    }
+
+    /// Returns the latest round number.
+    pub const fn latest_round(&self) -> u64 {
+        self.current_round
+    }
+
+    /// Returns the latest block coinbase target.
+    pub fn latest_coinbase_target(&self) -> Result<u64> {
+        Ok(self.get_header(self.current_height)?.coinbase_target())
+    }
+
+    /// Returns the latest block proof target.
+    pub fn latest_proof_target(&self) -> Result<u64> {
+        Ok(self.get_header(self.current_height)?.proof_target())
+    }
+
+    /// Returns the latest block timestamp.
+    pub fn latest_timestamp(&self) -> Result<i64> {
+        Ok(self.get_header(self.current_height)?.timestamp())
+    }
+}
