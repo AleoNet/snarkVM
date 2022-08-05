e0f99935f8ae86bcb468b3601b29f6c69aac46b1
raychu86 <14917648+raychu86@users.noreply.github.com>
2022-08-04 18:06:25 -0700

    Implement formal serialization for origin

--- vm/compiler/src/ledger/transition/input/mod.rs
+++ vm/compiler/src/ledger/transition/input/mod.rs
@@ -14,6 +14,9 @@
 // You should have received a copy of the GNU General Public License
 // along with the snarkVM library. If not, see <https://www.gnu.org/licenses/>.
 
+mod origin;
+pub use origin::*;
+
 mod bytes;
 mod serialize;
 mod string;
@@ -25,14 +28,6 @@ use console::{
 };
 
 type Variant = u16;
-
-#[derive(Clone, PartialEq, Eq)]
-pub enum Origin<N: Network> {
-    /// The origin is a commitment.
-    Commitment(Field<N>),
-    /// The origin is a state root.
-    StateRoot(N::StateRoot),
-}
 
 /// The transition input.
 #[derive(Clone, PartialEq, Eq)]
