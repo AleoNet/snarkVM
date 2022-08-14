b513b34a1e22f2cedd061bb95bd580506e358b0b
Howard Wu <9260812+howardwu@users.noreply.github.com>
2022-08-14 00:04:35 -0700

    Adds command methods

--- vm/compiler/src/program/finalize/command/mod.rs
+++ vm/compiler/src/program/finalize/command/mod.rs
@@ -16,3 +16,109 @@
 
 mod finalize;
 pub use finalize::*;
+
+use crate::program::Instruction;
+use console::network::prelude::*;
+
+#[derive(Clone, PartialEq, Eq, Hash)]
+pub enum Command<N: Network> {
+    Instruction(Instruction<N>),
+}
+
+impl<N: Network> FromBytes for Command<N> {
+    /// Reads the command from a buffer.
+    fn read_le<R: Read>(mut reader: R) -> IoResult<Self> {
+        // Read the variant.
+        let variant = u8::read_le(&mut reader)?;
+        match variant {
+            0 => {
+                // Read the instruction.
+                let instruction = Instruction::read_le(&mut reader)?;
+                Ok(Self::Instruction(instruction))
+            }
+            1.. => Err(error(format!("Invalid command variant: {}", variant))),
+        }
+    }
+}
+
+impl<N: Network> ToBytes for Command<N> {
+    /// Writes the command to a buffer.
+    fn write_le<W: Write>(&self, mut writer: W) -> IoResult<()> {
+        match self {
+            Self::Instruction(instruction) => {
+                // Write the variant.
+                0u8.write_le(&mut writer)?;
+                // Write the instruction.
+                instruction.write_le(&mut writer)
+            }
+        }
+    }
+}
+
+impl<N: Network> Parser for Command<N> {
+    /// Parses the string into the command.
+    #[inline]
+    fn parse(string: &str) -> ParserResult<Self> {
+        alt((map(Instruction::parse, |instruction| Self::Instruction(instruction)),))(string)
+    }
+}
+
+impl<N: Network> FromStr for Command<N> {
+    type Err = Error;
+
+    /// Parses the string into the command.
+    #[inline]
+    fn from_str(string: &str) -> Result<Self> {
+        match Self::parse(string) {
+            Ok((remainder, object)) => {
+                // Ensure the remainder is empty.
+                ensure!(remainder.is_empty(), "Failed to parse string. Found invalid character in: \"{remainder}\"");
+                // Return the object.
+                Ok(object)
+            }
+            Err(error) => bail!("Failed to parse string. {error}"),
+        }
+    }
+}
+
+impl<N: Network> Debug for Command<N> {
+    /// Prints the command as a string.
+    fn fmt(&self, f: &mut Formatter) -> fmt::Result {
+        Display::fmt(self, f)
+    }
+}
+
+impl<N: Network> Display for Command<N> {
+    /// Prints the command as a string.
+    fn fmt(&self, f: &mut Formatter) -> fmt::Result {
+        match self {
+            Self::Instruction(instruction) => Display::fmt(instruction, f),
+        }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use console::network::Testnet3;
+
+    type CurrentNetwork = Testnet3;
+
+    #[test]
+    fn test_command_bytes() {
+        // Instruction
+        let expected = "add r0 r1 into r2;";
+        let command = Command::<CurrentNetwork>::parse(expected).unwrap().1;
+        let bytes = command.to_bytes_le().unwrap();
+        assert_eq!(command, Command::from_bytes_le(&bytes).unwrap());
+    }
+
+    #[test]
+    fn test_command_parse() {
+        // Instruction
+        let expected = "add r0 r1 into r2;";
+        let command = Command::<CurrentNetwork>::parse(expected).unwrap().1;
+        assert_eq!(Command::Instruction(Instruction::from_str(expected).unwrap()), command);
+        assert_eq!(expected, command.to_string());
+    }
+}
