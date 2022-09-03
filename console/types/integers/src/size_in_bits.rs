d3bf2c77545a7f89015fe19693e69f24dd07518c
Howard Wu <9260812+howardwu@users.noreply.github.com>
2022-09-03 16:22:28 -0700

    Clean up comment

--- console/types/integers/src/size_in_bits.rs
+++ console/types/integers/src/size_in_bits.rs
@@ -20,7 +20,7 @@ impl<E: Environment, I: IntegerType> SizeInBits for Integer<E, I> {
     /// Returns the integer size in bits.
     #[inline]
     fn size_in_bits() -> usize {
-        // unwrap is okay because our integers have size at most 128 bits.
+        // Note: `unwrap()` is safe as the integer is at most 128 bits.
         usize::try_from(I::BITS).unwrap()
     }
 }
