3ec9f5fcca7d40b6676b654f4cde9d6813fb37a6
Howard Wu <9260812+howardwu@users.noreply.github.com>
2022-09-24 18:21:05 -0700

    Update bench perf

--- vm/compiler/benches/coinbase_puzzle/coinbase_puzzle.rs
+++ vm/compiler/benches/coinbase_puzzle/coinbase_puzzle.rs
@@ -88,14 +88,16 @@ fn coinbase_puzzle_accumulate(c: &mut Criterion) {
     let max_degree = 1 << 18;
     let max_config = PuzzleConfig { degree: max_degree };
     let universal_srs = CoinbasePuzzle::<Testnet3>::setup(max_config, rng).unwrap();
-    for degree in [1 << 13, 1 << 14, 1 << 15, 1 << 16, 1 << 17] {
+
+    for degree in [1 << 13, 1 << 14, 1 << 15, 1 << 16] {
+        let config = PuzzleConfig { degree };
+        let (pk, _) = CoinbasePuzzleInst::trim(&universal_srs, config).unwrap();
+        let (epoch_info, epoch_challenge) = sample_epoch_info_and_challenge(degree, rng);
+
         for batch_size in [10, 100, 1000, 10000] {
             c.bench_function(
                 &format!("CoinbasePuzzle::Accumulate {batch_size} of 2^{}", (degree as f64).log2()),
                 |b| {
-                    let config = PuzzleConfig { degree };
-                    let (pk, _) = CoinbasePuzzleInst::trim(&universal_srs, config).unwrap();
-                    let (epoch_info, epoch_challenge) = sample_epoch_info_and_challenge(degree, rng);
                     let solutions = (0..batch_size)
                         .map(|_| {
                             let (address, nonce) = sample_address_and_nonce(rng);
@@ -115,12 +117,14 @@ fn coinbase_puzzle_verify(c: &mut Criterion) {
     let max_degree = 1 << 18;
     let max_config = PuzzleConfig { degree: max_degree };
     let universal_srs = CoinbasePuzzle::<Testnet3>::setup(max_config, rng).unwrap();
-    for degree in [1 << 13, 1 << 14, 1 << 15, 1 << 16, 1 << 17] {
+
+    for degree in [1 << 13, 1 << 14, 1 << 15, 1 << 16] {
+        let config = PuzzleConfig { degree };
+        let (pk, vk) = CoinbasePuzzleInst::trim(&universal_srs, config).unwrap();
+        let (epoch_info, epoch_challenge) = sample_epoch_info_and_challenge(degree, rng);
+
         for batch_size in [10, 100, 1000, 10000] {
             c.bench_function(&format!("CoinbasePuzzle::Verify {batch_size} of 2^{}", (degree as f64).log2()), |b| {
-                let config = PuzzleConfig { degree };
-                let (pk, vk) = CoinbasePuzzleInst::trim(&universal_srs, config).unwrap();
-                let (epoch_info, epoch_challenge) = sample_epoch_info_and_challenge(degree, rng);
                 let solutions = (0..batch_size)
                     .map(|_| {
                         let (address, nonce) = sample_address_and_nonce(rng);
