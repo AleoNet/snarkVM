5f928934b28925dba6ba7b09cc41b577b1f6cdb8
raychu86 <14917648+raychu86@users.noreply.github.com>
2022-09-30 16:28:59 -0700

    Implement new to_difficulty_target for coinbase solutions

--- vm/compiler/src/coinbase_puzzle/helpers/partial_solution/mod.rs
+++ vm/compiler/src/coinbase_puzzle/helpers/partial_solution/mod.rs
@@ -63,6 +63,8 @@ impl<N: Network> PartialSolution<N> {
 
     /// Returns the difficulty target of the solution.
     pub fn to_difficulty_target(&self) -> Result<u64> {
-        Ok(sha256d_to_u64(&self.commitment.to_bytes_le()?))
+        let hash_to_u64 = sha256d_to_u64(&self.commitment.to_bytes_le()?);
+
+        Ok(u64::MAX / hash_to_u64)
     }
 }
