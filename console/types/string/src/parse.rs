c5cbcfe071c6a5e9951403c458f925eded584802
Alessandro Coglio <coglio@kestrel.edu>
2022-10-05 18:14:20 -0700

    Fix generation of random test strings.
    The details are in the comments for the newly added code.
    
    This fixes an observed failure of these tests.

--- console/types/string/src/parse.rs
+++ console/types/string/src/parse.rs
@@ -75,12 +75,63 @@ mod tests {
         // Ensure empty string succeeds.
         assert!(StringType::<CurrentEnvironment>::parse("\"\"").is_ok());
 
+        // After this function, we generate a random string and we parse it,
+        // testing that we get back the original string.
+        // Since our string parser rejects certain characters in strings
+        // (see Sanitizer::parse_safe_char),
+        // we must avoid generating those characters in the first place.
+        // This function, which is called on every randomly generated character,
+        // leaves allowed characters unchanged,
+        // but adjusts disallowed ones to something allowed (we pick '0').
+        // Note that the randomness of the characters is just for testing purposes
+        // (there is no cryptographic requirement on this randomness),
+        // so the slight bias towards the character '0' does not matter.
+        // We could modify this function to adjust disallowed characters differently.
+        // But note that the disallowed characters are very few, among all possible characters;
+        // so it does not matter too much.
+        fn adjust_forbidden_char(ch: char) -> char {
+            let code = ch as u32;
+            if code < 9
+                || code == 11
+                || code == 12
+                || (14..=31).contains(&code)
+                || code == 127
+                || (0x202a..=0x202e).contains(&code)
+                || (0x2066..=0x2069).contains(&code)
+            {
+                '0'
+            } else {
+                ch
+            }
+        }
+
+        // Aside from the characters rejected through the function adjust_forbidden_char above,
+        // the syntax of strings allows backslash and double quotes only in certain circumstances:
+        // backslash is used to introduce an escape,
+        // and there are constraints on what can occur after a backslash;
+        // double quotes is only used in escaped form just after a backslash.
+        // If we randomly generate characters, we may end up generating
+        // backslashes with malformed escape syntax,
+        // or double quotes not preceded by backslash.
+        // Thus, we also adjust backslashes and double quotes as we randomly generate characters.
+        // See the discussion for adjust_forbidden_char regarding randomness and bias.
+        // Note that, this way, we do not test the parsing of any escape sequences;
+        // to do that, we would need to reify the possible elements of strings,
+        // namely characters and escapes,
+        // and randomly generate such elements.
+        fn adjust_backslash_and_doublequote(ch: char) -> char {
+            if ch == '\\' || ch == '\"' { '0' } else { ch }
+        }
+
         let rng = &mut TestRng::default();
 
         for i in 0..ITERATIONS {
             // Sample a random string. Take 1/4th to ensure we fit for all code points.
-            let expected: String =
-                (0..(CurrentEnvironment::MAX_STRING_BYTES - i) / 4).map(|_| rng.gen::<char>()).collect();
+            let expected: String = (0..(CurrentEnvironment::MAX_STRING_BYTES - i) / 4)
+                .map(|_| rng.gen::<char>())
+                .map(adjust_forbidden_char)
+                .map(adjust_backslash_and_doublequote)
+                .collect();
             let expected_num_bytes = expected.len();
             assert!(expected_num_bytes <= CurrentEnvironment::MAX_STRING_BYTES as usize);
 
