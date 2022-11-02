8f9b86f0d48fdfc60389f69017cbc6644a4d498d
raychu86 <14917648+raychu86@users.noreply.github.com>
2022-11-02 13:12:00 -0700

    Add additional verification tests

--- synthesizer/src/process/stack/inclusion/mod.rs
+++ synthesizer/src/process/stack/inclusion/mod.rs
@@ -570,3 +570,37 @@ impl<N: Network> InclusionAssignment<N> {
         Ok(A::eject_assignment_and_reset())
     }
 }
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use snarkvm_utilities::TestRng;
+
+    #[test]
+    fn test_inclusion_verify_execution() {
+        let rng = &mut TestRng::default();
+        // Fetch an execution transaction.
+        let execution = crate::vm::test_helpers::sample_execution_transaction(rng);
+
+        match execution {
+            Transaction::Execute(_, execution, _) => {
+                assert!(Inclusion::verify_execution(&execution).is_ok());
+            }
+            _ => panic!("Expected an execution transaction"),
+        }
+    }
+
+    #[test]
+    fn test_inclusion_verify_fee() {
+        let rng = &mut TestRng::default();
+        // Fetch an execution transaction.
+        let execution = crate::vm::test_helpers::sample_deployment_transaction(rng);
+
+        match execution {
+            Transaction::Deploy(_, _, fee) => {
+                assert!(Inclusion::verify_fee(&fee).is_ok());
+            }
+            _ => panic!("Expected a deployment transaction"),
+        }
+    }
+}
