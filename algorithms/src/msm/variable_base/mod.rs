85b51fae6d560ad29a327080086f4516954f6210
Howard Wu <9260812+howardwu@users.noreply.github.com>
2022-11-03 17:19:59 -0700

    Add msm script

--- algorithms/src/msm/variable_base/mod.rs
+++ algorithms/src/msm/variable_base/mod.rs
@@ -14,8 +14,8 @@
 // You should have received a copy of the GNU General Public License
 // along with the snarkVM library. If not, see <https://www.gnu.org/licenses/>.
 
-mod batched;
-mod standard;
+pub mod batched;
+pub mod standard;
 
 #[cfg(all(feature = "cuda", target_arch = "x86_64"))]
 mod cuda;
