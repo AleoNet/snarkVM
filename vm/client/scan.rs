ee2769e42e97ff5ac3d24914cbbef8320cbbf6d1
tangsong <tangsong0202@gmail.com>
2022-12-05 15:21:03 +0800

    support local transaction

--- vm/client/scan.rs
+++ vm/client/scan.rs
@@ -41,19 +41,14 @@ impl<N: Network> Client<N> {
             let end_height = start_height + 50;
 
             // Prepare the URL.
-            let url = match self.base_url.join(&format!("/testnet3/blocks/phase2/{start_height}.{end_height}.blocks")) {
+            let url = match self.base_url.join(&format!("/testnet3/blocks?start={start_height}&end={end_height}")) {
                 Ok(url) => url,
                 Err(error) => bail!("Failed to parse url: {error}"),
             };
             let url = url.to_string();
             // Request the blocks.
-            let blocks_bytes = match reqwest::blocking::get(&url) {
-                Ok(response) => match response.bytes() {
-                    Ok(bytes) => bytes,
-                    Err(error) => {
-                        bail!("Failed to parse blocks {start_height} (inclusive) to {end_height} (exclusive): {error}")
-                    }
-                },
+            let blocks = match reqwest::blocking::get(&url)?.json::<Vec<Block<N>>>() {
+                Ok(blocks) => blocks,
                 Err(error) => {
                     // TODO (howardwu): Check if this range exceeds the latest block height that has been cached.
                     bail!("Failed to fetch blocks from {url}: {error}")
@@ -61,15 +56,11 @@ impl<N: Network> Client<N> {
             };
 
             // Convert the blocks bytes into an iterator of records.
-            let records_iter = match bincode::deserialize::<Vec<Block<N>>>(&blocks_bytes) {
-                Ok(blocks) => blocks.into_iter().flat_map(|block| block.into_records()),
-                Err(error) => {
-                    bail!("Failed to deserialize {start_height} (inclusive) to {end_height} (exclusive): {error}")
-                }
-            };
+            let records_iter = blocks.into_iter().flat_map(|block| block.into_records());
 
             // Filter the records by the view key.
             records.extend(records_iter.filter_map(|(commitment, record)| {
+                //
                 match record.is_owner_with_address_x_coordinate(&view_key, &address_x_coordinate) {
                     true => Some((commitment, record)),
                     false => None,
@@ -79,6 +70,43 @@ impl<N: Network> Client<N> {
 
         Ok(records)
     }
+
+    // #[allow(clippy::type_complexity)]
+    // pub fn create_transaction(
+    //     &self,
+    //     private_key: &PrivateKey<N>,
+    //     to: Address<N>,
+    //     amount: u64,
+    // ) -> Result<Transaction<N>> {
+    //     // Fetch the unspent records.
+    //     let records = self.scan(&ViewKey::try_from(private_key)?, 14200..14250)?;
+
+    //     if records.len().is_zero() {
+    //         bail!("The Aleo account has no records to spend.")
+    //     }
+
+    //     // Initialize an RNG.
+    //     let rng = &mut rand::thread_rng();
+
+    //     // Prepare the inputs.
+    //     let inputs = [
+    //         Value::Record(records.values().next().unwrap().clone()),
+    //         Value::from_str(&format!("{to}"))?,
+    //         Value::from_str(&format!("{amount}u64"))?,
+    //     ];
+
+    //     // Create a new transaction.
+    //     Transaction::execute(
+    //         &self.vm,
+    //         private_key,
+    //         ProgramID::from_str("credits.aleo")?,
+    //         Identifier::from_str("transfer")?,
+    //         inputs.iter(),
+    //         None,
+    //         None,
+    //         rng,
+    //     )
+    // }
 }
 
 #[cfg(test)]
@@ -93,6 +121,7 @@ mod tests {
     #[test]
     fn test_scan() {
         // Initialize the client.
+        // http://127.0.0.1:3030 APrivateKey1zkp8CZNn3yeCseEtxuVPbDCwSyhGW6yZKUYKfgXmcpoGPWH
         let client = Client::<N>::new("https://vm.aleo.org/api").unwrap();
 
         // Derive the view key.
@@ -101,7 +130,7 @@ mod tests {
         let view_key = ViewKey::<N>::try_from(&private_key).unwrap();
 
         // Scan the ledger at this range.
-        let records = client.scan(private_key, 14200..14250).unwrap();
+        let records = client.scan(private_key, 50..100).unwrap();
         assert_eq!(records.len(), 1);
 
         // Check the commitment.
@@ -114,10 +143,10 @@ mod tests {
         // Decrypt the record.
         let record = record.decrypt(&view_key).unwrap();
         let expected = r"{
-  owner: aleo18x0yenrkceapvt85e6aqw2v8hq37hpt4ew6k6cgum6xlpmaxt5xqwnkuja.private,
-  gates: 1099999999999864u64.private,
-  _nonce: 3859911413360468505092363429199432421222291175370483298628506550397056121761group.public
-}";
+          owner: aleo18x0yenrkceapvt85e6aqw2v8hq37hpt4ew6k6cgum6xlpmaxt5xqwnkuja.private,
+          gates: 1099999999999864u64.private,
+          _nonce: 3859911413360468505092363429199432421222291175370483298628506550397056121761group.public
+        }";
         assert_eq!(record.to_string(), expected);
     }
 }
