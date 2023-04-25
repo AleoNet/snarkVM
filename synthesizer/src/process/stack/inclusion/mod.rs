76bd4658ed994fe17fe3aacb6424a94fb71a209a
Geoffrey Mureithi <mureithinjuguna@gmail.com>
2023-04-25 11:08:28 +0300

    Tiny fix on release build

--- synthesizer/src/process/stack/inclusion/mod.rs
+++ synthesizer/src/process/stack/inclusion/mod.rs
@@ -13,8 +13,6 @@
 
 // You should have received a copy of the GNU General Public License
 // along with the snarkVM library. If not, see <https://www.gnu.org/licenses/>.
-
-#[cfg(debug_assertions)]
 use crate::{
     BlockStorage,
     BlockStore,
