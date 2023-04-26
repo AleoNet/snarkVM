9c949e60c887e6dbedda0cb29d129c3a0932e158
Pranav Gaddamadugu <pranav@aleo.org>
2023-04-26 11:55:36 -0700

    Introduce Workload trait

---
+++ synthesizer/benches/workloads/mod.rs
@@ -0,0 +1,35 @@
+// Copyright (C) 2019-2023 Aleo Systems Inc.
+// This file is part of the snarkVM library.
+
+// The snarkVM library is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+
+// The snarkVM library is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU General Public License for more details.
+
+// You should have received a copy of the GNU General Public License
+// along with the snarkVM library. If not, see <https://www.gnu.org/licenses/>.
+
+use console::{network::Testnet3, prelude::Network, program::Value};
+use snarkvm_synthesizer::Program;
+
+/// An operation executed in a workload.
+pub enum Operation<N: Network> {
+    /// Deploy a program.
+    Deploy(Program<N>),
+    /// Execute a program.
+    Execute(String, String, Vec<Value<N>>),
+}
+
+/// A trait for workloads.
+pub trait Workload<N: Network> {
+    /// The sequence of operations to be run when setting up the workload.
+    fn setup() -> Vec<Operation<N>>;
+
+    /// The sequence of operations to be run when benchmarking the workload.
+    fn run() -> Vec<Operation<N>>;
+}
