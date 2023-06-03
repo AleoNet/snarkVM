66b46d439c4b0f58022900d354d1ca7a8d92fa0a
Howard Wu <9260812+howardwu@users.noreply.github.com>
2023-06-03 16:31:17 -0700

    Expand finalize operation impls

--- synthesizer/src/block/transactions/mod.rs
+++ synthesizer/src/block/transactions/mod.rs
@@ -97,15 +97,7 @@ impl<N: Network> Transactions<N> {
 
     /// Returns the finalize root of the transactions.
     pub fn to_finalize_root(&self) -> Result<Field<N>> {
-        // Extract the finalize operations from the transactions and convert them to bytes.
-        let finalize_operations_bytes =
-            self.finalize_operations().map(|operation| operation.identifier()).collect::<Result<Vec<_>>>()?;
-
-        // Construct the preimage for the finalize root.
-        let preimage = finalize_operations_bytes.iter().flat_map(|op| op.to_bits_le()).collect::<Vec<_>>();
-
-        // Construct the finalize root by hashing the preimage.
-        N::hash_bhp1024(&preimage)
+        N::hash_bhp1024(&self.finalize_operations().flat_map(|op| op.to_bits_le()).collect::<Vec<_>>())
     }
 }
 
