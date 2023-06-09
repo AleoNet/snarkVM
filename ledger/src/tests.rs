118b107b66bf345a96481c1bd85153b9c0291a4d
Pranav Gaddamadugu <pranav@aleo.org>
2023-06-09 10:03:42 -0700

    Test check_transaction_basic with insufficient funds

--- ledger/src/tests.rs
+++ ledger/src/tests.rs
@@ -12,12 +12,17 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-use crate::{tests::test_helpers::CurrentLedger, Ledger};
-use console::network::{prelude::*, Testnet3};
+use crate::{tests::test_helpers::CurrentLedger, Ledger, RecordsFilter};
+use console::{
+    account::ViewKey,
+    network::{prelude::*, Testnet3},
+    program::{Entry, Identifier, Literal, Plaintext, Value},
+};
 use synthesizer::{
     block::Block,
     store::{helpers::memory::ConsensusMemory, ConsensusStore},
     vm::VM,
+    Program,
 };
 
 type CurrentNetwork = Testnet3;
@@ -99,4 +104,96 @@ fn test_state_path() {
     let commitment = commitments[0];
 
     let _state_path = ledger.get_state_path_for_commitment(commitment).unwrap();
+}
+
+#[test]
+fn test_insufficient_finalize_fees() {
+    let rng = &mut TestRng::default();
+
+    // Sample the genesis private key.
+    let private_key = test_helpers::sample_genesis_private_key(rng);
+    let view_key = ViewKey::try_from(&private_key).unwrap();
+    // Initialize the store.
+    let store = ConsensusStore::<_, ConsensusMemory<_>>::open(None).unwrap();
+    // Create a genesis block.
+    let genesis = VM::from(store).unwrap().genesis(&private_key, rng).unwrap();
+    // Initialize the ledger with the genesis block.
+    let ledger = CurrentLedger::load(genesis.clone(), None).unwrap();
+
+    // Deploy a test program to the ledger.
+    let program = Program::<CurrentNetwork>::from_str(
+        r"
+program dummy.aleo;
+function foo:
+    input r0 as u8.private;
+    finalize r0;
+finalize foo:
+    input r0 as u8.public;
+    add r0 r0 into r1;",
+    )
+    .unwrap();
+
+    // A helper function to find records.
+    let find_records = || {
+        let microcredits = Identifier::from_str("microcredits").unwrap();
+        ledger
+            .find_records(&view_key, RecordsFilter::SlowUnspent(private_key))
+            .unwrap()
+            .filter(|(_, record)| match record.data().get(&microcredits) {
+                Some(Entry::Private(Plaintext::Literal(Literal::U64(amount), _))) => !amount.is_zero(),
+                _ => false,
+            })
+            .collect::<indexmap::IndexMap<_, _>>()
+    };
+
+    // Fetch the unspent records.
+    let records = find_records();
+
+    // Prepare the additional fee.
+    let credits = records.values().next().unwrap().clone();
+    let additional_fee = (credits, 6466000);
+
+    // Deploy.
+    let transaction = ledger.vm().deploy(&private_key, &program, additional_fee, None, rng).unwrap();
+    // Verify.
+    assert!(ledger.vm().verify_transaction(&transaction, None));
+
+    // Construct the next block.
+    let block = ledger.prepare_advance_to_next_block(&private_key, vec![transaction], None, rng).unwrap();
+    // Advance to the next block.
+    ledger.advance_to_next_block(&block).unwrap();
+    assert_eq!(ledger.latest_height(), 1);
+    assert_eq!(ledger.latest_hash(), block.hash());
+
+    // Execute the test program, without providing enough fees for finalize, and ensure that the ledger deems the transaction invalid.
+    // Fetch the unspent records.
+    let records = find_records();
+
+    // Select a record to spend.
+    let record = records.values().next().unwrap().clone();
+
+    // Retrieve the VM.
+    let vm = ledger.vm();
+
+    // Prepare the inputs.
+    let inputs = [Value::<CurrentNetwork>::from_str("1u8").unwrap()].into_iter();
+
+    // Execute.
+    let transaction = vm
+        .execute(&private_key, ("dummy.aleo", "foo"), inputs.clone(), Some((record.clone(), 1_000)), None, rng)
+        .unwrap();
+    // Verify.
+    assert!(vm.verify_transaction(&transaction, None));
+
+    // Ensure that the ledger deems the transaction invalid.
+    assert!(ledger.check_transaction_basic(&transaction, None).is_err());
+
+    // Execute with enough fees.
+    let transaction =
+        vm.execute(&private_key, ("dummy.aleo", "foo"), inputs, Some((record, 2_000_000_000)), None, rng).unwrap();
+    // Verify.
+    assert!(vm.verify_transaction(&transaction, None));
+
+    // Ensure that the ledger deems the transaction valid.
+    assert!(ledger.check_transaction_basic(&transaction, None).is_ok());
 }
