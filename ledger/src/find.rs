076fdcdf42e457f3bdb97dc0f43de4531f9eb88f
Howard Wu <9260812+howardwu@users.noreply.github.com>
2023-06-14 08:34:39 -0700

    Update ledger/src/find.rs
    Signed-off-by: Howard Wu <9260812+howardwu@users.noreply.github.com>

--- ledger/src/find.rs
+++ ledger/src/find.rs
@@ -25,7 +25,7 @@ impl<N: Network, C: ConsensusStorage<N>> Ledger<N, C> {
         self.vm.block_store().find_block_hash(transaction_id)
     }
 
-    /// Returns the block hash that contains the given `puzzle commitment`.
+    /// Returns the block height that contains the given `puzzle commitment`.
     pub fn find_block_height_from_puzzle_commitment(
         &self,
         puzzle_commitment: &PuzzleCommitment<N>,
