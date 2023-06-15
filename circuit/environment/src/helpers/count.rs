7df9e85a5c56206dc9458c32b8b3c699ac90921b
Pranav Gaddamadugu <pranav@aleo.org>
2023-06-15 16:30:02 -0700

    Add tests for constraint counts for downcasting

--- circuit/environment/src/helpers/count.rs
+++ circuit/environment/src/helpers/count.rs
@@ -12,8 +12,6 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-use console::prelude::{bail, Error, Result};
-
 use core::{
     fmt::{Debug, Display, Formatter},
     ops::{Add, Mul, Sub},
