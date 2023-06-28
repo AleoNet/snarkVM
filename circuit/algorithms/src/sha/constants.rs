df89ce9d3ea5804a12adf062fd278b33116c1b10
Michael Turner <mturner@aleo.org>
2023-06-27 23:59:11 -0400

    Add naive SHA256 & SHA512 circuit algorithm

--- circuit/algorithms/src/sha/constants.rs
+++ circuit/algorithms/src/sha/constants.rs
@@ -12,16 +12,18 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-pub const H0: u32 = 0x6a09e667;
-pub const H1: u32 = 0xbb67ae85;
-pub const H2: u32 = 0x3c6ef372;
-pub const H3: u32 = 0xa54ff53a;
-pub const H4: u32 = 0x510e527f;
-pub const H5: u32 = 0x9b05688c;
-pub const H6: u32 = 0x1f83d9ab;
-pub const H7: u32 = 0x5be0cd19;
+use snarkvm_circuit_types::FromBits;
+use snarkvm_utilities::ToBits;
 
-pub const K: &[u32] = &[
+/// SHA-256 rotation constants.
+const ROT_256: [u32; 12] = [7, 18, 3, 17, 19, 10, 6, 11, 25, 2, 13, 22];
+
+/// Initial SHA-256 hash state.
+const H_256: [u32; 8] =
+    [0x6a09e667, 0xbb67ae85, 0x3c6ef372, 0xa54ff53a, 0x510e527f, 0x9b05688c, 0x1f83d9ab, 0x5be0cd19];
+
+/// SHA-256 round constants.
+pub const K_256: &[u32] = &[
     0x428a2f98, 0x71374491, 0xb5c0fbcf, 0xe9b5dba5, 0x3956c25b, 0x59f111f1, 0x923f82a4, 0xab1c5ed5, 0xd807aa98,
     0x12835b01, 0x243185be, 0x550c7dc3, 0x72be5d74, 0x80deb1fe, 0x9bdc06a7, 0xc19bf174, 0xe49b69c1, 0xefbe4786,
     0x0fc19dc6, 0x240ca1cc, 0x2de92c6f, 0x4a7484aa, 0x5cb0a9dc, 0x76f988da, 0x983e5152, 0xa831c66d, 0xb00327c8,
@@ -31,3 +33,126 @@ pub const K: &[u32] = &[
     0x5b9cca4f, 0x682e6ff3, 0x748f82ee, 0x78a5636f, 0x84c87814, 0x8cc70208, 0x90befffa, 0xa4506ceb, 0xbef9a3f7,
     0xc67178f2,
 ];
+
+/// SHA-512 rotation constants.
+const ROT_512: [u32; 12] = [1, 8, 7, 19, 61, 6, 14, 18, 41, 28, 34, 39];
+
+/// Initial SHA-512 hash state.
+const H_512: [u64; 8] = [
+    0x6a09e667f3bcc908,
+    0xbb67ae8584caa73b,
+    0x3c6ef372fe94f82b,
+    0xa54ff53a5f1d36f1,
+    0x510e527fade682d1,
+    0x9b05688c2b3e6c1f,
+    0x1f83d9abfb41bd6b,
+    0x5be0cd19137e2179,
+];
+
+/// SHA-512 round constants.
+pub const K_512: &[u64] = &[
+    0x428a2f98d728ae22,
+    0x7137449123ef65cd,
+    0xb5c0fbcfec4d3b2f,
+    0xe9b5dba58189dbbc,
+    0x3956c25bf348b538,
+    0x59f111f1b605d019,
+    0x923f82a4af194f9b,
+    0xab1c5ed5da6d8118,
+    0xd807aa98a3030242,
+    0x12835b0145706fbe,
+    0x243185be4ee4b28c,
+    0x550c7dc3d5ffb4e2,
+    0x72be5d74f27b896f,
+    0x80deb1fe3b1696b1,
+    0x9bdc06a725c71235,
+    0xc19bf174cf692694,
+    0xe49b69c19ef14ad2,
+    0xefbe4786384f25e3,
+    0x0fc19dc68b8cd5b5,
+    0x240ca1cc77ac9c65,
+    0x2de92c6f592b0275,
+    0x4a7484aa6ea6e483,
+    0x5cb0a9dcbd41fbd4,
+    0x76f988da831153b5,
+    0x983e5152ee66dfab,
+    0xa831c66d2db43210,
+    0xb00327c898fb213f,
+    0xbf597fc7beef0ee4,
+    0xc6e00bf33da88fc2,
+    0xd5a79147930aa725,
+    0x06ca6351e003826f,
+    0x142929670a0e6e70,
+    0x27b70a8546d22ffc,
+    0x2e1b21385c26c926,
+    0x4d2c6dfc5ac42aed,
+    0x53380d139d95b3df,
+    0x650a73548baf63de,
+    0x766a0abb3c77b2a8,
+    0x81c2c92e47edaee6,
+    0x92722c851482353b,
+    0xa2bfe8a14cf10364,
+    0xa81a664bbc423001,
+    0xc24b8b70d0f89791,
+    0xc76c51a30654be30,
+    0xd192e819d6ef5218,
+    0xd69906245565a910,
+    0xf40e35855771202a,
+    0x106aa07032bbd1b8,
+    0x19a4c116b8d2d0c8,
+    0x1e376c085141ab53,
+    0x2748774cdf8eeb99,
+    0x34b0bcb5e19b48a8,
+    0x391c0cb3c5c95a63,
+    0x4ed8aa4ae3418acb,
+    0x5b9cca4f7763e373,
+    0x682e6ff3d6b2b8a3,
+    0x748f82ee5defb2fc,
+    0x78a5636f43172f60,
+    0x84c87814a1f0ab72,
+    0x8cc702081a6439ec,
+    0x90befffa23631e28,
+    0xa4506cebde82bde9,
+    0xbef9a3f7b2c67915,
+    0xc67178f2e372532b,
+    0xca273eceea26619c,
+    0xd186b8c721c0c207,
+    0xeada7dd6cde0eb1e,
+    0xf57d4f7fee6ed178,
+    0x06f067aa72176fba,
+    0x0a637dc5a2c898a6,
+    0x113f9804bef90dae,
+    0x1b710b35131c471b,
+    0x28db77f523047d84,
+    0x32caab7b40c72493,
+    0x3c9ebe0a15c9bebc,
+    0x431d67c49c100d4c,
+    0x4cc5d4becb3e42b6,
+    0x597f299cfc657e2a,
+    0x5fcb6fab3ad6faec,
+    0x6c44198c4a475817,
+];
+
+pub(crate) fn sha256_rotation_constants<E: snarkvm_circuit_types::Environment>() -> Vec<snarkvm_circuit_types::U32<E>> {
+    R_256.iter().map(|&x| snarkvm_circuit_types::U32::from_bits_le(&x.to_bits_le())).collect()
+}
+
+pub(crate) fn sha512_rotation_constants<E: snarkvm_circuit_types::Environment>() -> Vec<snarkvm_circuit_types::U64<E>> {
+    R_512.iter().map(|&x| snarkvm_circuit_types::U64::from_bits_le(&x.to_bits_le())).collect()
+}
+
+pub(crate) fn sha256_initial_state<E: snarkvm_circuit_types::Environment>() -> Vec<snarkvm_circuit_types::U32<E>> {
+    H_256.iter().map(|&x| snarkvm_circuit_types::U32::from_bits_le(&x.to_bits_le())).collect()
+}
+
+pub(crate) fn sha256_round_constants<E: snarkvm_circuit_types::Environment>() -> Vec<snarkvm_circuit_types::U32<E>> {
+    K_256.iter().map(|&x| snarkvm_circuit_types::U32::from_bits_le(&x.to_bits_le())).collect()
+}
+
+pub(crate) fn sha512_initial_state<E: snarkvm_circuit_types::Environment>() -> Vec<snarkvm_circuit_types::U64<E>> {
+    H_512.iter().map(|&x| snarkvm_circuit_types::U64::from_bits_le(&x.to_bits_le())).collect()
+}
+
+pub(crate) fn sha512_round_constants<E: snarkvm_circuit_types::Environment>() -> Vec<snarkvm_circuit_types::U64<E>> {
+    K_512.iter().map(|&x| snarkvm_circuit_types::U64::from_bits_le(&x.to_bits_le())).collect()
+}
--- circuit/algorithms/src/sha/hash.rs
+++ circuit/algorithms/src/sha/hash.rs
@@ -13,13 +13,148 @@
 // limitations under the License.
 
 use super::*;
+use crate::sha::constants::*;
+use snarkvm_circuit_types::environment::circuit;
+use snarkvm_circuit_types::integers::Integer;
+
+const BIT_SIZE: usize = Sha::bit_size();
 
 impl<E: Environment, const NUM_BITS: usize> Hash for Sha<E, NUM_BITS> {
     type Input = Boolean<E>;
     type Output = Field<E>;
 
     #[inline]
-    fn hash(&self, _input: &[Self::Input]) -> Self::Output {
-        Field::<E>::one()
+    fn hash(&self, input: &[Self::Input]) -> Self::Output {
+        // Set constants
+        let message_length =
+            (input.len() as u64).to_bits_be().into_iter().map(|b| Boolean::<E>::constant(b)).collect::<Vec<_>>();
+        let chunk_size = NUM_BITS * 2;
+        let word_size = 32;
+
+        // Pad Message with 1 bit
+        let mut input = Cow::Borrowed(input);
+        let padded_length = input.len() + 1;
+        input.to_mut().resize(padded_length, Boolean::constant(true));
+
+        // Determine how many zeros we need to pad the message with
+        let chunks = padded_length / chunk_size + 1;
+        let remaining_bits = chunks * NUM_BITS - padded_length;
+        let extension_bits = if remaining_bits >= 64 { remaining_bits - 64 } else { remaining_bits + NUM_BITS - 64 };
+        input.to_mut().resize(padded_length + extension_bits, Boolean::constant(false));
+        input.to_mut().extend(message_length);
+
+        // Initialize state
+        let (mut state, round_constants) = Self::round_constants();
+        let rotation_constants = Self::rotation_constants();
+
+        // Initialize schedule array
+        for chunk in input.chunks_exact(chunk_size) {
+            let mut vec = vec![];
+            for (i, other) in chunk.chunks_exact(word_size).enumerate() {
+                vec.push(Integer::<E, bit_size!(NUM_BITS)>::from_bits_be(other));
+            }
+            let array_start = Self::schedule_array_size() - vec.len();
+            vec.extend(vec![initialize!(NUM_BITS); array_start]);
+            for i in array_start..Self::schedule_array_size() {
+                let s0 = vec[i - 15].shr_wrapped(&rotation_constants[0]) ^ vec[i - 15].shr_wrapped(&rotation_constants[1]) ^ vec[i - 15].shr_checked(&rotation_constants[2]);
+                let s1 = vec[i - 2].shr_wrapped(&rotation_constants[3]) ^ vec[i - 2].shr_wrapped(&rotation_constants[4]) ^ vec[i - 2].shr_checked(&rotation_constants[5]);
+                vec[i] = *vec[i - 16] + s0 + *vec[i - 7] + s1;
+            }
+
+            let (mut a, mut b, mut c, mut d, mut e, mut f, mut g, mut h) = (*state[0], *state[1], *state[2], *state[3], *state[4], *state[5], *state[6], *state[7]);
+
+            for i in 0..Self::schedule_array_size() {
+                let s1 = e.shr_wrapped(&rotation_constants[6]) ^ e.shr_wrapped(&rotation_constants[7]) ^ e.shr_wrapped(&rotation_constants[8]);
+                let ch = (e & f) ^ ((!e) & g);
+                let temp1 = h + s1 + ch + *round_constants[i] + *vec[i];
+                let s0 = a.shr_wrapped(&rotation_constants[9]) ^ a.shr_wrapped(&rotation_constants[10]) ^ a.shr_wrapped(&rotation_constants[11]);
+                let maj = (a & b) ^ (a & c) ^ (b & c);
+                let temp2 = s0 + maj;
+
+                let h = g;
+                let g = f;
+                let f = e;
+                let e = d + temp1;
+                let d = c;
+                let c = b;
+                let b = a;
+                let a = temp1 + temp2;
+            }
+
+            state[0] = a + *state[0];
+            state[1] = b + *state[1];
+            state[2] = c + *state[2];
+            state[3] = d + *state[3];
+            state[4] = e + *state[4];
+            state[5] = f + *state[5];
+            state[6] = g + *state[6];
+            state[7] = h + *state[7];
+        }
+
+        let bytes = state.into_iter().flat_map(|s| s.to_bits_be()).collect::<Vec<_>>();
+        Field::from_bits_be(&bytes)
     }
+}
+
+impl<E: Environment, const NUM_BITS: usize> Sha<E, NUM_BITS> {
+    #[inline]
+    const fn schedule_array_size() -> usize {
+        match NUM_BITS {
+            256 => 64,
+            512 => 80,
+            _ => E::halt("Invalid hash size"),
+        }
+    }
+
+    #[inline]
+    const fn round_constants() -> (Vec<Integer<E, bit_size!(NUM_BITS)>>, Vec<Integer<E, bit_size!(NUM_BITS)>>) {
+        match NUM_BITS {
+            256 => (sha256_initial_state(), sha256_round_constants()),
+            512 => (sha512_initial_state(), sha512_round_constants()),
+            _ => E::halt("Invalid hash size"),
+        }
+    }
+
+    #[inline]
+    const fn schedule_constants() -> (Vec<Integer<E, bit_size!(NUM_BITS)>>, Vec<Integer<E, bit_size!(NUM_BITS)>>) {
+        match NUM_BITS {
+            256 => (Integer::<E, bit_size!(NUM_BITS)>::),
+            512 => (sha512_initial_state(), sha512_round_constants()),
+            _ => E::halt("Invalid hash size"),
+        }
+    }
+
+    #[inline]
+    const fn rotation_constants() -> Vec<Integer<E, bit_size!(NUM_BITS)>> {
+        match NUM_BITS {
+            256 => sha256_rotation_constants(),
+            512 => sha512_rotation_constants(),
+            _ => E::halt("Invalid hash size"),
+        }
+    }
+}
+
+#[macro_export]
+macro_rules! bit_size {
+    (256) => {
+        u32
+    };
+    (512) => {
+        u64
+    };
+    ($n:literal) => {
+        $E::halt("Invalid hash size")
+    };
+}
+
+macro_rules! initialize {
+    (256) => {
+        U32::zero()
+    };
+    (512) => {
+        U64::zero()
+    };
+    ($n:literal) => {
+        $E::halt("Invalid hash size")
+    };
 }
