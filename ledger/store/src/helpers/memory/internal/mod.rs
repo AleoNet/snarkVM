21af23200d73e27ff51af96a5eaa791586a87f5d
Howard Wu <9260812+howardwu@users.noreply.github.com>
2023-07-01 19:51:17 -0700

    clippy ally

--- ledger/store/src/helpers/memory/internal/mod.rs
+++ ledger/store/src/helpers/memory/internal/mod.rs
@@ -12,6 +12,8 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
+#![allow(clippy::type_complexity)]
+
 use crate::helpers::{Map, MapRead};
 use console::network::prelude::*;
 use indexmap::IndexMap;
