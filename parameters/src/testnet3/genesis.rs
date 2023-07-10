0a53b0b0546e0cd68c7ebb71ef8791e284a6882e
raychu86 <14917648+raychu86@users.noreply.github.com>
2023-07-10 12:08:53 -0700

    Resample genesis block with ConfirmedTransmissions

--- parameters/src/testnet3/genesis.rs
+++ parameters/src/testnet3/genesis.rs
@@ -27,6 +27,6 @@ mod tests {
     #[test]
     fn test_genesis_block() {
         let bytes = GenesisBytes::load_bytes();
-        assert_eq!(22763, bytes.len() as u64, "Update me if serialization has changed");
+        assert_eq!(22764, bytes.len() as u64, "Update me if serialization has changed");
     }
 }
