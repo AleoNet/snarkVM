d09a7efdadec5baa8d1718cb205510f44eaee50c
raychu86 <14917648+raychu86@users.noreply.github.com>
2023-07-19 14:41:42 -0700

    Use next_bits apporach for Plaintext from_bits

--- console/program/src/data/plaintext/from_bits.rs
+++ console/program/src/data/plaintext/from_bits.rs
@@ -17,20 +17,24 @@ use super::*;
 impl<N: Network> FromBits for Plaintext<N> {
     /// Initializes a new plaintext from a list of little-endian bits *without* trailing zeros.
     fn from_bits_le(bits_le: &[bool]) -> Result<Self> {
-        let mut counter = 0;
+        let mut bits = bits_le.iter().cloned();
 
-        let variant = [bits_le[counter], bits_le[counter + 1]];
-        counter += 2;
+        // Helper function to get the next n bits as a slice.
+        let mut next_bits = |n: usize| -> Result<Vec<bool>> {
+            let bits: Vec<_> = bits.by_ref().take(n).collect();
+            if bits.len() < n {
+                bail!("Insufficient bits");
+            }
+            Ok(bits)
+        };
+
+        let variant = [next_bits(1)?[0], next_bits(1)?[0]];
 
         // Literal
         if variant == [false, false] {
-            let literal_variant = u8::from_bits_le(&bits_le[counter..counter + 8])?;
-            counter += 8;
-
-            let literal_size = u16::from_bits_le(&bits_le[counter..counter + 16])?;
-            counter += 16;
-
-            let literal = Literal::from_bits_le(literal_variant, &bits_le[counter..counter + literal_size as usize])?;
+            let literal_variant = u8::from_bits_le(&next_bits(8)?)?;
+            let literal_size = u16::from_bits_le(&next_bits(16)?)?;
+            let literal = Literal::from_bits_le(literal_variant, &next_bits(literal_size as usize)?)?;
 
             // Store the plaintext bits in the cache.
             let cache = OnceCell::new();
@@ -42,22 +46,15 @@ impl<N: Network> FromBits for Plaintext<N> {
         }
         // Struct
         else if variant == [false, true] {
-            let num_members = u8::from_bits_le(&bits_le[counter..counter + 8])?;
-            counter += 8;
+            let num_members = u8::from_bits_le(&next_bits(8)?)?;
 
             let mut members = IndexMap::with_capacity(num_members as usize);
             for _ in 0..num_members {
-                let identifier_size = u8::from_bits_le(&bits_le[counter..counter + 8])?;
-                counter += 8;
+                let identifier_size = u8::from_bits_le(&next_bits(8)?)?;
+                let identifier = Identifier::from_bits_le(&next_bits(identifier_size as usize)?)?;
 
-                let identifier = Identifier::from_bits_le(&bits_le[counter..counter + identifier_size as usize])?;
-                counter += identifier_size as usize;
-
-                let member_size = u16::from_bits_le(&bits_le[counter..counter + 16])?;
-                counter += 16;
-
-                let value = Plaintext::from_bits_le(&bits_le[counter..counter + member_size as usize])?;
-                counter += member_size as usize;
+                let member_size = u16::from_bits_le(&next_bits(16)?)?;
+                let value = Plaintext::from_bits_le(&next_bits(member_size as usize)?)?;
 
                 if members.insert(identifier, value).is_some() {
                     bail!("Duplicate identifier in struct.");
@@ -80,20 +77,24 @@ impl<N: Network> FromBits for Plaintext<N> {
 
     /// Initializes a new plaintext from a list of big-endian bits *without* trailing zeros.
     fn from_bits_be(bits_be: &[bool]) -> Result<Self> {
-        let mut counter = 0;
+        let mut bits = bits_be.iter().cloned();
 
-        let variant = [bits_be[counter], bits_be[counter + 1]];
-        counter += 2;
+        // Helper function to get the next n bits as a slice.
+        let mut next_bits = |n: usize| -> Result<Vec<bool>> {
+            let bits: Vec<_> = bits.by_ref().take(n).collect();
+            if bits.len() < n {
+                bail!("Insufficient bits");
+            }
+            Ok(bits)
+        };
+
+        let variant = [next_bits(1)?[0], next_bits(1)?[0]];
 
         // Literal
         if variant == [false, false] {
-            let literal_variant = u8::from_bits_be(&bits_be[counter..counter + 8])?;
-            counter += 8;
-
-            let literal_size = u16::from_bits_be(&bits_be[counter..counter + 16])?;
-            counter += 16;
-
-            let literal = Literal::from_bits_be(literal_variant, &bits_be[counter..counter + literal_size as usize])?;
+            let literal_variant = u8::from_bits_be(&next_bits(8)?)?;
+            let literal_size = u16::from_bits_be(&next_bits(16)?)?;
+            let literal = Literal::from_bits_be(literal_variant, &next_bits(literal_size as usize)?)?;
 
             // Store the plaintext bits in the cache.
             let cache = OnceCell::new();
@@ -105,22 +106,15 @@ impl<N: Network> FromBits for Plaintext<N> {
         }
         // Struct
         else if variant == [false, true] {
-            let num_members = u8::from_bits_be(&bits_be[counter..counter + 8])?;
-            counter += 8;
+            let num_members = u8::from_bits_be(&next_bits(8)?)?;
 
             let mut members = IndexMap::with_capacity(num_members as usize);
             for _ in 0..num_members {
-                let identifier_size = u8::from_bits_be(&bits_be[counter..counter + 8])?;
-                counter += 8;
+                let identifier_size = u8::from_bits_be(&next_bits(8)?)?;
+                let identifier = Identifier::from_bits_be(&next_bits(identifier_size as usize)?)?;
 
-                let identifier = Identifier::from_bits_be(&bits_be[counter..counter + identifier_size as usize])?;
-                counter += identifier_size as usize;
-
-                let member_size = u16::from_bits_be(&bits_be[counter..counter + 16])?;
-                counter += 16;
-
-                let value = Plaintext::from_bits_be(&bits_be[counter..counter + member_size as usize])?;
-                counter += member_size as usize;
+                let member_size = u16::from_bits_be(&next_bits(16)?)?;
+                let value = Plaintext::from_bits_be(&next_bits(member_size as usize)?)?;
 
                 if members.insert(identifier, value).is_some() {
                     bail!("Duplicate identifier in struct.");
