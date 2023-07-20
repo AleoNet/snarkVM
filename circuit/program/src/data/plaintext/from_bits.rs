ed1dcd137b63f30dbb4e1322908aa232deab52ff
ljedrz <ljedrz@gmail.com>
2023-07-20 14:17:46 +0200

    perf: remove allocations from circuit::Plaintext::from_bits_*
    Signed-off-by: ljedrz <ljedrz@gmail.com>

--- circuit/program/src/data/plaintext/from_bits.rs
+++ circuit/program/src/data/plaintext/from_bits.rs
@@ -19,39 +19,51 @@ impl<A: Aleo> FromBits for Plaintext<A> {
 
     /// Initializes a new plaintext from a list of little-endian bits *without* trailing zeros.
     fn from_bits_le(bits_le: &[Boolean<A>]) -> Self {
-        let mut bits = bits_le.iter().cloned();
+        let bits = bits_le;
+
+        // The starting index used to create subsequent subslices of the `bits` slice.
+        let mut index = 0;
 
         // Helper function to get the next n bits as a slice.
-        let mut next_bits = |n: usize| -> Vec<Boolean<A>> {
-            let bits: Vec<_> = bits.by_ref().take(n).collect();
-            match bits.len() == n {
-                true => bits,
-                false => A::halt("Insufficient bits."),
+        let mut next_bits = |n: usize| -> &[Boolean<A>] {
+            // Safely procure a subslice with the length `n` starting at `index`.
+            let subslice = bits.get(index..index + n);
+            // Check if the range is within bounds.
+            if let Some(next_bits) = subslice {
+                // Move the starting index.
+                index += n;
+                // Return the subslice.
+                next_bits
+            } else {
+                A::halt("Insufficient bits.")
             }
         };
 
-        let variant = next_bits(2).iter().map(|b| b.eject_value()).collect::<Vec<_>>();
+        let mut variant = next_bits(2).iter().map(|b| b.eject_value());
+        let variant1 = variant.next().unwrap();
+        let variant2 = variant.next().unwrap();
+        let variant = [variant1, variant2];
 
         // Literal
         if variant == [false, false] {
-            let literal_variant = U8::from_bits_le(&next_bits(8));
-            let literal_size = U16::from_bits_le(&next_bits(16)).eject_value();
-            let literal = Literal::from_bits_le(&literal_variant, &next_bits(*literal_size as usize));
+            let literal_variant = U8::from_bits_le(next_bits(8));
+            let literal_size = U16::from_bits_le(next_bits(16)).eject_value();
+            let literal = Literal::from_bits_le(&literal_variant, next_bits(*literal_size as usize));
 
             // Cache the plaintext bits, and return the literal.
             Self::Literal(literal, OnceCell::with_value(bits_le.to_vec()))
         }
         // Struct
         else if variant == [false, true] {
-            let num_members = U8::from_bits_le(&next_bits(8)).eject_value();
+            let num_members = U8::from_bits_le(next_bits(8)).eject_value();
 
             let mut members = IndexMap::with_capacity(*num_members as usize);
             for _ in 0..*num_members {
-                let identifier_size = U8::from_bits_le(&next_bits(8)).eject_value();
-                let identifier = Identifier::from_bits_le(&next_bits(*identifier_size as usize));
+                let identifier_size = U8::from_bits_le(next_bits(8)).eject_value();
+                let identifier = Identifier::from_bits_le(next_bits(*identifier_size as usize));
 
-                let member_size = U16::from_bits_le(&next_bits(16)).eject_value();
-                let value = Plaintext::from_bits_le(&next_bits(*member_size as usize));
+                let member_size = U16::from_bits_le(next_bits(16)).eject_value();
+                let value = Plaintext::from_bits_le(next_bits(*member_size as usize));
 
                 members.insert(identifier, value);
             }
@@ -67,39 +79,51 @@ impl<A: Aleo> FromBits for Plaintext<A> {
 
     /// Initializes a new plaintext from a list of big-endian bits *without* trailing zeros.
     fn from_bits_be(bits_be: &[Boolean<A>]) -> Self {
-        let mut bits = bits_be.iter().cloned();
+        let bits = bits_be;
+
+        // The starting index used to create subsequent subslices of the `bits` slice.
+        let mut index = 0;
 
         // Helper function to get the next n bits as a slice.
-        let mut next_bits = |n: usize| -> Vec<Boolean<A>> {
-            let bits: Vec<_> = bits.by_ref().take(n).collect();
-            match bits.len() == n {
-                true => bits,
-                false => A::halt("Insufficient bits."),
+        let mut next_bits = |n: usize| -> &[Boolean<A>] {
+            // Safely procure a subslice with the length `n` starting at `index`.
+            let subslice = bits.get(index..index + n);
+            // Check if the range is within bounds.
+            if let Some(next_bits) = subslice {
+                // Move the starting index.
+                index += n;
+                // Return the subslice.
+                next_bits
+            } else {
+                A::halt("Insufficient bits.")
             }
         };
 
-        let variant = next_bits(2).iter().map(|b| b.eject_value()).collect::<Vec<_>>();
+        let mut variant = next_bits(2).iter().map(|b| b.eject_value());
+        let variant1 = variant.next().unwrap();
+        let variant2 = variant.next().unwrap();
+        let variant = [variant1, variant2];
 
         // Literal
         if variant == [false, false] {
-            let literal_variant = U8::from_bits_be(&next_bits(8));
-            let literal_size = U16::from_bits_be(&next_bits(16)).eject_value();
-            let literal = Literal::from_bits_be(&literal_variant, &next_bits(*literal_size as usize));
+            let literal_variant = U8::from_bits_be(next_bits(8));
+            let literal_size = U16::from_bits_be(next_bits(16)).eject_value();
+            let literal = Literal::from_bits_be(&literal_variant, next_bits(*literal_size as usize));
 
             // Cache the plaintext bits, and return the literal.
             Self::Literal(literal, OnceCell::with_value(bits_be.to_vec()))
         }
         // Struct
         else if variant == [false, true] {
-            let num_members = U8::from_bits_be(&next_bits(8)).eject_value();
+            let num_members = U8::from_bits_be(next_bits(8)).eject_value();
 
             let mut members = IndexMap::with_capacity(*num_members as usize);
             for _ in 0..*num_members {
-                let identifier_size = U8::from_bits_be(&next_bits(8)).eject_value();
-                let identifier = Identifier::from_bits_be(&next_bits(*identifier_size as usize));
+                let identifier_size = U8::from_bits_be(next_bits(8)).eject_value();
+                let identifier = Identifier::from_bits_be(next_bits(*identifier_size as usize));
 
-                let member_size = U16::from_bits_be(&next_bits(16)).eject_value();
-                let value = Plaintext::from_bits_be(&next_bits(*member_size as usize));
+                let member_size = U16::from_bits_be(next_bits(16)).eject_value();
+                let value = Plaintext::from_bits_be(next_bits(*member_size as usize));
 
                 members.insert(identifier, value);
             }
