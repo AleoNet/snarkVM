3853fe31ec18d98b7ed4557d2b3476106241f9a3
Pranav Gaddamadugu <pranav@aleo.org>
2023-08-01 19:17:29 -0400

    Add circuit::Literal::Signature

--- circuit/program/src/data/literal/from_bits.rs
+++ circuit/program/src/data/literal/from_bits.rs
@@ -34,8 +34,9 @@ impl<A: Aleo> Literal<A> {
             12 => Literal::U64(U64::from_bits_le(literal)),
             13 => Literal::U128(U128::from_bits_le(literal)),
             14 => Literal::Scalar(Scalar::from_bits_le(literal)),
-            15 => Literal::String(StringType::from_bits_le(literal)),
-            16.. => A::halt(format!("Failed to initialize literal variant {} from bits (LE)", variant.eject_value())),
+            15 => Literal::Signature(Signature::from_bits_le(literal)),
+            16 => Literal::String(StringType::from_bits_le(literal)),
+            17.. => A::halt(format!("Failed to initialize literal variant {} from bits (LE)", variant.eject_value())),
         }
     }
 
@@ -58,8 +59,9 @@ impl<A: Aleo> Literal<A> {
             12 => Literal::U64(U64::from_bits_be(literal)),
             13 => Literal::U128(U128::from_bits_be(literal)),
             14 => Literal::Scalar(Scalar::from_bits_be(literal)),
-            15 => Literal::String(StringType::from_bits_be(literal)),
-            16.. => A::halt(format!("Failed to initialize literal variant {} from bits (BE))", variant.eject_value())),
+            15 => Literal::Signature(Signature::from_bits_be(literal)),
+            16 => Literal::String(StringType::from_bits_be(literal)),
+            17.. => A::halt(format!("Failed to initialize literal variant {} from bits (BE))", variant.eject_value())),
         }
     }
 }
@@ -127,6 +129,16 @@ mod tests {
             check_serialization(Literal::<Circuit>::U128(U128::new(mode, Uniform::rand(rng))));
             // Scalar
             check_serialization(Literal::<Circuit>::Scalar(Scalar::new(mode, Uniform::rand(rng))));
+            // Signature
+            check_serialization(Literal::<Circuit>::Signature(Signature::new(
+                mode,
+                console::Signature::from((
+                    Uniform::rand(rng),
+                    Uniform::rand(rng),
+                    snarkvm_console_account::ComputeKey::try_from((Uniform::rand(rng), Uniform::rand(rng)))
+                        .expect("ComputeKey::try_from failed"),
+                )),
+            )));
             // String
             // Sample a random string. Take 1/4th to ensure we fit for all code points.
             let string = rng.next_string(Circuit::MAX_STRING_BYTES / 4, false);
