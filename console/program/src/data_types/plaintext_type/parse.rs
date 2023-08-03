e6ed937895d3e752492def2a928d4ddbf77f721e
Pranav Gaddamadugu <pranav@aleo.org>
2023-08-03 11:19:37 -0700

    Fixes

--- console/program/src/data_types/plaintext_type/parse.rs
+++ console/program/src/data_types/plaintext_type/parse.rs
@@ -147,9 +147,10 @@ mod tests {
     #[test]
     fn test_display() -> Result<()> {
         assert_eq!(PlaintextType::<CurrentNetwork>::Literal(LiteralType::Field).to_string(), "field");
+        assert_eq!(PlaintextType::<CurrentNetwork>::Literal(LiteralType::Signature).to_string(), "signature");
         assert_eq!(
-            PlaintextType::<CurrentNetwork>::Struct(Identifier::from_str("signature")?).to_string(),
-            "signature"
+            PlaintextType::<CurrentNetwork>::Struct(Identifier::from_str("foo")?).to_string(),
+            "foo"
         );
         Ok(())
     }
