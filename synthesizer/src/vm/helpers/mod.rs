ed31014348010edc56b67f777644613112e1b750
Howard Wu <9260812+howardwu@users.noreply.github.com>
2023-08-13 15:24:08 -0700

    Clippy

--- synthesizer/src/vm/helpers/mod.rs
+++ synthesizer/src/vm/helpers/mod.rs
@@ -12,7 +12,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-pub mod committee;
+pub(crate) mod committee;
 pub use committee::*;
 
 mod cost;
