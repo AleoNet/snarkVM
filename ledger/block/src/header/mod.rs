da122faf2db8cf61b2e057e568aac9989cbcf7f7
raychu86 <14917648+raychu86@users.noreply.github.com>
2023-08-16 18:06:21 -0400

    Remove total_supply_in_microcredits usage from header

--- ledger/block/src/header/mod.rs
+++ ledger/block/src/header/mod.rs
@@ -136,11 +136,6 @@ impl<N: Network> Header<N> {
         self.metadata.height()
     }
 
-    /// Returns the total supply of microcredits at this block.
-    pub const fn total_supply_in_microcredits(&self) -> u64 {
-        self.metadata.total_supply_in_microcredits()
-    }
-
     /// Returns the cumulative weight for this block.
     pub const fn cumulative_weight(&self) -> u128 {
         self.metadata.cumulative_weight()
