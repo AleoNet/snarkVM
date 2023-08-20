b28818a215df4097a2457b5b52e4441cbeab68b4
Howard Wu <9260812+howardwu@users.noreply.github.com>
2023-08-20 14:10:07 +0400

    Adds support for sha3

--- circuit/algorithms/src/keccak256/mod.rs
+++ circuit/algorithms/src/keccak256/mod.rs
@@ -20,9 +20,32 @@ use snarkvm_utilities::{TestRng, Uniform};
 use crate::Hash;
 use snarkvm_circuit_types::{environment::prelude::*, Boolean, U64};
 
-/// The rows and columns are 5 bits.
-const MODULO: usize = 5;
+/// The Keccak-224 hash function.
+pub type Keccak224<E> = Keccak<E, { KeccakType::Keccak as u8 }, 224>;
+/// The Keccak-256 hash function.
+pub type Keccak256<E> = Keccak<E, { KeccakType::Keccak as u8 }, 256>;
+/// The Keccak-384 hash function.
+pub type Keccak384<E> = Keccak<E, { KeccakType::Keccak as u8 }, 384>;
+/// The Keccak-512 hash function.
+pub type Keccak512<E> = Keccak<E, { KeccakType::Keccak as u8 }, 512>;
 
+/// The SHA3-224 hash function.
+pub type Sha3_224<E> = Keccak<E, { KeccakType::Sha3 as u8 }, 224>;
+/// The SHA3-256 hash function.
+pub type Sha3_256<E> = Keccak<E, { KeccakType::Sha3 as u8 }, 256>;
+/// The SHA3-384 hash function.
+pub type Sha3_384<E> = Keccak<E, { KeccakType::Sha3 as u8 }, 384>;
+/// The SHA3-512 hash function.
+pub type Sha3_512<E> = Keccak<E, { KeccakType::Sha3 as u8 }, 512>;
+
+/// A helper to specify the hash type.
+enum KeccakType {
+    Keccak,
+    Sha3,
+}
+
+/// The rows and columns are 5-bit lanes.
+const MODULO: usize = 5;
 /// The permutation type `l`.
 const L: usize = 6;
 /// The number of rounds in a full-round operation.
@@ -39,15 +62,14 @@ const NUM_ROUNDS: usize = 12 + 2 * L;
 /// The padding rule `pad` is a function that takes a variable-length input and produces a fixed-length output.
 /// In Keccak, `pad` is a multi-rate padding, defined as `pad(M) = M || 0x01 || 0x00…0x00 || 0x80`,
 /// where `M` is the input data, and `0x01 || 0x00…0x00 || 0x80` is the padding.
-/// In SHA-3, `pad` is a SHAKE or cSHAKE (for XOF) padding, defined as `pad(M) = M || 0x01 || 0x00…0x00`,
-/// where `M` is the input data, and `0x01 || 0x00…0x00` is the padding.
+/// In SHA-3, `pad` is a SHAKE, defined as `pad(M) = M || 0x06 || 0x00…0x00 || 0x80`,
+/// where `M` is the input data, and `0x06 || 0x00…0x00 || 0x80` is the padding.
 ///
 /// The bitrate `r` is the number of bits that are absorbed into the sponge state in each iteration
 /// of the absorbing phase.
-/// For our case, `r = 1088`.
 ///
-/// In addition, the capacity is defined as `c := b - r`. For our case, `c = 512`, as `b = 1600` and `r = 1088`.
-pub struct Keccak<E: Environment> {
+/// In addition, the capacity is defined as `c := b - r`.
+pub struct Keccak<E: Environment, const TYPE: u8, const VARIANT: usize> {
     /// The round constants `RC[t] ∈ GF(2)` are defined as the
     /// output of a linear feedback shift register (LFSR).
     round_constants: Vec<U64<E>>,
@@ -55,7 +77,7 @@ pub struct Keccak<E: Environment> {
     rotl: Vec<usize>,
 }
 
-impl<E: Environment> Keccak<E> {
+impl<E: Environment, const TYPE: u8, const VARIANT: usize> Keccak<E, TYPE, VARIANT> {
     /// Initializes a new Keccak hash function.
     pub fn new() -> Self {
         Self {
@@ -65,34 +87,34 @@ impl<E: Environment> Keccak<E> {
     }
 }
 
-impl<E: Environment> Hash for Keccak<E> {
+impl<E: Environment, const TYPE: u8, const VARIANT: usize> Hash for Keccak<E, TYPE, VARIANT> {
     type Input = Boolean<E>;
     type Output = Vec<Boolean<E>>;
 
-    /// Returns the Keccak-256 hash of the given input as bits.
+    /// Returns the Keccak hash of the given input as bits.
     #[inline]
     fn hash(&self, input: &[Self::Input]) -> Self::Output {
-        /// The capacity `c`.
-        const CAPACITY: usize = 512;
         /// The permutation width `b`.
         const PERMUTATION_WIDTH: usize = 1600;
-        /// The bitrate `r`.
-        const BITRATE: usize = PERMUTATION_WIDTH - CAPACITY;
-        /// The output length `l`.
-        const OUTPUT_LENGTH: usize = 256;
 
-        debug_assert!(BITRATE < PERMUTATION_WIDTH, "The bitrate must be less than the permutation width");
+        // The bitrate `r`.
+        let bitrate = (200 - (VARIANT / 4)) * 8;
+        debug_assert!(bitrate < PERMUTATION_WIDTH, "The bitrate must be less than the permutation width");
 
         // Ensure the input is not empty.
         if input.is_empty() {
             E::halt("The input to the hash function must not be empty")
         }
 
-        // The padded blocks `P`.
-        let padded_blocks = Self::pad_keccak::<PERMUTATION_WIDTH>(input, CAPACITY, BITRATE);
-
         // The root state `s` is defined as `0^b`.
         let mut s = vec![Boolean::constant(false); PERMUTATION_WIDTH];
+
+        // The padded blocks `P`.
+        let padded_blocks = match TYPE {
+            0 => Self::pad_keccak(input, bitrate),
+            1 => Self::pad_sha3(input, bitrate),
+            2.. => unreachable!("Invalid Keccak type"),
+        };
 
         /* The first part of the sponge construction (the absorbing phase):
          *
@@ -120,20 +142,20 @@ impl<E: Environment> Hash for Keccak<E> {
          * return Z[0..l-1]
          */
         // Z = s[0..r-1]
-        let mut z = s[..BITRATE].to_vec();
+        let mut z = s[..bitrate].to_vec();
         // while |Z|_r < l do
-        while z.len() < OUTPUT_LENGTH {
+        while z.len() < VARIANT {
             // s = f(s)
             s = Self::permutation_f::<PERMUTATION_WIDTH, NUM_ROUNDS>(s, &self.round_constants, &self.rotl);
             // Z = Z || s[0..r-1]
-            z.extend(s.iter().take(BITRATE).cloned());
+            z.extend(s.iter().take(bitrate).cloned());
         }
         // return Z[0..l-1]
-        z.into_iter().take(OUTPUT_LENGTH).collect()
+        z.into_iter().take(VARIANT).collect()
     }
 }
 
-impl<E: Environment> Keccak<E> {
+impl<E: Environment, const TYPE: u8, const VARIANT: usize> Keccak<E, TYPE, VARIANT> {
     /// The values `ROUND_CONSTANTS[t] ∈ GF(2)` are defined as the
     /// output of a binary linear feedback shift register (LFSR):
     /// ```text
@@ -198,26 +220,62 @@ impl<E: Environment> Keccak<E> {
     /// where `M` is the input data, and `0x01 || 0x00…0x00 || 0x80` is the padding.
     /// The padding extends the input data to a multiple of the bitrate `r`, defined as `r = b - c`,
     /// where `b` is the width of the permutation, and `c` is the capacity.
-    fn pad_keccak<const WIDTH: usize>(input: &[Boolean<E>], capacity: usize, bitrate: usize) -> Vec<Vec<Boolean<E>>> {
-        debug_assert_eq!(bitrate, WIDTH - capacity, "The bitrate must be less than the permutation width");
+    fn pad_keccak(input: &[Boolean<E>], bitrate: usize) -> Vec<Vec<Boolean<E>>> {
+        debug_assert!(bitrate > 0, "The bitrate must be positive");
 
         // Resize the input to a multiple of 8.
-        let mut input = input.to_vec();
-        input.resize((input.len() + 7) / 8 * 8, Boolean::constant(false));
+        let mut padded_input = input.to_vec();
+        padded_input.resize((input.len() + 7) / 8 * 8, Boolean::constant(false));
 
-        // Initialize a vector to store the padded blocks.
-        let mut result = Vec::with_capacity(input.len() / capacity);
-        // Iterate over the input in `capacity` chunks.
-        for block in input.chunks(capacity) {
-            // Construct the padded block.
-            let mut m = block.to_vec();
-            m.push(Boolean::constant(true));
-            m.resize(WIDTH, Boolean::constant(false));
-            m[bitrate - 1] = Boolean::constant(true);
-            // Append the padded block to the result.
-            result.push(m);
+        // Step 1: Append the bit "1" to the message.
+        padded_input.push(Boolean::constant(true));
+
+        // Step 2: Append "0" bits until the length of the message is congruent to r-1 mod r.
+        while (padded_input.len() % bitrate) != (bitrate - 1) {
+            padded_input.push(Boolean::constant(false));
         }
-        // Return the result.
+
+        // Step 3: Append the bit "1" to the message.
+        padded_input.push(Boolean::constant(true));
+
+        // Construct the padded blocks.
+        let mut result = Vec::new();
+        for block in padded_input.chunks(bitrate) {
+            result.push(block.to_vec());
+        }
+        result
+    }
+
+    /// In SHA-3, `pad` is a SHAKE, defined as `pad(M) = M || 0x06 || 0x00…0x00 || 0x80`,
+    /// where `M` is the input data, and `0x06 || 0x00…0x00 || 0x80` is the padding.
+    /// The padding extends the input data to a multiple of the bitrate `r`, defined as `r = b - c`,
+    /// where `b` is the width of the permutation, and `c` is the capacity.
+    fn pad_sha3(input: &[Boolean<E>], bitrate: usize) -> Vec<Vec<Boolean<E>>> {
+        debug_assert!(bitrate > 1, "The bitrate must be greater than 1");
+
+        // Resize the input to a multiple of 8.
+        let mut padded_input = input.to_vec();
+        padded_input.resize((input.len() + 7) / 8 * 8, Boolean::constant(false));
+
+        // Step 1: Append the "0x06" byte to the message.
+        padded_input.push(Boolean::constant(false));
+        padded_input.push(Boolean::constant(true));
+        padded_input.push(Boolean::constant(true));
+        padded_input.push(Boolean::constant(false));
+
+        // Step 2: Append "0" bits until the length of the message is congruent to r-1 mod r.
+        while (padded_input.len() % bitrate) != (bitrate - 1) {
+            padded_input.push(Boolean::constant(false));
+        }
+
+        // Step 3: Append the bit "1" to the message.
+        padded_input.push(Boolean::constant(true));
+
+        // Construct the padded blocks.
+        let mut result = Vec::new();
+        for block in padded_input.chunks(bitrate) {
+            result.push(block.to_vec());
+        }
         result
     }
 
@@ -356,18 +414,114 @@ mod tests {
     use snarkvm_circuit_types::environment::Circuit;
     use snarkvm_utilities::{bits_from_bytes_le, bytes_from_bits_le};
 
-    use tiny_keccak::{Hasher, Keccak as TinyKeccak};
+    use tiny_keccak::{Hasher, Keccak as TinyKeccak, Sha3 as TinySha3};
 
     const ITERATIONS: usize = 3;
 
+    /// Computes the Keccak-224 hash of the given preimage as bytes.
+    fn keccak_224_native(preimage: &[u8]) -> [u8; 28] {
+        let mut keccak = TinyKeccak::v224();
+        keccak.update(preimage);
+
+        let mut hash = [0u8; 28];
+        keccak.finalize(&mut hash);
+        hash
+    }
+
     /// Computes the Keccak-256 hash of the given preimage as bytes.
-    fn keccak256_native(preimage: &[u8]) -> [u8; 32] {
+    fn keccak_256_native(preimage: &[u8]) -> [u8; 32] {
         let mut keccak = TinyKeccak::v256();
         keccak.update(preimage);
 
         let mut hash = [0u8; 32];
         keccak.finalize(&mut hash);
         hash
+    }
+
+    /// Computes the Keccak-384 hash of the given preimage as bytes.
+    fn keccak_384_native(preimage: &[u8]) -> [u8; 48] {
+        let mut keccak = TinyKeccak::v384();
+        keccak.update(preimage);
+
+        let mut hash = [0u8; 48];
+        keccak.finalize(&mut hash);
+        hash
+    }
+
+    /// Computes the Keccak-512 hash of the given preimage as bytes.
+    fn keccak_512_native(preimage: &[u8]) -> [u8; 64] {
+        let mut keccak = TinyKeccak::v512();
+        keccak.update(preimage);
+
+        let mut hash = [0u8; 64];
+        keccak.finalize(&mut hash);
+        hash
+    }
+
+    /// Computes the SHA3-224 hash of the given preimage as bytes.
+    fn sha3_224_native(preimage: &[u8]) -> [u8; 28] {
+        let mut keccak = TinySha3::v224();
+        keccak.update(preimage);
+
+        let mut hash = [0u8; 28];
+        keccak.finalize(&mut hash);
+        hash
+    }
+
+    /// Computes the SHA3-256 hash of the given preimage as bytes.
+    fn sha3_256_native(preimage: &[u8]) -> [u8; 32] {
+        let mut keccak = TinySha3::v256();
+        keccak.update(preimage);
+
+        let mut hash = [0u8; 32];
+        keccak.finalize(&mut hash);
+        hash
+    }
+
+    /// Computes the SHA3-384 hash of the given preimage as bytes.
+    fn sha3_384_native(preimage: &[u8]) -> [u8; 48] {
+        let mut keccak = TinySha3::v384();
+        keccak.update(preimage);
+
+        let mut hash = [0u8; 48];
+        keccak.finalize(&mut hash);
+        hash
+    }
+
+    /// Computes the SHA3-512 hash of the given preimage as bytes.
+    fn sha3_512_native(preimage: &[u8]) -> [u8; 64] {
+        let mut keccak = TinySha3::v512();
+        keccak.update(preimage);
+
+        let mut hash = [0u8; 64];
+        keccak.finalize(&mut hash);
+        hash
+    }
+
+    macro_rules! check_equivalence {
+        ($circuit:expr, $native:expr) => {
+            let rng = &mut TestRng::default();
+
+            let mut input_sizes = vec![1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 16, 32, 64, 128, 256, 512, 1024];
+            input_sizes.extend((0..5).map(|_| u8::rand(rng) as usize));
+
+            for num_inputs in input_sizes {
+                println!("Checking equivalence for {num_inputs} inputs");
+
+                // Prepare the preimage.
+                let native_input = (0..num_inputs).map(|_| Uniform::rand(rng)).collect::<Vec<bool>>();
+                let input = native_input.iter().map(|v| Boolean::<Circuit>::new(Mode::Private, *v)).collect::<Vec<_>>();
+
+                // Compute the native hash.
+                let expected = $native(&bytes_from_bits_le(&native_input));
+                let expected = bits_from_bytes_le(&expected).collect::<Vec<_>>();
+
+                // Compute the circuit hash.
+                let candidate = $circuit.hash(&input);
+                assert_eq!(expected, candidate.eject_value());
+                Circuit::reset();
+            }
+        };
     }
 
     fn check_hash(
@@ -382,7 +536,7 @@ mod tests {
         use console::Hash as H;
 
         // let native = console::Poseidon::<<Circuit as Environment>::Network, RATE>::setup(DOMAIN)?;
-        let keccak = Keccak::<Circuit>::new();
+        let keccak = Keccak256::<Circuit>::new();
 
         for i in 0..ITERATIONS {
             // Prepare the preimage.
@@ -391,7 +545,7 @@ mod tests {
 
             // Compute the native hash.
             // let expected = native.hash(&native_input).expect("Failed to hash native input");
-            let expected = keccak256_native(&bytes_from_bits_le(&native_input));
+            let expected = keccak_256_native(&bytes_from_bits_le(&native_input));
             let expected = bits_from_bytes_le(&expected).collect::<Vec<_>>();
 
             // Compute the circuit hash.
@@ -406,7 +560,7 @@ mod tests {
     }
 
     #[test]
-    fn test_hash_constant() {
+    fn test_keccak_256_hash_constant() {
         let mut rng = TestRng::default();
 
         check_hash(Mode::Constant, 1, 0, 0, 0, 0, &mut rng);
@@ -422,11 +576,16 @@ mod tests {
         check_hash(Mode::Constant, 64, 0, 0, 0, 0, &mut rng);
         check_hash(Mode::Constant, 128, 0, 0, 0, 0, &mut rng);
         check_hash(Mode::Constant, 256, 0, 0, 0, 0, &mut rng);
+        check_hash(Mode::Constant, 511, 0, 0, 0, 0, &mut rng);
         check_hash(Mode::Constant, 512, 0, 0, 0, 0, &mut rng);
+        check_hash(Mode::Constant, 513, 0, 0, 0, 0, &mut rng);
+        check_hash(Mode::Constant, 1023, 0, 0, 0, 0, &mut rng);
+        check_hash(Mode::Constant, 1024, 0, 0, 0, 0, &mut rng);
+        check_hash(Mode::Constant, 1025, 0, 0, 0, 0, &mut rng);
     }
 
     #[test]
-    fn test_hash_public() {
+    fn test_keccak_256_hash_public() {
         let mut rng = TestRng::default();
 
         check_hash(Mode::Public, 1, 0, 0, 138157, 138157, &mut rng);
@@ -443,10 +602,11 @@ mod tests {
         check_hash(Mode::Public, 128, 0, 0, 149248, 149248, &mut rng);
         check_hash(Mode::Public, 256, 0, 0, 150848, 150848, &mut rng);
         check_hash(Mode::Public, 512, 0, 0, 151424, 151424, &mut rng);
+        check_hash(Mode::Public, 1024, 0, 0, 152448, 152448, &mut rng);
     }
 
     #[test]
-    fn test_hash_private() {
+    fn test_keccak_256_hash_private() {
         let mut rng = TestRng::default();
 
         check_hash(Mode::Private, 1, 0, 0, 138157, 138157, &mut rng);
@@ -463,10 +623,51 @@ mod tests {
         check_hash(Mode::Private, 128, 0, 0, 149248, 149248, &mut rng);
         check_hash(Mode::Private, 256, 0, 0, 150848, 150848, &mut rng);
         check_hash(Mode::Private, 512, 0, 0, 151424, 151424, &mut rng);
+        check_hash(Mode::Private, 1024, 0, 0, 152448, 152448, &mut rng);
     }
 
     #[test]
-    fn test_keccak256_simple() {
+    fn test_keccak_224_equivalence() {
+        check_equivalence!(Keccak224::<Circuit>::new(), keccak_224_native);
+    }
+
+    #[test]
+    fn test_keccak_256_equivalence() {
+        check_equivalence!(Keccak256::<Circuit>::new(), keccak_256_native);
+    }
+
+    #[test]
+    fn test_keccak_384_equivalence() {
+        check_equivalence!(Keccak384::<Circuit>::new(), keccak_384_native);
+    }
+
+    #[test]
+    fn test_keccak_512_equivalence() {
+        check_equivalence!(Keccak512::<Circuit>::new(), keccak_512_native);
+    }
+
+    #[test]
+    fn test_sha3_224_equivalence() {
+        check_equivalence!(Sha3_224::<Circuit>::new(), sha3_224_native);
+    }
+
+    #[test]
+    fn test_sha3_256_equivalence() {
+        check_equivalence!(Sha3_256::<Circuit>::new(), sha3_256_native);
+    }
+
+    #[test]
+    fn test_sha3_384_equivalence() {
+        check_equivalence!(Sha3_384::<Circuit>::new(), sha3_384_native);
+    }
+
+    #[test]
+    fn test_sha3_512_equivalence() {
+        check_equivalence!(Sha3_512::<Circuit>::new(), sha3_512_native);
+    }
+
+    #[test]
+    fn test_keccak_256_simple() {
         let input = [
             91, 7, 224, 119, 168, 31, 252, 107, 71, 67, 95, 101, 168, 114, 123, 204, 84, 43, 198, 252, 15, 37, 165, 98,
             16, 239, 177, 167, 75, 136, 165, 174, 94, 93, 131, 178, 76, 70, 67, 50, 228, 244, 192, 226, 95, 102, 35,
@@ -476,12 +677,12 @@ mod tests {
             107, 59, 106, 234, 176, 69, 94, 6, 174, 161, 74, 60, 246, 41, 201, 195, 133, 117, 2, 65, 58, 246, 33, 201,
             230, 106, 98, 171, 43, 238, 105, 82,
         ];
-        assert_eq!(output, keccak256_native(&input));
+        assert_eq!(output, keccak_256_native(&input));
 
         let input_circuit =
             input.to_bits_le().iter().map(|b| Boolean::<Circuit>::new(Mode::Public, *b)).collect::<Vec<_>>();
 
-        let keccak = Keccak::<Circuit>::new();
+        let keccak = Keccak256::<Circuit>::new();
 
         let output_circuit = keccak.hash(&input_circuit);
 
