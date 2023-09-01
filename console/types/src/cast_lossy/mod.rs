e05b053a45761abce09d5e44191195385897546f
Pranav Gaddamadugu <pranav@aleo.org>
2023-09-01 16:10:53 -0400

    Introduce the CastLossy trait; implement on target types for fields

---
+++ console/types/src/cast_lossy/mod.rs
@@ -0,0 +1,48 @@
+// Copyright (C) 2019-2023 Aleo Systems Inc.
+// This file is part of the snarkVM library.
+
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at:
+// http://www.apache.org/licenses/LICENSE-2.0
+
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+mod field;
+
+use crate::prelude::{
+    bail,
+    Address,
+    Boolean,
+    Environment,
+    Field,
+    FromField,
+    One,
+    Result,
+    Scalar,
+    ToBits,
+    Zero,
+    I128,
+    I16,
+    I32,
+    I64,
+    I8,
+    U128,
+    U16,
+    U32,
+    U64,
+    U8,
+};
+
+/// Unary operator for casting values of one type to another, with lossy truncation.
+pub trait CastLossy<T: Sized = Self> {
+    /// Casts the value of `self` into a value of type `T`, with lossy truncation.
+    ///
+    /// This method makes a *best-effort* attempt to preserve all bits of information,
+    /// but it is not guaranteed to do so.
+    fn cast_lossy(&self) -> Result<T>;
+}
