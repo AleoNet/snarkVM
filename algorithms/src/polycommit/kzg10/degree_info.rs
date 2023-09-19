6acf4be72c8f26519c136c0f915053ab2807d1a1
Victor Sint Nicolaas <vicsn@users.noreply.github.com>
2023-09-19 15:14:08 +0100

    Use bit operator for union

--- algorithms/src/polycommit/kzg10/degree_info.rs
+++ algorithms/src/polycommit/kzg10/degree_info.rs
@@ -35,12 +35,12 @@ impl DegreeInfo {
         let max_fft_size = self.max_fft_size.max(other.max_fft_size);
         let mut new_bounds = self.degree_bounds.unwrap_or_default();
         if let Some(other_bounds) = &other.degree_bounds {
-            new_bounds = new_bounds.union(other_bounds).copied().collect();
+            new_bounds = &new_bounds | &other_bounds;
         }
         let degree_bounds = (!new_bounds.is_empty()).then_some(new_bounds);
         let mut new_l_sizes = self.lagrange_sizes.unwrap_or_default();
         if let Some(other_l_sizes) = &other.lagrange_sizes {
-            new_l_sizes = new_l_sizes.union(other_l_sizes).copied().collect();
+            new_l_sizes = &new_l_sizes | &other_l_sizes;
         }
         let lagrange_sizes = (!new_l_sizes.is_empty()).then_some(new_l_sizes);
         Self { max_degree, max_fft_size, degree_bounds, lagrange_sizes, hiding_bound }
