f7a483dcbe73616a6db630db0c11803caea37b57
Howard Wu <9260812+howardwu@users.noreply.github.com>
2023-09-27 09:16:02 -0700

    Update instruction count

--- synthesizer/program/src/logic/instruction/mod.rs
+++ synthesizer/program/src/logic/instruction/mod.rs
@@ -469,7 +469,7 @@ mod tests {
     fn test_opcodes() {
         // Sanity check the number of instructions is unchanged.
         assert_eq!(
-            67,
+            68,
             Instruction::<CurrentNetwork>::OPCODES.len(),
             "Update me if the number of instructions changes."
         );
