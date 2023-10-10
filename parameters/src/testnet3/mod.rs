20650522b5bea2ef7bb5104858bfe401e68dcd88
Howard Wu <9260812+howardwu@users.noreply.github.com>
2023-10-09 17:23:55 -0700

    Adds wasm guards on parameters, reintroduces shifted 16 local support

--- parameters/src/testnet3/mod.rs
+++ parameters/src/testnet3/mod.rs
@@ -22,7 +22,10 @@ const REMOTE_URL: &str = "https://s3-us-west-1.amazonaws.com/testnet3.parameters
 
 // Degrees
 impl_local!(Degree15, "resources/", "powers-of-beta-15", "usrs");
+#[cfg(not(feature = "wasm"))]
 impl_local!(Degree16, "resources/", "powers-of-beta-16", "usrs");
+#[cfg(feature = "wasm")]
+impl_remote!(Degree16, REMOTE_URL, "resources/", "powers-of-beta-16", "usrs");
 impl_remote!(Degree17, REMOTE_URL, "resources/", "powers-of-beta-17", "usrs");
 impl_remote!(Degree18, REMOTE_URL, "resources/", "powers-of-beta-18", "usrs");
 impl_remote!(Degree19, REMOTE_URL, "resources/", "powers-of-beta-19", "usrs");
@@ -38,6 +41,9 @@ impl_remote!(Degree28, REMOTE_URL, "resources/", "powers-of-beta-28", "usrs");
 
 // Shifted Degrees
 impl_local!(ShiftedDegree15, "resources/", "shifted-powers-of-beta-15", "usrs");
+#[cfg(not(feature = "wasm"))]
+impl_local!(ShiftedDegree16, "resources/", "shifted-powers-of-beta-16", "usrs");
+#[cfg(feature = "wasm")]
 impl_remote!(ShiftedDegree16, REMOTE_URL, "resources/", "shifted-powers-of-beta-16", "usrs");
 impl_remote!(ShiftedDegree17, REMOTE_URL, "resources/", "shifted-powers-of-beta-17", "usrs");
 impl_remote!(ShiftedDegree18, REMOTE_URL, "resources/", "shifted-powers-of-beta-18", "usrs");
