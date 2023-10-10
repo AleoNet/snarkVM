b48ea5c8c2a9b2085c4484a2efb875f71e2ef702
raychu86 <14917648+raychu86@users.noreply.github.com>
2023-10-10 17:25:15 -0400

    Update RATIFICATIONS_DEPTH to 16

--- console/program/src/state_path/configuration/mod.rs
+++ console/program/src/state_path/configuration/mod.rs
@@ -22,7 +22,7 @@ pub const HEADER_DEPTH: u8 = 3;
 /// The depth of the Merkle tree for finalize operations in a block.
 pub const FINALIZE_OPERATIONS_DEPTH: u8 = 20;
 /// The depth of the Merkle tree for the ratifications in a block.
-pub const RATIFICATIONS_DEPTH: u8 = 20;
+pub const RATIFICATIONS_DEPTH: u8 = 16;
 /// The depth the Merkle tree for the subdag certificates in a block.
 pub const SUBDAG_CERTIFICATES_DEPTH: u8 = 16;
 /// The depth of the Merkle tree for transactions in a block.
