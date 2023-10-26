c0648c15a5ebc3172733068c0839886c6f321381
raychu86 <14917648+raychu86@users.noreply.github.com>
2023-10-26 18:52:28 -0400

    Remove map in NestedMemoryMap if there are no more keys

--- ledger/store/src/helpers/memory/internal/nested_map.rs
+++ ledger/store/src/helpers/memory/internal/nested_map.rs
@@ -130,10 +130,25 @@ impl<
     fn remove_key(&self, map: &M, key: &K) -> Result<()> {
         // Determine if an atomic batch is in progress.
         match self.is_atomic_in_progress() {
-            // If a batch is in progress, add the key-None pair to the batch.
-            true => self.atomic_batch.lock().push((*map, Some(key.clone()), None)),
-            // Otherwise, remove the key-value pair directly from the map.
-            false => remove_key(&mut self.map.write(), &mut self.map_inner.write(), map, key),
+            true => {
+                // Add the key-None pair to the batch.
+                self.atomic_batch.lock().push((*map, Some(key.clone()), None));
+                // If there are no map-key pairs left, add the map-None pair to the batch.
+                if !self.contains_map_speculative(map)? {
+                    self.atomic_batch.lock().push((*map, None, None));
+                }
+            }
+            false => {
+                // Acquire the locks.
+                let mut map_lock = self.map.write();
+                let mut map_inner_lock = self.map_inner.write();
+                // Remove the key-value pair.
+                remove_key(&mut map_lock, &mut map_inner_lock, map, key);
+                // Remove the map if it is now empty.
+                if map_lock.is_empty() {
+                    remove_map(&mut map_lock, &mut map_inner_lock, map);
+                }
+            }
         }
         Ok(())
     }
@@ -266,6 +281,9 @@ impl<
     fn contains_map_speculative(&self, map: &M) -> Result<bool> {
         // If a batch is in progress, check the atomic batch first.
         if self.is_atomic_in_progress() {
+            // Fetch the remaining keys in the map.
+            let mut remaining_keys = self.get_map_confirmed(map)?.into_iter().map(|(k, _)| k).collect::<Vec<_>>();
+
             // We iterate from the back of the `atomic_batch` to find the latest value.
             for (m, k, v) in self.atomic_batch.lock().iter().rev() {
                 // If the map does not match the given map, then continue.
@@ -279,12 +297,22 @@ impl<
                 // If the value is 'None', then continue, as we do not know for certain whether the map exists.
                 // For instance, if the map contains two KV entries, then removing just one KV entry will not remove the map.
                 if v.is_none() {
+                    // Remove the key from the list of remaining keys.
+                    if let Some(index) = k.as_ref().and_then(|key| remaining_keys.iter().position(|x| x == key)) {
+                        remaining_keys.remove(index);
+                    }
+
                     continue;
                 }
                 // If the key is 'Some(K)' and the value is 'Some(V)', then the map exists.
                 if k.is_some() && v.is_some() {
                     return Ok(true);
                 }
+            }
+
+            // If there are no more remaining keys, then the map does not exist.
+            if remaining_keys.is_empty() {
+                return Ok(false);
             }
         }
         // Otherwise, check the confirmed map.
