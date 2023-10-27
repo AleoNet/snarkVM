b886930a9068c41c79c2ce1cc889161ae75586c7
Alessandro Coglio <coglio@kestrel.edu>
2023-10-27 13:17:25 -0700

    Update constraint counts.

--- circuit/program/src/data/literal/cast_lossy/field.rs
+++ circuit/program/src/data/literal/cast_lossy/field.rs
@@ -145,15 +145,15 @@ mod tests {
     fn test_field_to_address() {
         check_cast_lossy::<Address<Circuit>, console_root::types::Address<Testnet3>>(
             Mode::Constant,
-            count_less_than!(551, 0, 0, 0),
+            count_less_than!(4303, 0, 0, 0),
         );
         check_cast_lossy::<Address<Circuit>, console_root::types::Address<Testnet3>>(
             Mode::Public,
-            count_is!(277, 0, 899, 904),
+            count_is!(2029, 0, 6745, 6750),
         );
         check_cast_lossy::<Address<Circuit>, console_root::types::Address<Testnet3>>(
             Mode::Private,
-            count_is!(277, 0, 899, 904),
+            count_is!(2029, 0, 6745, 6750),
         );
     }
 
@@ -184,15 +184,15 @@ mod tests {
     fn test_field_to_group() {
         check_cast_lossy::<Group<Circuit>, console_root::types::Group<Testnet3>>(
             Mode::Constant,
-            count_less_than!(551, 0, 0, 0),
+            count_less_than!(4303, 0, 0, 0),
         );
         check_cast_lossy::<Group<Circuit>, console_root::types::Group<Testnet3>>(
             Mode::Public,
-            count_is!(277, 0, 899, 904),
+            count_is!(2029, 0, 6745, 6750),
         );
         check_cast_lossy::<Group<Circuit>, console_root::types::Group<Testnet3>>(
             Mode::Private,
-            count_is!(277, 0, 899, 904),
+            count_is!(2029, 0, 6745, 6750),
         );
     }
 
