0bc0295ff546304654fb7a212d0982223b1f103f
raychu86 <14917648+raychu86@users.noreply.github.com>
2023-12-19 00:15:45 -0800

    Use Signature::size_in_bytes

--- ledger/narwhal/batch-certificate/src/bytes.rs
+++ ledger/narwhal/batch-certificate/src/bytes.rs
@@ -63,11 +63,10 @@ impl<N: Network> FromBytes for BatchCertificate<N> {
                 )));
             }
             // Read the signature bytes.
-            let signature_size_in_bytes = (Signature::<N>::size_in_bits() + 7) / 8;
-            let mut signature_bytes = vec![0u8; num_signatures as usize * signature_size_in_bytes];
+            let mut signature_bytes = vec![0u8; num_signatures as usize * Signature::<N>::size_in_bytes()];
             reader.read_exact(&mut signature_bytes)?;
             // Read the signatures.
-            let signatures = cfg_chunks!(signature_bytes, signature_size_in_bytes)
+            let signatures = cfg_chunks!(signature_bytes, Signature::<N>::size_in_bytes())
                 .map(Signature::read_le)
                 .collect::<Result<IndexSet<_>, _>>()?;
             // Return the batch certificate.
