e7aeba30ef55f4c70668c4fcc2d420e10d9d4bd0
Howard Wu <9260812+howardwu@users.noreply.github.com>
2024-01-20 11:20:08 -0800

    Fix the number of serialized elements in the block

--- ledger/block/src/serialize.rs
+++ ledger/block/src/serialize.rs
@@ -19,7 +19,7 @@ impl<N: Network> Serialize for Block<N> {
     fn serialize<S: Serializer>(&self, serializer: S) -> Result<S::Ok, S::Error> {
         match serializer.is_human_readable() {
             true => {
-                let mut block = serializer.serialize_struct("Block", 8 + self.solutions.is_some() as usize)?;
+                let mut block = serializer.serialize_struct("Block", 9)?;
                 block.serialize_field("block_hash", &self.block_hash)?;
                 block.serialize_field("previous_hash", &self.previous_hash)?;
                 block.serialize_field("header", &self.header)?;
