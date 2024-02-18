c4ef7b4288bc8acd37e7e33e7a415d4ebda5bbe8
Alphena-EK <101400276+Alphena-EK@users.noreply.github.com>
2024-02-18 16:53:57 +0300

    Update tests.rs
    Signed-off-by: Alphena-EK <101400276+Alphena-EK@users.noreply.github.com>

--- wasm/src/tests.rs
+++ wasm/src/tests.rs
@@ -17,27 +17,47 @@ use snarkvm_console::{
     network::MainnetV0,
 };
 use snarkvm_utilities::TestRng;
-
-use core::str::FromStr;
 use wasm_bindgen_test::*;
 
+const ALEO_PRIVATE_KEY: &str = "APrivateKey1zkp8cC4jgHEBnbtu3xxs1Ndja2EMizcvTRDq5Nikdkukg1p";
+const ALEO_VIEW_KEY: &str = "AViewKey1n1n3ZbnVEtXVe3La2xWkUvY3EY7XaCG6RZJJ3tbvrrrD";
+const ALEO_ADDRESS: &str = "aleo1wvgwnqvy46qq0zemj0k6sfp3zv0mp77rw97khvwuhac05yuwscxqmfyhwf";
 const ITERATIONS: usize = 1000;
 
 #[wasm_bindgen_test]
 fn test_account() {
-    const ALEO_PRIVATE_KEY: &str = "APrivateKey1zkp8cC4jgHEBnbtu3xxs1Ndja2EMizcvTRDq5Nikdkukg1p";
-    const ALEO_VIEW_KEY: &str = "AViewKey1n1n3ZbnVEtXVe3La2xWkUvY3EY7XaCG6RZJJ3tbvrrrD";
-    const ALEO_ADDRESS: &str = "aleo1wvgwnqvy46qq0zemj0k6sfp3zv0mp77rw97khvwuhac05yuwscxqmfyhwf";
-
-    let private_key = PrivateKey::<MainnetV0>::from_str(ALEO_PRIVATE_KEY).unwrap();
+    let private_key = PrivateKey::<MainnetV0>::from_str(ALEO_PRIVATE_KEY)
+        .expect("Failed to parse private key string");
     assert_eq!(ALEO_PRIVATE_KEY, private_key.to_string());
 
-    let view_key = ViewKey::try_from(&private_key).unwrap();
+    let view_key = ViewKey::try_from(&private_key)
+        .expect("Failed to derive view key from private key");
     assert_eq!(ALEO_VIEW_KEY, view_key.to_string());
 
-    let address = Address::try_from(&view_key).unwrap();
+    let address = Address::try_from(&view_key)
+        .expect("Failed to derive address from view key");
     assert_eq!(ALEO_ADDRESS, address.to_string());
 }
+
+#[wasm_bindgen_test]
+fn test_account_sign() {
+    let mut rng = TestRng::default();
+
+    for _ in 0..ITERATIONS {
+        let private_key = PrivateKey::<MainnetV0>::new(&mut rng)
+            .expect("Failed to generate private key");
+        let address = Address::try_from(&private_key)
+            .expect("Failed to derive address from private key");
+
+        let result = private_key.sign_bytes("hello world!".as_bytes(), &mut rng);
+        assert!(result.is_ok(), "Failed to generate a signature");
+
+        let signature = result.unwrap();
+        let result = signature.verify_bytes(&address, "hello world!".as_bytes());
+        assert!(result, "Failed to execute signature verification");
+    }
+}
+
 
 #[wasm_bindgen_test]
 fn test_account_sign() {
