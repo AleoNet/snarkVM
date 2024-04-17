f2d060898d350c1cc25a27eba2686b544bd33342
Pranav Gaddamadugu <23022326+d0cd@users.noreply.github.com>
2024-04-17 07:15:21 -0700

    Introduce LimitedReader

--- utilities/src/bytes.rs
+++ utilities/src/bytes.rs
@@ -478,6 +478,35 @@ pub fn bytes_from_bits_le(bits: &[bool]) -> Vec<u8> {
     bytes
 }
 
+pub struct LimitedReader<R: Read> {
+    reader: R,
+    limit: usize,
+    remaining: usize,
+}
+
+impl<R: Read> LimitedReader<R> {
+    pub fn new(reader: R, limit: usize) -> Self {
+        Self { reader, limit, remaining: limit }
+    }
+}
+
+impl<R: Read> Read for LimitedReader<R> {
+    fn read(&mut self, buf: &mut [u8]) -> IoResult<usize> {
+        if self.remaining == 0 {
+            return Err(std::io::Error::new(std::io::ErrorKind::Other, format!("Byte limit exceeded: {}", self.limit)));
+        }
+
+        let max_read = std::cmp::min(buf.len(), self.remaining);
+        match self.reader.read(&mut buf[..max_read]) {
+            Ok(n) => {
+                self.remaining -= n;
+                Ok(n)
+            }
+            Err(e) => Err(e),
+        }
+    }
+}
+
 #[cfg(test)]
 mod test {
     use super::*;
