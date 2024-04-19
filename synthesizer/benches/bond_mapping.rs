4e5d9843452d097f481e12ecc5a3d17605c31971
Michael Turner <mturner@aleo.org>
2024-04-19 07:09:25 -0400

    Bench get_value_speculative/confirmed on bonded mapping at increasing intervals

--- synthesizer/benches/bond_mapping.rs
+++ synthesizer/benches/bond_mapping.rs
@@ -12,20 +12,6 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-// Copyright (C) 2019-2023 Aleo Systems Inc.
-// This file is part of the snarkVM library.
-
-// Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at:
-// http://www.apache.org/licenses/LICENSE-2.0
-
-// Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
-// limitations under the License.
-
 #[macro_use]
 extern crate criterion;
 
@@ -42,9 +28,11 @@ use ledger_store::helpers::memory::ConsensusMemory;
 use ledger_store::helpers::rocksdb::ConsensusDB;
 use ledger_store::ConsensusStore;
 use snarkvm_synthesizer::VM;
+use utilities::cfg_into_iter;
 
 use criterion::Criterion;
 use indexmap::indexmap;
+#[cfg(not(feature = "serial"))]
 use rayon::iter::{IntoParallelIterator, ParallelIterator};
 use std::{str::FromStr, time::Duration};
 
@@ -54,27 +42,28 @@ pub type CurrentStorage = ConsensusDB<CurrentNetwork>;
 #[cfg(not(feature = "rocks"))]
 pub type CurrentStorage = ConsensusMemory<CurrentNetwork>;
 
+pub const BONDED_INTERVALS: &[usize; 7] = &[10, 5_000, 10_000, 20_000, 40_000, 80_000, 100_000];
+
 fn bench_bonded_mappings(c: &mut Criterion) {
     // Construct the credits.aleo program ID.
     let credits_program_id = ProgramID::from_str("credits.aleo").unwrap();
     // Construct the bonded mapping name.
     let bonded_mapping = Identifier::from_str("bonded").unwrap();
-    // Construct the bonded_state struct identifiers.
-    let validator_identifier = Identifier::from_str("validator").expect("Failed to parse 'validator'");
-    let microcredits_identifier = Identifier::from_str("microcredits").expect("Failed to parse 'microcredits'");
+    // Construct the bond_state identifiers.
+    let validator_identifier = Identifier::from_str("validator").unwrap();
+    let microcredits_identifier = Identifier::from_str("microcredits").unwrap();
     // Create a DB store for the consensus.
     let store = ConsensusStore::<CurrentNetwork, CurrentStorage>::open(None).unwrap();
     // Create a VM from the store.
     let vm = VM::from(store).unwrap();
-    // Get a sample validator address.
+    // Create a sample validator address.
     let validator_address =
         Address::from_str("aleo1rhgdu77hgyqd3xjj8ucu3jj9r2krwz6mnzyd80gncr5fxcwlh5rsvzp9px").unwrap();
 
     // Generate 100_000 bonded mapping entries.
-    let bonded_map = (0..(MAX_DELEGATORS as u64))
-        .into_par_iter()
+    let bonded_map = cfg_into_iter!((0..(MAX_DELEGATORS as u64)))
         .map(|i| {
-            // Generate addresses for the stakers.
+            // Generate a staker address.
             let private_key = PrivateKey::try_from(Field::from_u64(i)).unwrap();
             let staker_address = Address::<CurrentNetwork>::try_from(&private_key).unwrap();
 
@@ -90,26 +79,29 @@ fn bench_bonded_mappings(c: &mut Criterion) {
         })
         .collect::<Vec<(Plaintext<CurrentNetwork>, Value<CurrentNetwork>)>>();
 
-    // Insert 100_000 entries into the bonded mapping.
-    vm.finalize_store().replace_mapping(credits_program_id, bonded_mapping, bonded_map).unwrap();
-
-    // Get a key to access the bonded mapping with.
-    let private_key = PrivateKey::try_from(Field::from_u64(0)).unwrap();
+    // Get a key that exists within the bonded mapping.
+    let private_key = PrivateKey::try_from(Field::from_u64(5)).unwrap();
     let staker_address = Address::<CurrentNetwork>::try_from(&private_key).unwrap();
     let key = Plaintext::from(Literal::Address(staker_address));
 
-    // Benchmark get_value_speculative on the bonded mapping at 100_000 entries.
-    c.bench_function("bonded mapping - get_value_speculative - 100_000 entries", |b| {
-        b.iter(|| {
-            let _value = vm.finalize_store().get_value_speculative(credits_program_id, bonded_mapping, &key).unwrap();
-        })
-    });
+    // Insert increasing numbers of entries into the bonded mapping and bench get_value_speculative and get_value_confirmed at each interval.
+    BONDED_INTERVALS.iter().for_each(|num_entries| {
+        let entries = bonded_map.iter().take(*num_entries).cloned().collect::<Vec<_>>();
+        vm.finalize_store().replace_mapping(credits_program_id, bonded_mapping, entries).unwrap();
 
-    // Benchmark get_value_confirmed on the bonded mapping at 100_000 entries.
-    c.bench_function("bonded mapping - get_value_confirmed - 100_000 entries", |b| {
-        b.iter(|| {
-            let _value = vm.finalize_store().get_value_confirmed(credits_program_id, bonded_mapping, &key).unwrap();
-        })
+        // Benchmark get_value_speculative on the bonded mapping.
+        c.bench_function(&format!("bonded mapping - get_value_speculative - {num_entries} entries"), |b| {
+            b.iter(|| {
+                vm.finalize_store().get_value_speculative(credits_program_id, bonded_mapping, &key).unwrap();
+            })
+        });
+
+        // Benchmark get_value_confirmed on the bonded mapping.
+        c.bench_function(&format!("bonded mapping - get_value_confirmed - {num_entries} entries"), |b| {
+            b.iter(|| {
+                vm.finalize_store().get_value_confirmed(credits_program_id, bonded_mapping, &key).unwrap();
+            })
+        });
     });
 }
 
