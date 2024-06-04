c693573a1ed62cc170774b350f3099748fdc7587
Robert Wafle <robert@wafle.com>
2024-06-04 11:59:26 -0500

    test circleci fix

---
+++ .circleci/use_circleci_runners.sh
@@ -0,0 +1,2 @@
+sed -i '' 's@resource_class: anf/xlarge@resource_class: xlarge@g' config.yml
+sed -i '' 's@resource_class: anf/2xlarge@resource_class: 2xlarge@g' config.yml
---
+++ .circleci/use_self_hosted_runners.sh
@@ -0,0 +1,2 @@
+sed -i '' 's@resource_class: xlarge@resource_class: anf/xlarge@g' config.yml
+sed -i '' 's@resource_class: 2xlarge@resource_class: anf/2xlarge@g' config.yml
