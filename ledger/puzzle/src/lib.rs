4fba8563508602794a0c0894601e581a6d0aa496
Jos Dehaes <jos.dehaes@gmail.com>
2024-08-15 09:53:41 +0200

    fix: deduplicate log lines

--- ledger/puzzle/src/lib.rs
+++ ledger/puzzle/src/lib.rs
@@ -172,7 +172,7 @@ impl<N: Network> Puzzle<N> {
                     // Ensure that the proof target matches the expected proof target.
                     ensure!(
                         solution.target() == *proof_target,
-                        "The proof target does not match the expected proof target"
+                        "The proof target does not match the cached proof target"
                     );
                     targets[i] = *proof_target
                 }
@@ -195,7 +195,7 @@ impl<N: Network> Puzzle<N> {
                     // Ensure that the proof target matches the expected proof target.
                     ensure!(
                         solution.target() == proof_target,
-                        "The proof target does not match the expected proof target"
+                        "The proof target does not match the computed proof target"
                     );
                     // Insert the proof target into the cache.
                     self.proof_target_cache.write().put(*solution_id, proof_target);
