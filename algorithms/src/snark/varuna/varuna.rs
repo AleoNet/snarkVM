83707b8bf19ef44f330bcac0c055604fab400385
raychu86 <14917648+raychu86@users.noreply.github.com>
2024-09-09 12:31:28 -0400

    nit

--- algorithms/src/snark/varuna/varuna.rs
+++ algorithms/src/snark/varuna/varuna.rs
@@ -332,7 +332,7 @@ where
 
     /// This is the main entrypoint for creating proofs.
     /// You can find a specification of the prover algorithm in:
-    /// https://github.com/AleoHQ/protocol-docs
+    /// https://github.com/AleoNet/protocol-docs
     fn prove_batch<C: ConstraintSynthesizer<E::Fr>, R: Rng + CryptoRng>(
         universal_prover: &Self::UniversalProver,
         fs_parameters: &Self::FSParameters,
@@ -621,7 +621,7 @@ where
 
     /// This is the main entrypoint for verifying proofs.
     /// You can find a specification of the verifier algorithm in:
-    /// https://github.com/AleoHQ/protocol-docs
+    /// https://github.com/AleoNet/protocol-docs
     fn verify_batch<B: Borrow<Self::VerifierInput>>(
         universal_verifier: &Self::UniversalVerifier,
         fs_parameters: &Self::FSParameters,
